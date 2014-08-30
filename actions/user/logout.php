4735bd321933dd5a3687693465e6507697442955
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:25:23 +0500

    initial release

---
+++ actions/user/logout.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+ 
+session_destroy();
+redirect();	
