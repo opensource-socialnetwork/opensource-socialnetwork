89a61923c835d843828512e92e48f700e7e57491
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:26:59 +0500

    initial release

---
+++ components/OssnAds/locale/ossn.en.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+$en = array(
+			'ossnads' => 'Ads Manager',
+			'fields:required' => 'All fields are required',
+			'ad:created' => 'Ad have been created',
+			'ad:create:fail' => 'Cannot crate ad',
+			'ad:title' => 'Title',
+			'ad:site:url' => 'Siteurl',
+			'ad:desc' => 'Description',
+			'ad:photo' => 'Photo',
+			'ad:browse' => 'Browse',
+			'ad:clicks' => 'Clicks',
+			'sponsored' => 'Sponsored',
+	  );
+ossn_register_languages('en', $en); 
