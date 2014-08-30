ddf8b016ba003ce2369311cd84a78738ad8452fc
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:31:52 +0500

    initial release

---
+++ components/OssnGroups/js/groups.php
@@ -0,0 +1,7 @@
+Ossn.RegisterStartupFunction(function(){
+   $(document).ready(function(){
+	     $('#ossn-group-add').click(function(){
+                      Ossn.MessageBox('groups/add');
+         }); 
+	});
+});
