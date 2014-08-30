b85849af8d519d8de1ebf7a20145c2693516ef36
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:25:08 +0500

    initial release

---
+++ actions/administrator/theme/theme_install.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+$OssnTheme = new OssnThemes;
+if($OssnTheme->upload()){
+   ossn_trigger_message(ossn_print('theme:installed'), 'success', 'admin');
+   redirect(REF);	
+} else { 
+   ossn_trigger_message('theme:install:error', 'success', 'admin');
+   redirect(REF);	 
+}
