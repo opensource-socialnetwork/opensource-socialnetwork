fec99c7f2621848d8f2907917d68566e94680d4d
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:58:40 +0500

    initial release

---
+++ pages/administrator/contents/themes.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+$OssnThemes = new OssnThemes;
+foreach($OssnThemes->getThemes() as $id){
+	$Theme = $OssnThemes->getTheme($id);
+	echo "<div class='ossn-components-item'>";
+	echo "<div class='component-controls'>";
+	 if(ossn_site_settings('theme') == $id){
+	 echo "<a href='#' class='components-button components-button-blue'>Enabled</a>";
+	 } else {
+	 $enable  = ossn_site_url("action/theme/enable?theme={$id}");
+	 echo "<a href='{$enable}' class='components-button components-button-green'>Enable</a>";		 
+	 }
+	 if($OssnThemes->getActive() !== $id){
+	 $delete = ossn_site_url("action/theme/delete?theme={$id}");
+     echo "<a href='{$delete}' class='components-button components-button-red'>Delete</a>";
+	 }
+     echo "</div>";
+    
+	echo "<div class='component-name'>{$Theme->theme_name} {$Theme->theme_version}</div>";
+	echo "<div class='compontnet-meta'>
+	        Author: {$Theme->theme_author}<br />
+			Website: {$Theme->theme_author_url}
+	 </div>";
+	echo "<div class='component-description'>{$Theme->theme_description}</div>";
+	echo '</div>';
+}
