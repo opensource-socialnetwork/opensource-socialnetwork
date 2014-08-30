0c69fcdc3b71697614ced6a16aba0e895d78cba2
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:44:01 +0500

    initial release

---
+++ forms/admin/users/list_search.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+?>
+<div class="user-search">
+<input type="text" name="search_users"placeholder="<?php echo ossn_print('search');?>" />
+</div>
