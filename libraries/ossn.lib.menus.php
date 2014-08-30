550d7914cde0e44e54a91fb73750179dcd6dbfee
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:57:27 +0500

    initial release

---
+++ libraries/ossn.lib.menus.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+/**
+* Register a menu;
+* @params: $name  = name of menu;
+* @params:  $text = text for menu;
+* @params $link = link for menu;
+*
+* @last edit: $arsalanshah
+* @return void 
+*/ 
+function ossn_register_menu_link($name , $text, $link ,$for = 'site'){
+	global $Ossn;	
+    $Ossn->menu[$for][$name][$text] = $link;
+}
+
+/**
+* Unregister menu from system;
+* @params: $menu = menu name
+*
+* @last edit: $arsalanshah
+* @return void;
+* 
+*/
+function unregister_menu($menu, $for = 'site'){
+	global $Ossn;
+	unset($Ossn->menu[$for][$menu]);
+}
+/**
+* View a menu;
+* @params: $menu = menu name
+* @note This will fetch layout from defualt template that how menu should appear; check menu file for more info;
+*
+* @last edit: $arsalanshah
+* @return: mixed data;
+* 
+*/
+function ossn_view_menu($menu){
+	global $Ossn;
+	$active_theme = ossn_site_settings('theme'); 
+	$params['menu'] = $Ossn->menu[$menu];
+	$params['menuname'] = $menu;
+ 	return ossn_view("themes/{$active_theme}/menus/{$menu}", $params);
+}
+/**
+* Register a section base menu;
+* @params: $params array(type, section, url, text, icon, link)
+*
+* @last edit: $arsalanshah
+* @return void 
+*/
+function ossn_register_sections_menu($menu, $params){
+	global $Ossn;	
+	$type = $params['type'];
+	$link = $params['url'];
+	$text = $params['text'];
+	$type = $params['type'];
+	$icon = $params['icon'];
+	$section = $params['section'];
+	if(empty($type)){
+	 $type = 'frontend';	
+	}
+    $Ossn->sectionsmenu[$type][$menu][$section][$text] = array($link, $icon, $params['params']);
+}
+/**
+* View section base menu;
+* @params: $type = (frontend or backend(
+* @note This will fetch layout from defualt template that how menu should appear; check menu file for more info;
+*
+* @last edit: $arsalanshah
+* @return: mixed data;
+* 
+*/
+function ossn_view_sections_menu($menu, $type = 'frontend'){
+ global $Ossn;	
+  if(isset($menu) && isset($Ossn->sectionsmenu[$type][$menu])){
+	$active_theme = ossn_site_settings('theme'); 
+	$params['menu'] =  $Ossn->sectionsmenu[$type][$menu];
+	$params['menuname'] = $menu;
+ 	return ossn_view("themes/{$active_theme}/menus/sections/{$menu}", $params);
+}	
+}
