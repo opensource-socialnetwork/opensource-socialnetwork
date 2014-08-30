95a1bf3d58b098ce823dd57e915e6cc642c74f45
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:58:57 +0500

    initial release

---
+++ pages/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+$title = ossn_print('site:index');
+$content = ossn_set_page_layout('startup', array(
+								   'content' => ossn_view('pages/contents/index')
+								));
+echo ossn_view_page($title, $content);
