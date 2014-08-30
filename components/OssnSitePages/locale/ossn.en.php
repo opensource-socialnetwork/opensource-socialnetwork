613e6521e2774176360e238b7e2406ed940723b8
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:42:47 +0500

    initial release

---
+++ components/OssnSitePages/locale/ossn.en.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+$en = array(
+			'ossnsitepages' => 'Site Pages',
+			'site:privacy' => 'Privacy',
+			'site:about' => 'About',
+			'site:terms' => 'Terms and Conditions',
+			
+			'page:saved' => 'Page is saved successfully',
+			'page:save:error' => 'Cannot save page',
+			
+	  );
+ossn_register_languages('en', $en); 
