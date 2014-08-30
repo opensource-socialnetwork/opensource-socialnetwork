28f52a344da344a1f17618f275586be213e8474d
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:42:54 +0500

    initial release

---
+++ components/OssnSitePages/pages/page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+?>
+<div class="ossn-site-pages">
+  <div class="ossn-site-pages-inner">
+       <div class="ossn-site-pages-title">
+         <?php echo $params['title']; ?>
+       </div>
+       <div class="ossn-site-pages-body">
+         <?php echo $params['contents']; ?>
+       </div>
+  </div>
+</div>
