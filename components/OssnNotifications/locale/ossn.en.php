9a26c6a0930c6dce15e65de7c28364627501d5d0
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:34:21 +0500

    initial release

---
+++ components/OssnNotifications/locale/ossn.en.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+
+$en = array(
+			'ossn:notification:are:friends' => 'You are now friends',
+			'ossn:notification:delete:friend' => 'Friend request deleted',
+			'notifications' => 'Notifications',
+			'see:all' => 'See All',
+			
+	  );
+ossn_register_languages('en', $en); 
