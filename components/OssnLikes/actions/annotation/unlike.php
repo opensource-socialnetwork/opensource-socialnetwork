2b6154e0fc7b9eb3ac27d92edef18ef7314ad484
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:32:18 +0500

    initial release

---
+++ components/OssnLikes/actions/annotation/unlike.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+$OssnLikes = new OssnLikes;
+$anotation = input('annotation');
+if($OssnLikes->UnLike($anotation, ossn_loggedin_user()->guid, 'annotation')){
+  redirect(REF);	
+} 
+else {
+  redirect(REF);
+}
+
