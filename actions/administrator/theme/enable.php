61cb7206aa6dbe3c903c3ad51df35424bb96934f
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:25:06 +0500

    initial release

---
+++ actions/administrator/theme/enable.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+$theme = input('theme');
+$enable = new OssnThemes;
+if($enable->Enable($theme)){
+ ossn_trigger_message(ossn_print('theme:enabled'), 'success', 'admin');	
+ redirect(REF);	
+}
