3532818f598a7e6bd9fd81f57a6afd140fb49322
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:31:56 +0500

    initial release

---
+++ components/OssnGroups/locale/ossn.en.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+$en = array(
+			'groups' => 'Groups',
+			'add:group' => 'Add Group',
+			'requests' => 'Requests',
+			
+			'members' => 'Members',
+			'member:add:error' => 'Something went wrong please try again',
+			'member:added' => 'Membership approved',
+			
+			'member:request:deleted' => 'Membership successfully declined',
+			'member:request:delete:fail' => 'Cannot decline membership please try again later',
+			'membership:cancel:succes' => 'Membership successfully canceled',
+			'membership:cancel:fail' => 'Cannot cancel membership please try again later',
+			
+			'group:added' => 'Successfully created the group',
+			'group:add:fail' => 'Cannot create group',
+			
+			'memebership:sent' => 'Request successfully sent',
+			'memebership:sent:fail' => 'Cannot send request',
+			
+			'group:updated' => 'Group has been updated successfully',
+			'group:update:fail' => 'Cannot update group',
+			
+			'group:name' => 'Group Name',
+			'group:desc' => 'Group Description',
+			'privacy:group:public' => 'Any one can see the group, its post, only members can post',
+			'privacy:group:close' => 'Any one can see the group, only members can post',
+			
+			'group:memb:remove' => 'Remove',
+			'leave:group' => 'Leave Group',
+			'join:group' => 'Join Group',
+			'total:members' => 'Total Members',
+			'view:all' => 'View all',
+			'member:requests' => 'REQUESTS (%s)',
+			'about:group' => 'Group About',
+			'cancel:membership' => 'Membership cancel',
+			
+			'no:requests' => 'No Requests',
+			'approve' => 'Approve',
+			'decline' => 'Decline',
+			'search:groups' => 'Search Groups',
+			
+
+	  );
+ossn_register_languages('en', $en); 
