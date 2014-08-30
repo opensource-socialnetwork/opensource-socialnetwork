eb50e97491977e68d5c9b554da05bfa7dba6a35a
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:41:27 +0500

    initial release

---
+++ components/OssnProfile/locale/ossn.en.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+$en = array(
+	   'people:like:this' => 'People Who like this post',
+	   'change:cover' => 'Change Cover',
+	   'change:photo' => 'Change Photo',
+	   'update:info' => 'Update Info',
+	   'message' => 'Message',
+	   'save:position' => 'Save Position',
+	  );
+ossn_register_languages('en', $en); 
