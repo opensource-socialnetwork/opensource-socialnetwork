1079f20c4ab509921e7e6231491410f20177448e
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:59:19 +0500

    initial release

---
+++ system/templates/users_list.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+$users = $params['users'];
+if(!isset($params['icon_size'])){
+ $avatar_size = 'large';	
+} else {
+  $avatar_size = $params['icon_size'];	
+}
+foreach($users as $user){ ?>
+
+<div class="ossn-list-users">
+  <img src="<?php echo ossn_site_url("avatar/{$user->username}/{$avatar_size}");?>" />
+ <div class="uinfo">
+    <a class="userlink" href="<?php echo ossn_site_url();?>u/<?php echo $user->username;?>"><?php echo $user->fullname;?></a>
+ </div>
+     	      <?php   
+			  if( ossn_loggedin_user()->guid !== $user->guid){
+			  if(!ossn_user_is_friend(ossn_loggedin_user()->guid ,$user->guid)){ ?>   
+                              <a href="<?php echo ossn_site_url();?>action/friend/add?user=<?php echo $user->guid;?>" class='friendlink button-grey-light'>Add Friend</a>
+                      <?php } else { ?>
+                               <a href="<?php echo ossn_site_url();?>action/friend/remove?user=<?php echo $user->guid;?>" class='friendlink button-grey-light'>Unfriend</a>   
+                        <?php } 
+						
+			  }?>
+</div>
+
+
+<?php } ?>
