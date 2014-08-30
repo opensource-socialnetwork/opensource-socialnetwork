906590b1c7e93ca1e63fa9823681f8524009afb8
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:27:24 +0500

    initial release

---
+++ components/OssnComments/classes/OssnComments.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+class OssnComments extends OssnAnnotation {
+   /**
+	* Post Comment
+	*
+	* @params $subject_id: Id of item on which user comment
+	*         $guid: User id
+	*         $comment: Comment
+	*         $type: Post or Entity
+	*
+	* @return bool;
+	*/	
+	public function PostComment($subject_id, $guid, $comment, $type = 'post'){
+        $this->subject_guid = $subject_id;
+        $this->owner_guid = $guid;
+        $this->type = "comments:{$type}";
+        $this->value = $comment;
+        if($this->addAnnotation()){
+            return true;
+		}
+        return false; 
+	}
+   /**
+	* Get Comments
+	*
+	* @params $subject_id: Id of item on which users comment
+	*         $type: Post or Entity
+	*
+	* @return object;
+	*/		
+    public function GetComments($subject, $type = 'post'){
+        $this->subject_guid = $subject;
+        $this->type = "comments:{$type}";
+		$this->order_by = 'id ASC';
+        $comments = $this->getAnnotationBySubject();
+        if(!empty($comments)){
+            return $comments;
+		}
+	}
+   /**
+	* Get Comment
+	*
+	* @params $id: Comment Id
+	*
+	* @return object;
+	*/	
+	public function GetComment($id){
+	  $this->annotation_id = $id;
+	  return $this->getAnnotationById();
+	}
+   /**
+	* Count Total Comments on Subject
+	*
+	* @params $subject_id: Subject id
+	*         $type: Comments type
+	*
+	* @return int;
+	*/		
+	public function countComments($subject_id, $type = 'post'){
+		return count(get_object_vars($this->GetComments($subject_id, $type)));
+	}
+   /**
+	* Delete All Comments by Subject id
+	*
+	* @params $subject: Subject id
+	*
+	* @return bool
+	*/			
+	public function commentsDeleteAll($subject){
+	   if($this->annon_delete_by_subject($subject, 'comments:post')){
+		 return true;   
+	   }
+	 return false;  
+	}
+   /**
+	* Get newly created comment id
+	*
+	* @return int
+	*/		
+	public function getCommentId(){
+	  return $this->getAnnotationId();	
+	}
+
+}//class
