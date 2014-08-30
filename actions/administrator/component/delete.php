27e0e64b038337a2d155c576fea3a26a0ae106a2
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:24:44 +0500

    initial release

---
+++ actions/administrator/component/delete.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+ 
+$com = input('component');
+$delete = new OssnComponents;
+if($delete->deletecom($com)){
+	   ossn_trigger_message(ossn_print('com:deleted'), 'success', 'admin');
+	   redirect(REF);	
+} else {
+	   ossn_trigger_message(ossn_print('con:delete:error'), 'error', 'admin');
+	   redirect(REF);	
+}
