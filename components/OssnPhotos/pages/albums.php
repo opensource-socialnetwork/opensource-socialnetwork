9143148d9e94adc07408dbfe0f7a815283374de3
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:40:56 +0500

    initial release

---
+++ components/OssnPhotos/pages/albums.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+
+$albums = new OssnAlbums;
+$photos = $albums->GetAlbum($params['album']);
+echo '<div class="ossn-photos">';
+echo '<h2>'.$photos->album->title.'</h2>';
+foreach($photos->photos as $photo){
+	$image = str_replace('album/photos/', '', $photo->value);
+	$image = ossn_site_url()."album/getphoto/{$params['album']}/{$image}?size=album";
+	$view_url = ossn_site_url().'photos/view/'.$photo->guid;
+	echo "<li><a href='{$view_url}'><img src='{$image}' /></a></li>";	
+}
+echo '</div>';
