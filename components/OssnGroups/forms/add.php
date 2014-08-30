b53f5a3660428c18da9c61ae68751ab2f90a92e6
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:31:33 +0500

    initial release

---
+++ components/OssnGroups/forms/add.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+?>
+<label><?php echo ossn_print('group:name');?></label>
+<input type="text" name="groupname" />
+<input type="submit" class="ossn-hidden" id="ossn-group-submit"/>
+
+<div class="ossn-privacy">
+<table border="0">
+  <tr>
+    <td style="vertical-align:top;">
+      <label><?php echo ossn_print('privacy');?></label>
+    </td>
+    <td>
+    <input type="radio" name="privacy" value="2" checked="checked" />
+    <span><?php echo ossn_print('public');?></span>
+    <p><?php echo ossn_print('privacy:group:public');?></p>
+    
+    <input type="radio" name="privacy" value="1" />
+    <span><?php echo ossn_print('close');?></span>
+    <p> <?php echo ossn_print('privacy:group:close');?></p>
+    </td>
+  </tr>
+</table>
+</div>
