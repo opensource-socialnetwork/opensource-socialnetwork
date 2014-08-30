c1bd2e0f4e7ec1a9b4d570e9ff96076353174599
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:42:21 +0500

    initial release

---
+++ components/OssnSitePages/actions/edit/about.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+$save = new OssnSitePages;
+$save->pagename = 'about';
+$save->pagebody = input('pagebody');
+if($save->SaveSitePage()){
+    ossn_trigger_message(ossn_print('page:saved'), 'success', 'admin');
+	redirect(REF);	
+} else {
+    ossn_trigger_message(ossn_print('page:save:error'), 'error', 'admin');
+	redirect(REF);	
+}
