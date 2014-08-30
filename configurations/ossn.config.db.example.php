019670eafd29b61e5a5b7bd5a5158b67f1d7a979
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:43:41 +0500

    initial release

---
+++ configurations/ossn.config.db.example.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+ 
+ 
+// replace <<host>> with your database host name;
+$Ossn->host = '<<host>>';
+
+// replace <<user>> with your database username;
+$Ossn->user = '<<user>>';
+
+// replace <<password>> with your database password;
+$Ossn->password = '<<password>>';
+
+// replace <<dbname>> with your database name;
+$Ossn->database = '<<dbname>>';
