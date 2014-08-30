d1774dd796744fe56a99d5a11581f0833e830a89
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:44:06 +0500

    initial release

---
+++ forms/login.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+?>  
+<div>
+<label><?php echo ossn_print('username');?> </label> 
+<input type="text" name="username"/>
+</div>
+
+<div>
+<label> <?php echo ossn_print('password');?> </label> 
+<input type="password" name="password"/>
+</div>
+
+<input type="submit" value="Login" class="ossn-button ossn-button-submit"/>
+<br />
