fdb74ccd500a713cd32fa3594ff9897d0d8218b5
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:27:49 +0500

    initial release

---
+++ components/OssnComments/post/comments.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+ 
+$object = $params->guid;
+
+$OssnComments = new OssnComments;
+$OssnLikes = new OssnLikes;
+
+$comments  = $OssnComments->GetComments($object);
+if(is_object($comments)){
+    echo "<div class='ossn-comments-list-{$object}'>";
+    $count = 0; 
+    foreach($comments as $comment){  
+        if($count <= 5){
+           $data['comment'] = get_object_vars($comment);
+           echo ossn_view('components/OssnComments/templates/comment', $data);
+        }
+        $count++;
+	}
+    echo '</div>';
+} 
+echo  '<div class="poster-image">';
+echo '<img src="'.ossn_site_url().'avatar/'.ossn_loggedin_user()->username.'/smaller" />';
+echo   '</div>';
+echo  '<script>  Ossn.PostComment('.$object.'); </script>';
+
+echo ossn_view_form('post/comment_add', array(
+							   'action' => ossn_site_url().'action/post/comment',
+							   'component' => 'OssnComments',
+							   'id' => "comment-container-{$object}", 
+							   'class' => 'comment-container',
+							   'autocomplete' => 'off',
+							   'params' => array('object' => $object)
+							    ), false);
