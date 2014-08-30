17adf80d06d8fdaf8e46fded567d9121c6ba4f45
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:44:40 +0500

    initial release

---
+++ installation/application/pages/account.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+ 
+
+echo bframework_view_page(array(
+'body' => bframework_view(bframework_get_approot_path().'template/account'),
+'title' => bframework_print('ossn:setting:account')
+));
