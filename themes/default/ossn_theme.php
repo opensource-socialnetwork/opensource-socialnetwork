45cc653e6e1e523bfa254a27f7fb98ed1bc8b76d
Arsalan Shah <manactric@gmail.com>
2014-08-30 17:00:38 +0500

    initial release

---
+++ themes/default/ossn_theme.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+define('__THEMEDIR__', ossn_route()->themes.'default/');
+ossn_new_css('ossn.default', 'themes/default/style/default');
+ossn_new_css('ossn.admin.default', 'themes/default/style/administrator');
+
+ossn_load_css('ossn.default');
+ossn_load_css('ossn.admin.default', 'admin');
