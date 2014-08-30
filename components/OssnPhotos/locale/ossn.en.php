8802f828fa2f80e5311db020dd6c14bb8c50a4e4
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:40:40 +0500

    initial release

---
+++ components/OssnPhotos/locale/ossn.en.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+
+$en = array(
+			'ossn:notification:are:friends' => 'You are now friends',
+			'ossn:notification:delete:friend' => 'Friend request deleted',
+			'notifications' => 'Notifications',
+			'album:name' => 'Album Name',
+			'add:album' => 'Add Album',
+			'photo:select' => 'Select Photo',
+			'no:albums' => 'No Albums',
+			'no:photos' => 'No Photos',
+			'back:to:album' => 'Back to Album',
+			'photo:albums' => 'Photo Albums',
+			'edit:profile' => 'Edit Profile',
+			'reposition:cover' => 'Reposition',
+
+	  );
+ossn_register_languages('en', $en); 
