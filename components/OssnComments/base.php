da85ed007b2155a734a7c0d98b7c5a135d1b883d
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:27:21 +0500

    initial release

---
+++ components/OssnComments/base.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+echo '<div class="comments-likes">';
+	 if(ossn_is_hook('post', 'likes:entity')){
+	  $entity['entity_guid'] = $params['entity_guid'];
+	 echo ossn_call_hook('post', 'likes:entity', $entity); 
+     }
+    if(ossn_is_hook('post', 'comments:entity')){
+	  $entity['entity_guid'] = $params['entity_guid'];
+	 echo ossn_call_hook('post', 'comments:entity', $entity); 
+     }
+echo '</div>';
