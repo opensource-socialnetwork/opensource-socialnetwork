d8c0c8395c50d2ed35b86e519063932aaf0ff04d
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:32:02 +0500

    initial release

---
+++ components/OssnGroups/page/group.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+?>
+<style> body { background:#E9EAED; } </style>
+<div class="ossn-layout-group">
+<div class="ossn-inner">
+      <div class="coloum-left">
+   &nbsp;
+   <?php 
+   if(ossn_is_hook('newsfeed', "left")){
+	$newsfeed_left = ossn_call_hook('newsfeed', "left", NULL, array());
+	echo implode('', $newsfeed_left);
+   }
+   ?>
+      </div>  
+      <div class="coloum-middle">
+             <?php echo $params['content']; ?>
+  
+      </div>
+     
+     </div>
+</div>
