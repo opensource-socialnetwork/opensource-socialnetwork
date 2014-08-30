4e7858bde89c91d82eafb30b38a856fb41348612
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:43:38 +0500

    initial release

---
+++ configurations/libraries.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+
+$Ossn->libraries = array(  
+						'actions',
+						'input',
+						'languages',
+						'relations',
+						'system',
+						'views',
+						'page',
+						'menus',
+						'css',
+						'cache',
+						'javascripts',
+						'initialize',
+						'users',
+						'image',
+						'admin',
+						'components',
+						);
