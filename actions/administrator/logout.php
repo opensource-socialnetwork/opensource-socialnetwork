46fad23c3e4495ec088a912a6b78cd7b985c3faa
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:24:58 +0500

    initial release

---
+++ actions/administrator/logout.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+ 
+$loggedout = input('l');
+if(empty($loggedout)){
+    session_destroy();
+	redirect('action/admin/logout?l=1');
+}
+if($loggedout == 1){
+ ossn_trigger_message(ossn_print('logged:out'), 'success', 'admin');	
+ redirect('administrator');
+}
