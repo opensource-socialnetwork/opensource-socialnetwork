e9caab85a01c02c80277d19788a540613d93de6f
Arsalan Shah <manactric@gmail.com>
2014-08-30 19:00:06 +0500

    Update ossn.en.php

--- components/OssnNotifications/locale/ossn.en.php
+++ components/OssnNotifications/locale/ossn.en.php
@@ -10,10 +10,18 @@
  */
 
 $en = array(
-			'ossn:notification:are:friends' => 'You are now friends',
-			'ossn:notification:delete:friend' => 'Friend request deleted',
-			'notifications' => 'Notifications',
-			'see:all' => 'See All',
+	'ossn:notifications:comments:post' => "%s commeted on the post",
+	'ossn:notifications:like:post' => "%s liked your post",
+	'ossn:notifications:like:annotation' => "%s liked your comment",
+	'ossn:notifications:like:entity:file:ossn:aphoto' => "%s liked your photo",
+	'ossn:notifications:comments:entity:file:ossn:aphoto' => '%s commented on your photo',
+	'ossn:notifications:wall:friends:tag' => '%s tag you in a post',
+	'ossn:notification:are:friends' => 'You are now friends',
+		
+	'ossn:notification:are:friends' => 'You are now friends',
+	'ossn:notification:delete:friend' => 'Friend request deleted',
+	'notifications' => 'Notifications',
+	'see:all' => 'See All',
 			
 	  );
 ossn_register_languages('en', $en); 
