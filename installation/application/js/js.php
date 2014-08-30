4330644ef19ce3fc45aee5f061f33228c47e785b
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:44:35 +0500

    initial release

---
+++ installation/application/js/js.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Buddyexpress Framework Core
+ *
+ * @package   Bframework
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2012 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://bserver.buddyexpress.net
+ * @Contributors http://www.buddyexpress.net/bframework/contributors.b
+ */
+ 
+require_once(dirname(dirname(__FILE__)).'/start.php');
+echo bframework_view_js();
+
+?>
