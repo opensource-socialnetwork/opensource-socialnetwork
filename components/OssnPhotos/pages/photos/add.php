9196aaa7de014fc83ec8a1d9ed06c1e3232c8f94
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:41:04 +0500

    initial release

---
+++ components/OssnPhotos/pages/photos/add.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+$album = input('album');
+echo ossn_view_form('photos/add', array(
+									   'action' => ossn_site_url().'action/ossn/photos/add?album='.$album,
+									   'method' => 'POST',
+									   'component' => 'OssnPhotos',
+									   ), false);
