b43218ef14840386a1bdbad3e685acb730b6a7d2
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:57:14 +0500

    initial release

---
+++ libraries/ossn.lib.components.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+/**
+ * Check whether component is active or not.
+ *
+ * @param string $com   Component id
+ *
+ * @return bool
+ */ 
+function com_is_active($comn){
+	$com = new OssnComponents;
+	if($com->isActive($comn)){
+	return true;	
+	}
+return false;	
+}
+/**
+ * Count total components
+ *
+ * @return bool
+ */ 
+function ossn_total_components(){
+	$com = new OssnComponents;	
+	return $com->total();
+}
+
+/**
+ * Load the locales
+ *
+ * @return array
+ */ 
+ossn_load_locales();
+
+/**
+ * Includes all components and active theme
+ *
+ * @return bool
+ */ 
+ 
+ //loads active theme
+$themes = new OssnThemes;
+include_once($themes->getActivePath());
+
+//load active components
+$coms = new OssnComponents;
+$coms->loadComs();
+
+foreach(ossn_registered_com_panel() as $configure){
+ ossn_register_menu_link('configure', $configure, ossn_site_url("administrator/component/{$configure}"), 'topbar_admin');	
+}
