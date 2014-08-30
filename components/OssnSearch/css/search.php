b1f2e21f01d8fca16c01a3274cb7858fa18d047f
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:41:48 +0500

    initial release

---
+++ components/OssnSearch/css/search.php
@@ -0,0 +1,27 @@
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+.ossn-search-page{
+  width: 526px;
+  display: inline-table;
+  background: #FFF;
+  border: 1px solid;
+  border-color: #E5E6E9 #DFE0E4 #D0D1D5;
+  -webkit-border-radius: 3px;
+  float: right;	
+}
+.ossn-search-page .search-data {
+  padding: 12px;	
+}
+.ossn-search-page .ossn-view-users {
+  width: 100%;
+  display: block;
+  margin-bottom:10px;
+  margin-left:0px;
+}
