2be376d297226cc00ad83d009bf3233788ed7635
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:59:04 +0500

    initial release

---
+++ system/start.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * OpenSource-SocialNetwork
+ *
+ * @package   OpenSource-SocialNetwork
+ * @author    Informatikon Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.informatikon.com/
+ */
+session_start();
+
+global $Ossn;
+if (!isset($Ossn)) {
+	$Ossn = new stdClass;
+} 
+
+include_once(dirname(dirname(__FILE__)).'/libraries/ossn.lib.route.php');
+
+if(!is_file(ossn_route()->configs.'ossn.config.site.php') && !is_file(ossn_route()->configs.'ossn.config.db.php')){
+    header("Location: installation");
+}
+include_once(ossn_route()->configs.'libraries.php');
+include_once(ossn_route()->configs.'classes.php');
+
+include_once(ossn_route()->configs.'ossn.config.site.php');
+include_once(ossn_route()->configs.'ossn.config.db.php');
+
+foreach($Ossn->classes as $class){
+     if(!include_once(ossn_route()->classes."Ossn{$class}.php")){
+		 throw new exception('Cannot include all classes'); 
+	 }
+}
+foreach($Ossn->libraries as $lib){
+     if(!include_once(ossn_route()->libs."Ossn.lib.{$lib}.php")){
+		 throw new exception('Cannot include all libraries'); 
+	 }
+}
+ossn_trigger_callback('ossn', 'init');
+//need to update user last_action 
+// @note why its here?
+update_last_activity();
