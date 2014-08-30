b63d62d01249f52e74642afed26d9b7bb6d7841c
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:27:28 +0500

    initial release

---
+++ components/OssnComments/forms/entity/comment_add.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+  $object = $params['object'];
+  ?>
+      <input type="text" name="comment" id="comment-box-<?php echo $object;?>" class="comment-box" placeholder="<?php echo ossn_print('write:comment'); ?>" />
+      <input type="hidden" name="entity" value="<?php echo $object;?>" />
+
+          
+      
