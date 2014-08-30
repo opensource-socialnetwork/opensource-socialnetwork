dd5af81355ff9a32b2cf1d9edc932f7f592a9f20
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:43:34 +0500

    initial release

---
+++ components/OssnWall/wall/group.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+echo '<div class="ossn-wall-container">';
+echo ossn_view_form('group/container', array(
+							   'action' => ossn_site_url().'action/wall/post/g',
+							   'component' => 'OssnWall',
+							   'params' => array('group' => $params['group']),
+							    ), false);
+echo '</div>';
+echo '<div class="user-activity">';
+
+$posts = new OssnWall;
+$posts->type = 'group';
+$posts = $posts->GetPostByOwner($params['group']->guid);
+
+$Pagination = new OssnPagination;
+$Pagination->setItem($posts);
+$posts = $Pagination->getItem();
+foreach($posts as $post){ 
+   $data = json_decode(html_entity_decode($post->description));
+   $text = $data->post;
+   $location = '';
+   if(isset($data->location)){
+   $location = '- '.$data->location;
+   }
+   $user = ossn_user_by_guid($post->poster_guid); 
+   echo ossn_view('components/OssnWall/templates/group-activity-item', array(
+																	'post' => $post,
+																	'friends' =>  explode(',' ,$data->friend),
+																	'text' => $text,
+																	'location' => $location,
+																	'user' => $user,
+															 ));  
+
+   } 
+echo $Pagination->pagination();
+
+echo '</div>';
