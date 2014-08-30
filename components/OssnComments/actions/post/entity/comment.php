02662d41de10411f704c616a393ca2cb3df2c309
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:27:19 +0500

    initial release

---
+++ components/OssnComments/actions/post/entity/comment.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+$OssnComment = new OssnComments;
+if($OssnComment->PostComment(input('entity'),  ossn_loggedin_user()->guid, input('comment'), 'entity')){
+  $data['comment']['id'] = $OssnComment->getCommentId();
+  $data['comment']['owner_guid'] = ossn_loggedin_user()->guid;
+  $data['comment']['value'] = input('comment');
+  $data['comment']['time_created'] = time();
+  echo ossn_view('components/OssnComments/templates/comment', $data);
+  if(!ossn_is_xhr()){
+    redirect(REF);	
+  }
+}
