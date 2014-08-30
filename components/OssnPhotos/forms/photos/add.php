03d74570143af4b584c2e4e63ccde74973d798ce
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:39:09 +0500

    initial release

---
+++ components/OssnPhotos/forms/photos/add.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+?>
+<label><?php echo ossn_print('photo:select');?></label>
+<input type="file" name="ossnphoto" />
+<input type="submit" class="ossn-hidden" id="ossn-photos-submit"/>
+<?php
+  // Shouldn't album privacy applied on photos? $dev.arsalan
+  //echo ossn_view('system/templates/privacy');
+?>
