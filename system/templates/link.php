44756f228cfa12a21b9da6ede1896a0386836476
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:59:08 +0500

    initial release

---
+++ system/templates/link.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+$attributes = ossn_args($params);
+$text = $params['text'];
+unset($params['text']);
+
+echo "<a {$attributes}>{$text}</a>";
