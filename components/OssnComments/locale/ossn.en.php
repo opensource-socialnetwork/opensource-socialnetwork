a9c36c9055a018d0c9f226982ab1f99ad6e20995
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:27:36 +0500

    initial release

---
+++ components/OssnComments/locale/ossn.en.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+$en = array(
+			'write:comment' => 'Write a comment...',
+			'like' => 'Like',
+			'unlike' => 'Unlike'
+	  );
+ossn_register_languages('en', $en); 
