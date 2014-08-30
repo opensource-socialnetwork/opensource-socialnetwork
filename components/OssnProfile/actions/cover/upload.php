fec42ecbca6faca5eb27c284ebdaeb2ce024ae93
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:41:11 +0500

    initial release

---
+++ components/OssnProfile/actions/cover/upload.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+$file = new OssnFile;
+$file->owner_guid = ossn_loggedin_user()->guid;;
+$file->type = 'user';
+$file->subtype = 'profile:cover';
+$file->setFile('coverphoto');
+$file->setPath('profile/cover/');
+if($file->addFile()){
+   echo 1;
+} else {
+	 echo 0;
+} 
