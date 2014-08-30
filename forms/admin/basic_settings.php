19c13ae2facd90c60db14a87c881fc64c9d768f5
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:43:48 +0500

    initial release

---
+++ forms/admin/basic_settings.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+$setting = new OssnSite;
+$setting = $setting->getAllSettings();
+?>
+<label><?php echo ossn_print('website:name');?></label>
+<input type='text' name="sitename"  value="<?php echo $setting->site_name;?>" placeholder='Website Name'/>
+<label><?php echo ossn_print('owner:email');?></label>
+<input type='text' name="owneremail" value="<?php echo $setting->owner_email;?>" placeholder='Owner Email'/>
+
+<label><?php echo ossn_print('default:lang');?></label>
+<select name="sitelang">
+<?php foreach(ossn_locales() as $lang){ 
+if($lang == $setting->language){ $select = 'selected'; }
+?>
+<option class="option"value="<?php echo $lang;?>" <?php echo $select;?>><?php echo ossn_print($lang);?></option>
+
+<?php } ?>
+</select>
+<input type="submit" class="ossn-admin-button button-dark-blue" value="<?php echo ossn_print('save');?>"/>
