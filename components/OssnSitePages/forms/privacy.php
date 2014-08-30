687ad35bb670efb868a73e885ea59123c334ca95
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:42:40 +0500

    initial release

---
+++ components/OssnSitePages/forms/privacy.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+$OssnSitePages = new OssnSitePages;
+$OssnSitePages->pagename = 'privacy';
+$OssnSitePages = $OssnSitePages->getPage();
+?>
+<label><?php echo ossn_print('site:privacy');?></label>
+<textarea name="pagebody" class="ossn-editor"><?php echo html_entity_decode($OssnSitePages->description); ?></textarea><br />
+<input type="submit" class="ossn-admin-button button-dark-blue" value="<?php echo ossn_print('save');?>"/>
