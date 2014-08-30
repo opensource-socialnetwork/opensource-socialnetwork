ff3e05e55949c4d4bdb4e62af6fbb52280bad41d
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:39:06 +0500

    initial release

---
+++ components/OssnPhotos/css/photos.php
@@ -0,0 +1,53 @@
+.ossn-profile-modlue-albums img {
+  padding: 1.5px;	
+}
+.ossn-profile-modlue-albums h3 {
+ padding:4px;
+ text-align:center;
+ color:#ccc;
+}  
+.ossn-photos {
+   	
+}
+.ossn-photos li {
+ display:inline-block;
+ margin-right: 5px;
+ border:1px solid #ccc;
+ 
+ min-height:200px;
+ width:200px;
+
+}
+.ossn-album-name {
+ text-align: left;
+ font-weight: bold;
+ padding: 3px;
+ margin-bottom: 3px;
+}
+
+.ossn-photos {
+   	
+}
+.ossn-photos li img{
+ border:1px solid #ccc;
+}
+.ossn-photos h2 {
+ text-align:center;	
+}
+.ossn-photo-view a{
+ margin-top:-7px;
+ float:left;
+}
+.ossn-photo-viewer {
+ text-align:center;
+ background:#F6F6F6;
+ width:736px;
+}
+.ossn-viewer-comments {
+ margin-top: 25px;	
+}
+.ossn-viewer-comments .comments-likes textarea, 
+.ossn-viewer-comments .comments-likes .comment-text,
+.ossn-viewer-comments .comments-likes input[type='text'] {
+  width:245px;	
+}
