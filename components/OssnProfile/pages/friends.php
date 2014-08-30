735db31e7451a6eb09ad306c1808c7494ddf14b4
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:41:42 +0500

    initial release

---
+++ components/OssnProfile/pages/friends.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+echo '<div style="margin-left:3px;">';
+$users['users'] = get_user_friends($params['user']->guid);
+echo ossn_view("system/templates/users", $users);
+echo '</div>';
