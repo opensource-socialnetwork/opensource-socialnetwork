1f621224e6fe6e81eedb731278d1ce415321213d
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:42:19 +0500

    initial release

---
+++ components/OssnSearch/pages/search.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+?>
+<div class="ossn-search-page">
+<div class="search-data">
+<?php
+echo $params['contents'];
+?>
+</div>
+</div>
