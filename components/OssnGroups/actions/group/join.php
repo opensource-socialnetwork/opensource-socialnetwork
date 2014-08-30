d227e24bccb3b34c13e9dd2ff0b268406ac117ea
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:28:01 +0500

    initial release

---
+++ components/OssnGroups/actions/group/join.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+$add = new OssnGroup;
+$group = input('group');
+if(empty($group)) {
+     ossn_trigger_message(ossn_print('member:add:error'), 'error');	  		
+	 redirect(REF);
+}
+if($add->sendRequest(ossn_loggedin_user()->guid, $group)){
+  ossn_trigger_message(ossn_print('memebership:sent'), 'error');	  			
+  redirect("group/{$group}");	
+} else {
+  ossn_trigger_message(ossn_print('memebership:sent:fail'), 'error');	  			
+  redirect(REF);	
+}
