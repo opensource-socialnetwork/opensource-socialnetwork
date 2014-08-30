9211b0323c4ef939bd5ab574f5b89e49d1e6fd7e
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:43:08 +0500

    initial release

---
+++ components/OssnWall/forms/home/container.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+ ?>
+<div class="tabs-input">
+ <div class="wall-tabs">
+    <div class="item">
+      <div class="ossn-wall-status"></div> 
+      <div class="text"><?php echo ossn_print('post');?></div>
+     </div>
+ </div> 
+ <textarea placeholder="<?php echo ossn_print('wall:post:container');?>" name="post"></textarea>
+ <div id="ossn-wall-friend" style="display:none;">
+ <input type="text" placeholder="<?php echo ossn_print('tag:friends');?>" name="friends" id="ossn-wall-friend-input"/>
+ </div>
+ <div id="ossn-wall-location" style="display:none;">
+ <input type="text" placeholder="<?php echo ossn_print('enter:location');?>" name="location" id="ossn-wall-location-input"/>
+</div>
+ <div id="ossn-wall-photo" style="display:none;">
+  <input type="file" name="ossn_photo"/>
+ </div>
+</div>
+ <div class="controls">
+  <li>
+    <div class="ossn-wall-friend"></div>
+  </li>
+   <li>
+    <div class="ossn-wall-location"></div>
+  </li>  
+  <li>
+    <div class="ossn-wall-photo"></div>  
+  </li>
+  <div style="float:right;">
+      <input class="ossn-button-submit-b ossn-wall-post" type="submit" value="<?php echo ossn_print('post');?>" />
+  </div>
+ </div>
+       <input type="hidden" value="<?php echo $params['user']->guid;?>" name="wallowner" />
+      <input type="hidden" name="wall" value="<?php echo rand();?>"/>   
