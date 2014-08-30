0a7a3e7a39fb3b5c4f54edb780c46894ecddfaa4
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:32:20 +0500

    initial release

---
+++ components/OssnLikes/actions/post/unlike.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+$OssnLikes = new OssnLikes;
+$anotation = input('post');
+$entity = input('entity');
+if(!empty($anotation)){
+	 $subject = $anotation;
+	 $type = 'post';
+}
+if(!empty($entity)){
+     $subject = $entity;	
+	 $type = 'entity';
+
+}
+
+if($OssnLikes->UnLike($subject, ossn_loggedin_user()->guid, $type)){
+   if(!ossn_is_xhr()){
+    redirect(REF);	
+   } else {
+     header('Content-Type: application/json'); 	   
+	 echo json_encode(array(
+							'done' => 1,
+							'button' => 'Like',
+							));   
+   }
+} 
+else {
+ 	  if(!ossn_is_xhr()){
+	    redirect(REF);   
+       } else {
+	    header('Content-Type: application/json'); 	   
+        echo json_encode(array(
+							'done' => 0,
+							'button' => 'Unlike',
+							));    
+       }
+}
+
+exit;
