db5cb1b2bc475e6a35e091830ad71dfc92e1d169
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:43:57 +0500

    initial release

---
+++ forms/admin/theme_installer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+ ?>
+<input type="file" name="theme_file"/>
+<input type="submit" class="ossn-admin-button button-dark-blue" value="<?php echo ossn_print('upload');?>"/>
+<div class="ossn-com-install-notice">
+  <?php echo ossn_print('theme:install:notice'); ?>
+</div>
