ae2c716307f736fbf6cae037a395c28d6199423b
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:43:17 +0500

    initial release

---
+++ components/OssnWall/locale/ossn.en.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+$en = array(
+			'post:created' => 'Successfully created Post',
+			'post:create:error' => 'Cannot create post',
+			'post' => 'Post',
+			'enter:location' => 'Enter Location',
+			'tag:friends' => 'Tag Friends',
+			'wall:post:container' => "What's on your mind?",
+			'post' => 'Post',
+			'post:view' => 'Post View',
+	  );
+ossn_register_languages('en', $en); 
