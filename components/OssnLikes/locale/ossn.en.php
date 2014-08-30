cba09986ee9816b1ac5edfd1401c1077e841ee47
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:32:28 +0500

    initial release

---
+++ components/OssnLikes/locale/ossn.en.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+$en = array(
+	   'people:like:this' => 'People Who like this post',
+	  );
+ossn_register_languages('en', $en); 
