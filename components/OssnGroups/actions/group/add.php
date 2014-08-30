5d9ea4623f43285e51b6c14152fbe9333f1174f0
Arsalan Shah <manactric@gmail.com>
2014-08-30 13:27:56 +0500

    initial release

---
+++ components/OssnGroups/actions/group/add.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+$add = new OssnGroup;
+$params['owner_guid'] = ossn_loggedin_user()->guid;
+$params['name'] = input('groupname');
+$params['description'] = input('description');
+$params['privacy'] = input('privacy');
+if($add->createGroup($params)){
+  ossn_trigger_message(ossn_print('group:added'), 'success');	  	 
+  redirect("group/{$add->getGuid()}");	
+} else {
+  ossn_trigger_message(ossn_print('group:add:fail'), 'error');	  	 	
+  redirect(REF);	
+}
