57d159ff4057d5069dc659c5f789ba033ed465ab
Arsalan Shah <manactric@gmail.com>
2014-08-30 16:58:45 +0500

    initial release

---
+++ pages/contents/error.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+ ?>
+<div class="ossn-error-page">
+         <div class="error-heading"><?php echo ossn_print('page:error:heading');?></div>
+         <div class="error-text"><?php echo ossn_print('page:error:text');?></div>
+</div>
