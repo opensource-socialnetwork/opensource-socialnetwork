fbb2c10ed282ba96b1c215857ccae32d368dcc04
Arsalan Shah <manactric@gmail.com>
2014-08-31 23:35:31 +0500

    Update administrator.php

--- themes/default/style/administrator.php
+++ themes/default/style/administrator.php
@@ -1,3 +1,23 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+?>
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
 body, fieldset, table, textarea, input {
  margin: 0;
  padding: 0;
