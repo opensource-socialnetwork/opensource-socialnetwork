e5a0ee1340bb5c9ef3238b92ad581fbe622c1464
Arsalan Shah <manactric@gmail.com>
2014-09-02 20:12:39 +0500

    release 1.2

--- components/OssnAds/css/ads.php
+++ components/OssnAds/css/ads.php
@@ -1,29 +1,32 @@
 .ossn-ad-tiem-small {
-	margin-bottom:20px;
+  margin-bottom:20px;
 }
 .ossn-ad-tiem-small img{
-	display:block;
+  display:block;
 }
 .ossn-ad-tiem-small .descript{
-	display:block;
-	margin-top:5px;
+  display:block;
+  margin-top:5px;
 }
 .ad-heading {
  width:127px;	
 }
 .ad-heading a{
-	font-size:12px;
-	font-weight:bold;
+ font-size:12px;
+ font-weight:bold;
 }
 .ossn-ad-tiem-small .ossn-ads-link {
-font-size: 11px;
-margin-bottom: 5px;
-margin-top: 3px;
-width: 100px;
+ font-size: 11px;
+ margin-bottom: 5px;
+ margin-top: 3px;
+ width: 100px;
 }
 .ossn-ads .sponsered {
  margin-top: 8px;
  margin-bottom: 5px;
  font-weight: bold;
  color: #999;	
+}
+.ossn-ads-small { 
+ width: 125px !important;
 }
