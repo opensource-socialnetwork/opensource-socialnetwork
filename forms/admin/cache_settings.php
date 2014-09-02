2a0af76d0f4369b2ae59a52fdc8d130e167ed375
Arsalan Shah <manactric@gmail.com>
2014-09-02 20:19:25 +0500

    release 1.2

--- forms/admin/cache_settings.php
+++ forms/admin/cache_settings.php
@@ -8,18 +8,20 @@
  * @license   General Public Licence http://www.opensocialwebsite.com/licence 
  * @link      http://www.opensocialwebsite.com/licence
  */
-$cache = ossn_site_settings('site_name');
+$cache = ossn_site_settings('cache');
 if($cache == 1){
- $enabled = 'selected';	
-} else {
+  $enabled = 'selected';
+} elseif($cache == 0){
   $disabled = 'selected';
 }
 ?>
+<h3> Status : <?php echo ossn_print("cache:{$cache}"); ?> </h3>
+
 <select name="cache">
 <option value="1" <?php echo $enabled;?>> <?php echo ossn_print('cache:enable');?> </option>
 <option value="0" <?php echo $disabled;?>> <?php echo ossn_print('cache:disable');?>  </option>
 </select>
 <input type="submit" class="ossn-admin-button button-dark-blue" value="<?php echo ossn_print('save');?>"/>
 <div class="ossn-com-install-notice">
-  <?php echo ossn_print('cachel:notice'); ?>
+  <?php echo ossn_print('cache:notice'); ?>
 </div>
