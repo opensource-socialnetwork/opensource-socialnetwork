028472eb19f96e52da5f329799eba71f341bde24
Arsalan Shah <manactric@gmail.com>
2014-09-02 22:22:21 +0500

    release 1.2

--- components/OssnComments/ossn_com.php
+++ components/OssnComments/ossn_com.php
@@ -10,7 +10,8 @@
  */
 define('__OSSN_COMMENTS__', ossn_route()->com.'OssnComments/');
 require_once(__OSSN_COMMENTS__.'classes/OssnComments.php');
-ossn_extend_view('js/opensource.socialnetwork', 'components/OssnLikes/js/ossn_likes');
+function ossn_comments(){
+
 ossn_register_action('post/comment', __OSSN_COMMENTS__.'actions/post/comment.php');
 ossn_register_action('post/entity/comment', __OSSN_COMMENTS__.'actions/post/entity/comment.php');
 
@@ -19,6 +20,7 @@ ossn_add_hook('post', 'comments:entity', 'ossn_post_comments_entity');
 
 ossn_extend_view('js/opensource.socialnetwork', 'components/OssnComments/js/OssnComments');
 
+}
 function ossn_post_comments($h, $t, $r, $p){
  return ossn_view('components/OssnComments/post/comments', $p);	
 }
@@ -30,3 +32,4 @@ function ossn_post_comments_delete($event, $type, $params){
 	$delete = new OssnComments;
 	$delete->commentsDeleteAll($params);
 }
+ossn_register_callback('ossn', 'init', 'ossn_comments');
