d40374f882780ed4497a076e63d45785d5720540
Arsalan Shah <manactric@gmail.com>
2014-09-02 20:19:10 +0500

    release 1.2

--- components/OssnWall/pages/view.php
+++ components/OssnWall/pages/view.php
@@ -15,13 +15,33 @@ echo '<div class="user-activity">';
      if(isset($data->location)){
       $location = '- '.$data->location;
      }
+	 if(isset($post->{'file:wallphoto'})){
+     $image = str_replace('ossnwall/images/', '', $post->{'file:wallphoto'});
+      } else {
+	   unset($image);    
+     } 
 	 $user = ossn_user_by_guid($params['post']->poster_guid); 
+	 if($params['post']->type == 'user'){
 	 echo ossn_view('components/OssnWall/templates/activity-item', array(
+																	'post' => $params['post'],
+																	'text' =>  $text,
+																	'friends' =>  explode(',' ,$data->friend),
+																	'location' => $location,
+																	'user' => $user,
+																	'image' => $image,
+																		 
+																		 )); 
+	 }
+	 if($params['post']->type == 'group'){
+		 echo ossn_view('components/OssnWall/templates/group-activity-item', array(
 																	'post' => $params['post'],
 																	'text' =>  $text,
 																	'location' => $location,
 																	'user' => $user,
-																		 
-																		 )); 
+																	'image' => $image,
+																	'show_group' => true,																	
+																		 )); 	 
+
+	 }
 
 echo '</div>';
