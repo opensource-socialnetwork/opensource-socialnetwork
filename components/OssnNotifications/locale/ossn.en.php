a4ca8a5fae6f029dab1309900a6809695126f194
Arsalan Shah <manactric@gmail.com>
2014-09-08 22:27:15 +0500

    refactory

--- components/OssnNotifications/locale/ossn.en.php
+++ components/OssnNotifications/locale/ossn.en.php
@@ -10,14 +10,16 @@
  */
 
 $en = array(
-	'ossn:notifications:comments:post' => "%s commeted on the post",
+	'ossn:notifications:comments:post' => "%s commented on the post",
 	'ossn:notifications:like:post' => "%s liked your post",
 	'ossn:notifications:like:annotation' => "%s liked your comment",
 	'ossn:notifications:like:entity:file:ossn:aphoto' => "%s liked your photo",
 	'ossn:notifications:comments:entity:file:ossn:aphoto' => '%s commented on your photo',
 	'ossn:notifications:wall:friends:tag' => '%s tag you in a post',
 	'ossn:notification:are:friends' => 'You are now friends',
-	'ossn:notifications:comments:post:group:wall' => "%s commeted on the group post",
+	'ossn:notifications:comments:post:group:wall' => "%s commented on the group post",
+	'ossn:notifications:like:entity:file:profile:photo' => "%s commented the profile photo",
+	'ossn:notifications:comments:entity:file:profile:photo' => "%s liked the profile photo",
 		
 	'ossn:notification:are:friends' => 'You are now friends',
 	'ossn:notification:delete:friend' => 'Friend request deleted',
