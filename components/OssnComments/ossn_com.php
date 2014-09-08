2738606df8561c3e6caa55a97844f778b30968fc
Arsalan Shah <manactric@gmail.com>
2014-09-08 22:26:36 +0500

    refactory

--- components/OssnComments/ossn_com.php
+++ components/OssnComments/ossn_com.php
@@ -10,26 +10,48 @@
  */
 define('__OSSN_COMMENTS__', ossn_route()->com.'OssnComments/');
 require_once(__OSSN_COMMENTS__.'classes/OssnComments.php');
+/**
+* Initialize Comments Component
+*
+* @return void;
+* @access private
+*/
 function ossn_comments(){
-if(ossn_isLoggedin()){
-		
-ossn_register_action('post/comment', __OSSN_COMMENTS__.'actions/post/comment.php');
-ossn_register_action('post/entity/comment', __OSSN_COMMENTS__.'actions/post/entity/comment.php');
+ if(ossn_isLoggedin()){	
+  ossn_register_action('post/comment', __OSSN_COMMENTS__.'actions/post/comment.php');
+  ossn_register_action('post/entity/comment', __OSSN_COMMENTS__.'actions/post/entity/comment.php');
+ }
+  ossn_add_hook('post', 'comments', 'ossn_post_comments');
+  ossn_add_hook('post', 'comments:entity', 'ossn_post_comments_entity');
 
+  ossn_extend_view('js/opensource.socialnetwork', 'components/OssnComments/js/OssnComments');
+ 
+  ossn_register_callback('post', 'delete', 'ossn_post_comments_delete');
 }
-ossn_add_hook('post', 'comments', 'ossn_post_comments');
-ossn_add_hook('post', 'comments:entity', 'ossn_post_comments_entity');
-
-ossn_extend_view('js/opensource.socialnetwork', 'components/OssnComments/js/OssnComments');
-
+/**
+* View comments bar on wall posts
+*
+* @return mix data;
+* @access private
+*/
+function ossn_post_comments($hook, $type, $return, $params){
+ return ossn_view('components/OssnComments/post/comments', $params);	
 }
-function ossn_post_comments($h, $t, $r, $p){
- return ossn_view('components/OssnComments/post/comments', $p);	
+/**
+* View comments bar on entity
+*
+* @return mix data;
+* @access private
+*/
+function ossn_post_comments_entity($hook, $type, $return, $params){
+ return ossn_view('components/OssnComments/post/comments_entity', $params);	
 }
-function ossn_post_comments_entity($h, $t, $r, $p){
- return ossn_view('components/OssnComments/post/comments_entity', $p);	
-}
-ossn_register_callback('post', 'delete', 'ossn_post_comments_delete');
+/**
+* Delete post comments
+*
+* @return voud;
+* @access private
+*/
 function ossn_post_comments_delete($event, $type, $params){
 	$delete = new OssnComments;
 	$delete->commentsDeleteAll($params);
