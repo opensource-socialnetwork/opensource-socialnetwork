448151649c0cc4b8037690505a875106fe28535f
Arsalan Shah <manactric@gmail.com>
2014-09-08 22:28:29 +0500

    update view.php

--- components/OssnLikes/pages/view.php
+++ components/OssnLikes/pages/view.php
@@ -9,8 +9,13 @@
  * @link      http://www.opensocialwebsite.com/licence
  */
 echo '<div class="ossn-likes-view">';
- $likes = new OssnLikes;
-  foreach($likes->GetLikes(input('post')) as $us){ 
+  $likes = new OssnLikes;
+  $guid = input('guid');
+  $type = input('type');
+  if(empty($type)){
+	 $type = 'post';  
+  }
+  foreach($likes->GetLikes($guid, $type) as $us){ 
     if($us->guid !== ossn_loggedin_user()->guid){ 
  	$users[] = ossn_user_by_guid($us->guid);
 	}
