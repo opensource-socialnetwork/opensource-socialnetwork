c3cd07658e300ccca7bcc00d90e299e495388a7b
Arsalan Shah <manactric@gmail.com>
2014-09-12 23:43:22 +0500

    ossnblock #42

---
+++ components/OssnBlock/locale/ossn.en.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+$en = array(
+			'user:blocked' => 'User has been blocked',
+			'user:block:error' => 'Cannot block user please try again later',
+			'user:block' => 'Block',
+			'user:unblock' => 'Unblock',
+	  );
+ossn_register_languages('en', $en); 
