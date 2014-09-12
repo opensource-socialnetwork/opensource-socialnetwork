7eb9a74ef5b98134c2d0af9a33bc28ca9c35b056
Arsalan Shah <manactric@gmail.com>
2014-09-12 23:43:08 +0500

    ossnblock #42

---
+++ components/OssnBlock/actions/user/unblock.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+$block = new OssnBlock;
+$user = input('user');
+if($block->removeBlock(ossn_loggedin_user()->guid, $user)){
+    ossn_trigger_message(ossn_print('user:blocked'), 'success');
+	redirect(REF);	
+} else {
+    ossn_trigger_message(ossn_print('user:block:error'), 'error');
+	redirect(REF);	   
+}
