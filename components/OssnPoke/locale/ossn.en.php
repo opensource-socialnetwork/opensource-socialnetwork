2685aaf18fae3fa77863fc50439eff230237ad47
Arsalan Shah <manactric@gmail.com>
2014-09-12 23:45:38 +0500

    ossnpoke #42

---
+++ components/OssnPoke/locale/ossn.en.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * OpenSocialWebsite
+ *
+ * @package   OpenSocialWebsite
+ * @author    Open Social Website Core Team <info@opensocialwebsite.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensocialwebsite.com/licence 
+ * @link      http://www.opensocialwebsite.com/licence
+ */
+$en = array(
+			'ossn:notifications:ossnpoke:poke' => "%s has poked you",
+			'user:poked' => 'You have poked user',
+			'user:poke:error' => 'Cannot poke please try again later',
+	  );
+ossn_register_languages('en', $en); 
