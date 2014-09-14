2f461416c739d60cf828d870336cbf3a40e43b9a
Arsalan Shah <manactric@gmail.com>
2014-09-14 00:40:22 -0700

    ossncomments add ajax likes #52

---
+++ components/OssnLikes/css/likes.php
@@ -0,0 +1,79 @@
+/**********
+Wall
+*********/
+.comments-likes {
+ background:#F6F7F8;
+ min-height:50px;
+ width:100%;
+ /*border-top: 1px solid #E1E2E3;*/
+}
+.comments-like-comment-links {
+  background:#fff;
+  margin-top: -10px;
+}
+.comments-likes .poster-image {
+ padding: 10px;
+ display:inline-table;
+}
+.comments-likes .comment-container {
+ display:inline-table;
+}
+.comments-likes .comment-text {
+ display: inline-table;
+ vertical-align: top;
+ margin-top: 10px;
+ margin-left: -5px;
+ font-size: 12px;
+ width: 455px;
+ margin-bottom: 6px;
+}
+.comments-likes .like_share {
+padding: 10px;
+border-bottom: 1px solid #EEE;
+font-size: 12px;
+}
+
+.dot-comments {
+ vertical-align: top;
+ margin: -3px 2px 0 0px;
+ display: inline-block;
+}
+.dot-likes {
+ vertical-align: top;
+ margin: -1px 5px 0 0px;
+ display: inline-block;
+}
+.comments-likes textarea,
+.comments-likes input[type='text'] {
+float: right;
+border: 1px solid #D3D6DB;
+padding: 7px;
+height: 16px;
+margin-top: -32px;
+margin-left: -7px;
+width: 452px;
+resize:none;
+}
+.comment-text p {
+ margin-top: 1px;
+ line-height: 15px;
+}
+.comment-metadata {
+margin-top: -9px;
+color:#9B9B9B;
+}
+.comments-item {
+margin-bottom: -11px;
+}
+
+.comments-likes .like_share .button-container,
+.comments-likes .like_share a{
+ display:inline-block;
+}
+.ossn-like-icon {
+  background:url('<?php echo ossn_site_url();?>components/OssnLikes/images/like.png') no-repeat;
+  width:14px;
+  height:15px;
+  margin-right:3px;
+  display:inline-block;
+}
