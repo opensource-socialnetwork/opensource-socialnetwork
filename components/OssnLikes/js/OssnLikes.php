d5dd6f923236921fa2050b3dbe85bc26f2d7b1b9
Arsalan Shah <manactric@gmail.com>
2014-09-14 00:40:29 -0700

    ossncomments add ajax likes #52

--- components/OssnLikes/js/OssnLikes.php
+++ components/OssnLikes/js/OssnLikes.php
@@ -91,3 +91,50 @@ Ossn.EntityLike = function(entity){
 					 }); 
 
 };
+Ossn.RegisterStartupFunction(function(){
+   $(document).ready(function(){
+	     $(document).delegate('.ossn-like-comment', 'click', function(e){
+                e.preventDefault();
+                var $item = $(this); 
+                var $type = $.trim($item.text());
+                var $url = $item.attr('href');
+              	Ossn.PostRequest({
+					 url: $url,
+					 beforeSend: function(){
+					    $item.html('<img src="'+Ossn.site_url+'components/OssnComments/images/loading.gif" />');
+					 },
+					 callback:function(callback){
+                     if(callback == 1){
+                           $total_guid = Ossn.UrlParams('annotation', $url);
+                           $total = $('.ossn-total-likes-'+$total_guid).attr('data-likes');           
+                        if($type == 'Like'){
+                           $item.html('Unlike');
+                           var unlike = $url.replace("like", "unlike");
+                           $item.attr('href', unlike);
+                           $total_likes = $total;
+                           $total_likes++;
+                           $('.ossn-total-likes-'+$total_guid).attr('data-likes', $total_likes);
+                           $('.ossn-total-likes-'+$total_guid).html('<span class="dot-likes">.</span><div class="ossn-like-icon"></div>'+$total_likes);
+                        }
+                        if($type == 'Unlike'){
+                           $item.html('Like');
+                           var like = $url.replace("unlike", "like");
+                           $item.attr('href', like);
+                           if($total > 1){
+                              $like_remove = $total;0
+                              $like_remove--;
+                              $('.ossn-total-likes-'+$total_guid).attr('data-likes', $like_remove); 
+                              $('.ossn-total-likes-'+$total_guid).html('<span class="dot-likes">.</span><div class="ossn-like-icon"></div>'+$like_remove);
+                           }  
+                            if($total == 1){
+                               $('.ossn-total-likes-'+$total_guid).attr('data-likes', 0); 
+                               $('.ossn-total-likes-'+$total_guid).html('');
+
+                            }
+                        }
+                      }
+                     },
+					 });    
+         }); 
+	});
+});
