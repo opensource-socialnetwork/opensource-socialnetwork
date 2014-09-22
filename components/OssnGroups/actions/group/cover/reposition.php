b0c2ea81743d19315302e8cf97e17c3e99a34d97
Arsalan Shah <manactric@gmail.com>
2014-09-22 22:19:56 +0500

    1.4 release

---
+++ components/OssnGroups/actions/group/cover/reposition.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.org>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.org/licence 
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+header('Content-Type: application/json'); 
+$group = ossn_get_group_by_guid(input('group'));
+if($group->owner_guid !== ossn_loggedin_user()->guid){
+    exit;	
+}
+if($group->repositionCOVER($group->guid, input('top'), input('left'))){
+ $params = $group->coverParameters($group->guid);
+ echo json_encode(array(
+				  'top' => $params[0],
+				  'left' => $params[1]
+				  ));
+}
