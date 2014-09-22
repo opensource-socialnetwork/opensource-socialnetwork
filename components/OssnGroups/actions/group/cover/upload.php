89ad887bb7cf1bf9310200325fedbaaa5ec10211
Arsalan Shah <manactric@gmail.com>
2014-09-22 22:19:53 +0500

    1.4 release

---
+++ components/OssnGroups/actions/group/cover/upload.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-Type: application/json'); 
+$group = input('group');
+$group = ossn_get_group_by_guid($group);
+if(ossn_loggedin_user()->guid !== $group->owner_guid){
+	  echo json_encode(array(
+						   'type' => 0
+						   ));
+	  exit;
+} 
+if($group->UploadCover()){
+    	echo json_encode(array(
+						   'type' => 1,
+						   'url' => $group->coverURL(),
+						   ));
+} else {
+    	echo json_encode(array(
+						   'type' => 0
+						   ));
+}
