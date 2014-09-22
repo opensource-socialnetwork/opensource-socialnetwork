987e69272ea3da01be8993a3c1c1fdc120570a0c
Arsalan Shah <manactric@gmail.com>
2014-09-22 22:19:10 +0500

    1.4 release

---
+++ components/OssnPhotos/libraries/ossn.lib.albums.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   Open Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence 
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+function ossn_albums(){
+  $albums = new OssnAlbums;
+  return $albums;
+}
