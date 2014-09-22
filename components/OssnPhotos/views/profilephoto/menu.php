f68c615d63adf4d3a74d7befdee9fedbd73a2445
Arsalan Shah <manactric@gmail.com>
2014-09-22 22:19:32 +0500

    1.4 release

---
+++ components/OssnPhotos/views/profilephoto/menu.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+?>
+<div class="ossn-photo-menu">
+ <li><a href="<?php echo ossn_site_url("action/profile/photo/delete?id={$params->guid}");?>"> Delete Photo </a> </li>
+</div>
