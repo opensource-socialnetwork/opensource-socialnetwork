d7c90bf6dc400b53ae8f4d66346ee8727b7ee71e
Arsalan Shah <manactric@gmail.com>
2014-09-22 22:18:53 +0500

    1.4 release

---
+++ actions/user/resetlogin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+$user = input('email');
+if(empty($user)){
+ ossn_trigger_message(ossn_print('password:reset:email:required'), 'error');   
+ redirect(REF);    
+}
+$user = ossn_user_by_email($user);
+if($user->SendResetLogin()){
+	ossn_trigger_message(ossn_print('passord:reset:email:success'), 'success');   
+	redirect();  	
+} else {
+	ossn_trigger_message(ossn_print('passord:reset:fail'), 'error');   
+	redirect(REF);   	
+}
