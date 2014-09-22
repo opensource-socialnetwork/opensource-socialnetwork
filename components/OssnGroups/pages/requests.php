06e5faa1fc7166704d042c6f187f42ca35002728
Arsalan Shah <manactric@gmail.com>
2014-09-22 22:20:04 +0500

    1.4 release

--- components/OssnGroups/pages/requests.php
+++ components/OssnGroups/pages/requests.php
@@ -17,7 +17,7 @@ foreach($requests as $user){ ?>
 <div class="ossn-group-members">
   <img src="<?php echo ossn_site_url("avatar/{$user->username}/large");?>" width='100' height="100"/>
  <div class="uinfo">
-    <a class="userlink" href="<?php echo ossn_site_url();?>u/<?php echo $user->username;?>"><?php echo $user->fullname;?></a>
+    <a class="userlink" href="<?php echo $user->profileURL();?>"><?php echo $user->fullname;?></a>
  </div>
     <a href="<?php echo ossn_site_url();?>action/group/member/approve?group=<?php echo $params['group']->guid;?>&user=<?php echo $user->guid;?>" class='friendlink button-grey-light'><?php echo ossn_print('approve');?></a>
     <a href="<?php echo ossn_site_url();?>action/group/member/decline?group=<?php echo $params['group']->guid;?>&user=<?php echo $user->guid;?>" class='friendlink button-grey-light'><?php echo ossn_print('decline');?></a>
