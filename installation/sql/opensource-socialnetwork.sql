9f9fd3521241cba39f72d7171512d05008c20966
Arsalan Shah <manactric@gmail.com>
2014-09-22 22:55:11 +0500

    Update opensource-socialnetwork.sql

--- installation/sql/opensource-socialnetwork.sql
+++ installation/sql/opensource-socialnetwork.sql
@@ -175,7 +175,7 @@ INSERT INTO `ossn_site_settings` (`setting_id`, `name`, `value`) VALUES
 (3, 'language', 'en'),
 (4, 'cache', '0'),
 (5, 'owner_email', '<<owner_email>>'),
-(6, 'notification_email', '<<notification_email>>');
+(6, 'notification_email', '<<notification_email>>'),
 (7, 'upgrades', '["1410545706.php","1411396351.php"]'),
 (9, 'display_errors', 'off'); 
 
