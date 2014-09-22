6015bc83cd7ae7b3dc44517dc530f2c6cb56a3ea
Arsalan Shah <manactric@gmail.com>
2014-09-22 22:19:19 +0500

    1.4 release

--- components/OssnPhotos/pages/profile/photos/all.php
+++ components/OssnPhotos/pages/profile/photos/all.php
@@ -16,7 +16,7 @@ echo '<h2>'.ossn_print('profile:photos').'</h2>';
 foreach($photos as $photo){
 	$imagefile = str_replace('profile/photo/', '', $photo->value);
 	$image = ossn_site_url()."album/getphoto/{$params['user']->guid}/{$imagefile}?size=larger&type=1";
-	$view_url = ossn_site_url().'photos/user/view/'.$photo->guid."?type=1&image={$imagefile}";
+	$view_url = ossn_site_url().'photos/user/view/'.$photo->guid;
 	echo "<li><a href='{$view_url}'><img src='{$image}'  class='pthumb'/></a></li>";	
 }
 echo '</div>';
