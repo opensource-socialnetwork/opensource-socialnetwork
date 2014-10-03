25660e612041f7305d6ab8bd7d8c2afc211d1dcb
Arsalan Shah <manactric@gmail.com>
2014-10-03 22:59:20 +0500

    add missing tag

--- themes/ossnblack/menus/footer.php
+++ themes/ossnblack/menus/footer.php
@@ -11,6 +11,6 @@
 $menus = $params['menu'];
 foreach($menus as $menu){
 	foreach($menu as $text => $link){
-         echo "<a href='{$link}'>{$text}</>";
+         echo "<a href='{$link}'>{$text}</a>";
 	}
 }
