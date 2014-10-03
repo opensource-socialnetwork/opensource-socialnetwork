84e7b50294d5cacd5ba08d01e850c0c2a8948e9b
Arsalan Shah <manactric@gmail.com>
2014-10-03 22:53:48 +0500

    ossn.chat.1.0

---
+++ components/OssnChat/views/OssnChat/smilies/view.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 	OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence 
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+foreach(OssnChat::loadIcons() as $val => $Icon){
+  echo '<div class="ossn-chat-inline-table ossn-chat-item-smiles" title="'.$val.'" onClick=\'Ossn.ChatInsertSmile("'.$val.'",'.$params['tab'].');\'>'.$Icon.'</div>';	
+}
