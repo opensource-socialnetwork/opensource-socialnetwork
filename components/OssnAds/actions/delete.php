de58ef68ffc8b5dd16ab57d0d7d71f917d204636
Arsalan Shah <manactric@gmail.com>
2014-11-05 10:28:42 -0800

    delete ad

---
+++ components/OssnAds/actions/delete.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   Open Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$delete = new OssnAds;
+$entites = $_REQUEST['entites'];
+foreach($entites as $entity){
+   $entity = get_ad_entity((int)$entity);
+   if(empty($entity->guid)){
+ 	  ossn_trigger_message(ossn_print('ad:delete:fail'), 'error', 'admin');
+   } else {
+       if (!$delete->deleteAd($entity->guid)) {
+		ossn_trigger_message(ossn_print('ad:delete:fail'), 'error', 'admin');
+	   } else {
+		ossn_trigger_message(ossn_print('ad:deleted', array($entity->title)), 'success', 'admin');  
+	   }	   
+   }
+}
+
+redirect(REF);
