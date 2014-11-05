29a183d5ca92a0d1e6bc2dfca8ef2c03881445df
Arsalan Shah <manactric@gmail.com>
2014-11-05 10:29:22 -0800

    edit ossnad, check if image is avaiable for upload

--- components/OssnAds/classes/OssnAds.php
+++ components/OssnAds/classes/OssnAds.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  *    OpenSource-SocialNetwork
  *
@@ -25,7 +24,9 @@ class OssnAds extends OssnObject {
         $this->owner_guid = 1;
         $this->type = 'site';
         $this->subtype = 'ossnads';
-
+        if(empty($_FILES['ossn_ads']['tmp_name'])){
+			return false;
+		}
         if ($this->addObject()) {
             if (isset($_FILES['ossn_ads'])) {
                 $this->OssnFile->owner_guid = $this->getObjectId();
@@ -61,12 +62,62 @@ class OssnAds extends OssnObject {
         $this->subtype = 'ossnads';
         return $this->getObjectByOwner();
     }
-
+    /**
+     * Get ad entity
+     * 
+	 * @param (int) $guid ad guid
+	 *
+     * @return object;
+     */	
+    public function getAd($guid){
+		$this->object_guid = $guid;
+		return $this->getObjectById();
+	}
+    /**
+     * Delete ad
+     * 
+	 * @param (int) $ad ad guid
+	 *
+     * @return bool;
+     */	
     public function deleteAd($ad) {
         if ($this->deleteObject($ad)) {
             return true;
         }
         return false;
     }
+    /**
+     * Edit
+     * 
+	 * @param (array) $params Contain title , description and guid of ad
+	 *
+     * @return bool;
+     */		
+	public function EditAd($params){
+	   if(!empty($params['guid']) && !empty($params['title']) 
+		   && !empty($params['description']) && !empty($params['siteurl'])){
+		   $entity = get_ad_entity($params['guid']);
+		    $fields = array(
+			 'title',
+			 'description'
+		    );
+		    $data = array(
+			 $params['title'],
+			 $params['description']
+		    );		   
+		   if($this->updateObject($fields, $data, $entity->guid)){
+			  if(isset($_FILES['ossn_ads']) && $_FILES['ossn_ads']['size']  !== 0){
+				  $path  = $entity->getParam('file:ossnads');
+				  $replace_file = ossn_get_userdata("object/{$entity->guid}/{$path}");
+				  if(!empty($path)){
+					 $regen_image = ossn_resize_image($_FILES['ossn_ads']['tmp_name'], 2048, 2048);
+					 file_put_contents($replace_file, $regen_image); 
+				  }
+			  }
+			  return true; 
+		   }
+	   }
+	   return false;
+	}
 
 }//class
