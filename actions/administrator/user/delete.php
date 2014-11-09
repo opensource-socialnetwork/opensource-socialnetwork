60d97a33d668017a63cfdf8b682afecd5d31c712
Arsalan Shah <manactric@gmail.com>
2014-11-09 23:07:50 +0500

    delete user

---
+++ actions/administrator/user/delete.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *    OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+
+$guid = input('guid');
+$user = ossn_user_by_guid($guid);
+if($user && $user->guid !== ossn_loggedin_user()->guid){
+	if($user->deleteUser()){
+		ossn_trigger_message(ossn_print('admin:user:deleted'), 'success', 'admin');
+	} else {
+		ossn_trigger_message(ossn_print('admin:user:delete:error'), 'error', 'admin');		
+	}
+}
+redirect(REF);
