16aabe472e4a404899789a4194339ad7f7582342
Arsalan Shah <manactric@gmail.com>
2014-11-09 23:21:18 +0500

    add copyrights

--- pages/administrator/contents/users/list.php
+++ pages/administrator/contents/users/list.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ *    OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
 echo ossn_view_form('admin/users/list_search', array(
     'action' => ossn_site_url('administrator/users'),
     'class' => 'ossn-admin-form',
