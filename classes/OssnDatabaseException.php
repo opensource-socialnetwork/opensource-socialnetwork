c5d7d61ec73209de8ebbd99f068041f21da6f052
Arsalan Shah <manactric@gmail.com>
2014-11-30 00:31:44 +0500

    Add exception

---
+++ classes/OssnDatabaseException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *  OpenSource-SocialNetwork
+ *   
+ *  A generic parent class for Class exceptions
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+ 
+class OssnDatabaseException extends Exception {}
