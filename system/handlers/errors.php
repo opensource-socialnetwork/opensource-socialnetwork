3e672f495afe5ccb6ee78a39205c14ed0672d5d6
Arsalan Shah <manactric@gmail.com>
2014-11-30 00:34:44 +0500

    Add exception page

---
+++ system/handlers/errors.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+$email = ossn_site_settings('owner_email');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title><?php echo ossn_site_settings('site_name'); ?></title>
+    <link rel="stylesheet"
+          href="<?php echo ossn_site_url(); ?>themes/<?php echo ossn_site_settings('theme'); ?>/style/exception.css"
+          type="text/css"/>
+</head>
+<div class="ossn-exception-header">
+    <div class="inner">
+        <?php echo ossn_site_settings('site_name'); ?>
+    </div>
+</div>
+<div class="ossn-exception-handler">
+    <div class="container-inner">
+        <div class="ossn-logo"></div>
+        <div class="title"><?php echo ossn_print('ossn:exception:title', array($email)); ?></div>
+    </div>
+    <div class="ossn-exception-description"><?php echo $params['exception']; ?></div>
+
+</div>
+<body>
+</body>
+</html>
