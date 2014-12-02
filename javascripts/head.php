5d967fd18021b90d848c5e422c09269a2fa70049
Arsalan Shah <manactric@gmail.com>
2014-12-02 22:30:33 +0500

    Add config

--- javascripts/head.php
+++ javascripts/head.php
@@ -1,2 +1,22 @@
-Ossn.site_url = '<?php echo ossn_site_url();?>';
-Ossn.Init();
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   Open Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+ $timestamp = time();
+ $token = ossn_generate_action_token($timestamp);
+ 
+ $token = array(
+			'ossn_ts' => $timestamp,
+			'ossn_token' => $token,
+			);
+ $configs = array('token' => $token);
+?> 	
+	Ossn.site_url = '<?php echo ossn_site_url();?>';
+	Ossn.Config = <?php echo json_encode($configs);?>;
+	Ossn.Init();
