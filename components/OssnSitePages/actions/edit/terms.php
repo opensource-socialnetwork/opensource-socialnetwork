3437338ee8c378b307fd99de74ab1b0119a179d6
Arsalan Shah <manactric@gmail.com>
2014-12-12 23:05:02 +0500

    no need to add site/admin in trigger message

--- components/OssnSitePages/actions/edit/terms.php
+++ components/OssnSitePages/actions/edit/terms.php
@@ -12,10 +12,10 @@ $save = new OssnSitePages;
 $save->pagename = 'terms';
 $save->pagebody = input('pagebody');
 if ($save->SaveSitePage()) {
-    ossn_trigger_message(ossn_print('page:saved'), 'success', 'admin');
+    ossn_trigger_message(ossn_print('page:saved'), 'success');
     redirect(REF);
 } else {
-    ossn_trigger_message(ossn_print('page:save:error'), 'error', 'admin');
+    ossn_trigger_message(ossn_print('page:save:error'), 'error');
     redirect(REF);
 }
 
