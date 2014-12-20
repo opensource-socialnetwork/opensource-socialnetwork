36e3816f26ebd1e8bc550f1411119687e0e40adb
Arsalan Shah <manactric@gmail.com>
2014-12-20 14:09:41 +0500

    Add notification email
    Add missing notification emails from settings

--- actions/administrator/settings/save/basic.php
+++ actions/administrator/settings/save/basic.php
@@ -13,6 +13,8 @@ $settings = new OssnDatabase;
 $sitename = input('sitename');
 $owneremail = input('owneremail');
 $sitelang = input('sitelang');
+$notification_email = input('notification_email');
+
 $errors = input('errors');
 
 if (empty($sitename) || empty($owneremail) || empty($sitelang) || empty($errors)
@@ -23,6 +25,7 @@ if (empty($sitename) || empty($owneremail) || empty($sitelang) || empty($errors)
 ossn_site_setting_update('site_name', $sitename, 2);
 ossn_site_setting_update('lang', $sitelang, 3);
 ossn_site_setting_update('owner_email', $owneremail, 5);
+ossn_site_setting_update('notification_email', $notification_email, 6);
 
 //update errors settings
 $update['table'] = 'ossn_site_settings';
