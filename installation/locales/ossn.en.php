86d151c27154c870a3f615058a125cc4a57aacf6
Arsalan Shah <manactric@gmail.com>
2014-12-20 13:53:35 +0500

    add new translations

--- installation/locales/ossn.en.php
+++ installation/locales/ossn.en.php
@@ -30,7 +30,33 @@ $englsih = array(
     'notification_email' => 'Notification Email',
     'create:admin:account' => 'Create Admin Account',
     'ossn:setting:account' => 'Account settings',
-
+	
+	'data:directory:invalid' => 'Invalid data directoy or directory is not writeable',	
+	'data:directory:outside' => 'Data directory must outside',
+	'all:files:required' => 'All files are required please check your files',
+	
+	'ossn:install:php' => 'PHP ',
+	'ossn:install:old:php' => "You have old PHP " . PHP_VERSION . " You need PHP 5.3 or Higher",
+	
+	'ossn:install:mysqli' => 'MYSQLI ENABLED',
+	'ossn:install:mysqli:required' => 'MYSQLI PHP EXTENSION REQUIRED',
+	
+	'ossn:install:apache' => 'APACHE ENABLED',
+	'ossn:install:apache:required' => 'APACHE IS REQUIRED',
+	
+	'ossn:install:modrewrite' => 'MOD_REWRITE',
+	'ossn:install:modrewrite:required' => 'MOD_REWRITE REQUIRED',
+	
+	'ossn:install:curl' => 'PHP CURL',
+	'ossn:install:curl:required' => 'PHP CURL REQUIRED',
+	
+	'ossn:install:gd' => 'PHP GD LIBRARY',
+	'ossn:install:gd:required' => 'PHP GD LIBRARY REQUIRED',
+	
+	'ossn:install:config' => 'CONFIGURATION DIRECTORY WRITEABLE',
+	'ossn:install:config:error' => 'CONFIGURATION DIRECTORY IS NOT WRITEABLE',
+	
+	'ossn:install:next' => 'Next',
 );
 
 ossn_installation_register_languages($englsih);
