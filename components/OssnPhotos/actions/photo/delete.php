25ea078aded4a13394422ebde9cb5976605a0b32
Arsalan Shah <manactric@gmail.com>
2014-12-20 15:35:34 +0500

    Redirect back to album after delete  #131

--- components/OssnPhotos/actions/photo/delete.php
+++ components/OssnPhotos/actions/photo/delete.php
@@ -19,15 +19,15 @@ $photo = $delete->GetPhoto($delete->photoid);
 $owner = ossn_albums();
 $owner = $owner->GetAlbum($photo->owner_guid);
 
-if (($owner->owner_guid == ossn_loggedin_user()->guid) || ossn_isAdminLoggedin()) {
+if (($owner->album->owner_guid == ossn_loggedin_user()->guid) || ossn_isAdminLoggedin()) {
     if ($delete->deleteAlbumPhoto()) {
         ossn_trigger_message(ossn_print('photo:deleted:success'), 'success');
-        redirect();
+        redirect("album/view/{$owner->album->guid}");
     } else {
         ossn_trigger_message(ossn_print('photo:delete:error'), 'error');
-        redirect();
+        redirect("album/view/{$owner->guid}");
     }
 } else {
     ossn_trigger_message(ossn_print('photo:delete:error'), 'error');
-    redirect();
+    redirect(REF);
 }
