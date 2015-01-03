9738ab56b64f135c9e7306199d2a81d611a53b9c
Arsalan Shah <manactric@gmail.com>
2015-01-04 00:46:39 +0500

    display groups I'm a member of in left column #155

--- components/OssnGroups/libraries/groups.php
+++ components/OssnGroups/libraries/groups.php
@@ -8,28 +8,56 @@
  * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
  * @link      http://www.opensource-socialnetwork.org/licence
  */
+ 
+/**
+ * Get group by guid
+ *
+ * @param int $guid Group guid
+ * @return object
+ */
 function ossn_get_group_by_guid($guid) {
     $group = new OssnGroup;
     return $group->getGroup($guid);
 }
-
+/**
+ * Get group layout
+ *
+ * @param html $content Content of page (html, php)
+ * @return mixed data
+ */
 function ossn_group_layout($contents) {
     $content['content'] = $contents;
     return ossn_view('components/OssnGroups/page/group', $content);
 }
-
+/**
+ * Get user groups (owned/member of)
+ *
+ * @param object $user User entity
+ * @return object
+ */
 function ossn_get_user_groups($user) {
     if ($user) {
         $groups = new OssnGroup;
-        return $groups->getUserGroups($user->guid);
+		//get user owned/member of groups #155
+        return $groups->getMyGroups($user);
     }
 }
-
+/**
+ * Group subpage set
+ *
+ * @param string $page Page name
+ * @return void
+ */
 function ossn_group_subpage($page) {
     global $VIEW;
-    return $VIEW->pagePush[] = $page;
+    $VIEW->pagePush[] = $page;
 }
-
+/**
+ * Check if page is instace of group subpage
+ *
+ * @param string $page Page name
+ * @return bool
+ */
 function ossn_is_group_subapge($page) {
     global $VIEW;
     if (in_array($page, $VIEW->pagePush)) {
@@ -37,7 +65,12 @@ function ossn_is_group_subapge($page) {
     }
     return false;
 }
-
+/**
+ * Get group url
+ *
+ * @param object $group Group entity
+ * @return string
+ */
 function ossn_group_url($group) {
     return ossn_site_url("group/{$group}/");
 }
