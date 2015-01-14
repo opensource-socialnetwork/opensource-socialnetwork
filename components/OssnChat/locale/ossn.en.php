4cd4a2d221051afe4f693327dbeb6eb46f84c9a9
Arsalan Shah <manactric@gmail.com>
2015-01-14 18:20:13 +0500

    Add language file for OssnChat

---
+++ components/OssnChat/locale/ossn.en.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   Open Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$en = array(
+	'ossn:chat:no:friend:online' => 'No one is online',
+);
+ossn_register_languages('en', $en); 
