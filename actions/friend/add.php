3137bcd5726ddd876e7f793e9c1d63fd60dcc8cd
Arsalan Shah <manactric@gmail.com>
2015-01-14 18:40:00 +0500

    Update add.php

--- actions/friend/add.php
+++ actions/friend/add.php
@@ -13,7 +13,7 @@ if (ossn_is_xhr()) {
 }
 if (ossn_add_friend(ossn_loggedin_user()->guid, input('user'))) {
     if (!ossn_is_xhr()) {
-        ossn_trigger_message(ossn_print('ossn:notification:are:friends'), 'error');
+        ossn_trigger_message(ossn_print('ossn:notification:are:friends'));
         redirect(REF);
     }
     if (ossn_is_xhr()) {
