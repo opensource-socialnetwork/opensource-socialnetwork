df61f874fd439a8c529ef3942170fd00767eba31
Arsalan Shah <manactric@gmail.com>
2015-01-14 18:20:30 +0500

    Add hardcoded string into language file

--- components/OssnChat/views/OssnChat/friends/status.php
+++ components/OssnChat/views/OssnChat/friends/status.php
@@ -19,5 +19,5 @@ if ($friends) {
     }
 }
 if ($have !== 1) {
-    echo '<div class="ossn-chat-none">No one is online</div>';
+    echo '<div class="ossn-chat-none">'.ossn_print('ossn:chat:no:friend:online').'</div>';
 }
