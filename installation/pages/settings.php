1863f7690e08fced24affdf1b59a57320fde2e21
githubertus <github@spam-s.cked.de>
2015-01-18 00:16:01 +0100

    replaced several hardcoded strings

--- installation/pages/settings.php
+++ installation/pages/settings.php
@@ -15,38 +15,38 @@ echo '<h2>' . ossn_installation_print('site:settings') . '</h2>';
 <form action="<?php echo ossn_installation_paths()->url; ?>?action=install" method="post">
 
     <div>
-        <label> Database Settings: </label>
+        <label> <?php echo ossn_installation_print('ossn:dbsettings'); ?>: </label>
 
-        <input type="text" name="dbuser" placeholder="Database User"/>
-        <input type="password" name="dbpwd" placeholder="Database Password"/>
-        <input type="text" name="dbname" placeholder="Database Name"/>
-        <input type="text" name="dbhost" placeholder="Database Host"/>
+        <input type="text" name="dbuser" placeholder="<?php echo ossn_installation_print('ossn:dbuser'); ?>"/>
+        <input type="password" name="dbpwd" placeholder="<?php echo ossn_installation_print('ossn:dbpassword'); ?>"/>
+        <input type="text" name="dbname" placeholder="<?php echo ossn_installation_print('ossn:dbname'); ?>"/>
+        <input type="text" name="dbhost" placeholder="<?php echo ossn_installation_print('ossn:dbhost'); ?>"/>
     </div>
 
     <div>
 
-        <label> Site Settings: </label>
+        <label> <?php echo ossn_installation_print('ossn:sitesettings'); ?>: </label>
 
-        <input type="text" name="sitename" placeholder="Website Name"/>
+        <input type="text" name="sitename" placeholder="<?php echo ossn_installation_print('ossn:websitename'); ?>"/>
 
-        <input type="text" name="owner_email" placeholder="Site Owner Email"/>
-        <input type="text" name="notification_email" placeholder="Notification Email (noreply@domain.com)"/>
+        <input type="text" name="owner_email" placeholder="<?php echo ossn_installation_print('owner_email'); ?>"/>
+        <input type="text" name="notification_email" placeholder="<?php echo ossn_installation_print('notification_email'); ?>"/>
     </div>
 
-    <label> Main Settings: </label>
+    <label> <?php echo ossn_installation_print('ossn:mainsettings'); ?>: </label>
     <br/>
 
     <div>
-        <label> Site Url </label>
+        <label> <?php echo ossn_installation_print('ossn:weburl'); ?> </label>
         <input type="text" name="url" value="<?php echo OssnInstallation::url(); ?>"/>
     </div>
 
     <div>
-        <label> Data directory </label>
+        <label> <?php echo ossn_installation_print('ossn:datadir'); ?> </label>
         <input type="text" name="datadir" value="<?php echo OssnInstallation::DefaultDataDir(); ?>"/>
     </div>
 
-    <input type="submit" value="Install" class="button-blue primary">
+    <input type="submit" value="<?php echo ossn_installation_print('ossn:install:install'); ?>" class="button-blue primary">
 
 </form>
 
