6beef1c02fbd4ea949824e38b617fefc1d48340b
Arsalan Shah <manactric@gmail.com>
2015-01-19 01:29:16 +0500

    Add German Language

---
+++ components/OssnBlock/locale/ossn.de.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   Open Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$de = array(
+    'user:blocked' => 'Der Benutzer wurde geblockt',
+    'user:block:error' => 'Der Benutzer kann nicht geblockt werden - versuche es später nochmal',
+    'user:block' => 'Blocken',
+    'user:unblock' => 'Entblocken',
+);
+ossn_register_languages('de', $de); 
