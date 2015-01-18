fbb590418eaae5d3e1f4d754e22cba61f9e21fe7
Arsalan Shah <manactric@gmail.com>
2015-01-19 01:29:14 +0500

    Add German Language

---
+++ components/OssnAds/locale/ossn.de.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   Open Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$de = array(
+    'ossnads' => 'Anzeigen-Verwaltung',
+    'fields:required' => 'Es müssen alle Felder ausgefüllt werden',
+    'ad:created' => 'Die Anzeige wurde erstellt',
+    'ad:create:fail' => 'Die Anzeige kann nicht erstellt werden',
+    'ad:title' => 'Titel',
+    'ad:site:url' => 'Url der Webseite',
+    'ad:desc' => 'Beschreibung',
+    'ad:photo' => 'Foto',
+    'ad:browse' => 'Ansehen',
+    'ad:clicks' => 'Klicks',
+    'sponsored' => 'Gesponsert',
+	'ad:deleted' => "Die Anzeige mit dem Titel '%s' wurde erfolgreich gelöscht",
+	'ad:delete:fail' => 'Die Anzeige kann nicht gelöscht werden - versuche es erneut',
+	'ad:edited' => 'Die Anzeige wurde erfolgreich geändert',
+	'ad:edit:fail' => 'Die Anzeige kann nicht geändert werden - bitte versuche es erneut',
+);
+ossn_register_languages('de', $de);
