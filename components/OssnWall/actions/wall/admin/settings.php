c8de1d733857dd52d179b863d583bff20ba2cf9c
Arsalan Shah <manactric@gmail.com>
2015-01-21 23:20:29 +0500

    add settings to save default wall type

---
+++ components/OssnWall/actions/wall/admin/settings.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   Open Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$type  = input('type');
+$types = array(
+		'friends',
+		'public'
+);
+if(!in_array($type, $types)) {
+		ossn_trigger_message(ossn_print('ossn:wall:settings:save:error'), 'error');
+		redirect(REF);
+}
+if(ossn_set_homepage_wall_access($type)) {
+		ossn_trigger_message(ossn_print('ossn:wall:settings:saved'));
+} else {
+		ossn_trigger_message(ossn_print('ossn:wall:settings:save:error'), 'error');
+}
+redirect(REF);
