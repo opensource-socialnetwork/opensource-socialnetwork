8dd81dfeb987ec9bdeb0c1f038cef44e4c90ebd0
Arsalan Shah <manactric@gmail.com>
2015-01-22 13:10:46 +0500

    catch and log error messages when sending email

--- classes/OssnMail.php
+++ classes/OssnMail.php
@@ -13,27 +13,32 @@
 require_once(ossn_route()->classes . 'mail/PHPMailerAutoload.php');
 
 class OssnMail extends PHPMailer {
-    /**
-     * Send email to user.
-     *
-     * @parans = $email => user email
-     *           $subject => email subject
-     *           $body = email body
-     *
-     * @return (bool)
-     */
-    public function NotifiyUser($email, $subject, $body) {
-        $this->setFrom(ossn_site_settings('notification_email'), ossn_site_settings('site_name'));
-        $this->addAddress($email);
-
-        $this->Subject = $subject;
-        $this->Body = $body;
-        $this->CharSet = "UTF-8";
-        
-        if ($this->send()) {
-            return true;
-        }
-        return false;
-    }
-
-}//class
+		/**
+		 * Send email to user.
+		 *
+		 * @parans = $email => user email
+		 *           $subject => email subject
+		 *           $body = email body
+		 *
+		 * @return (bool)
+		 */
+		public function NotifiyUser($email, $subject, $body) {
+				$this->setFrom(ossn_site_settings('notification_email'), ossn_site_settings('site_name'));
+				$this->addAddress($email);
+				
+				$this->Subject = $subject;
+				$this->Body    = $body;
+				$this->CharSet = "UTF-8";
+				
+				try {
+						if($this->send()) {
+								return true;
+						}
+				}
+				catch(phpmailerException $e) {
+						error_log("Cannot send email " . $e->errorMessage(), 0);
+				}
+				return false;
+		}
+		
+} //class
