348ebcbaae6806b38dd688c8a6349a81cf0ac1f1
Arsalan Shah <manactric@gmail.com>
2015-01-24 17:19:23 +0500

    group admins must be able to delete ANY comment in their own group #170

--- components/OssnComments/actions/comment/delete.php
+++ components/OssnComments/actions/comment/delete.php
@@ -10,8 +10,22 @@
  */
 $comment = input('comment');
 $delete = new OssnComments;
-if ($delete->GetComment($comment)->owner_guid == ossn_loggedin_user()->guid || ossn_isAdminLoggedin()) {
-    if ($delete->deleteComment($comment)) {
+$comment = $delete->GetComment($comment);
+
+//group admins must be able to delete ANY comment in their own group #170
+//first get wall post then get group and check if loggedin user is group owner
+if ($comment->type == 'comments:post') {
+    $post = ossn_get_object($comment->subject_guid);
+    if ($post && $post->type == 'group') {
+        $group = ossn_get_group_by_guid($post->owner_guid);
+    }
+}
+if ($comment->type == 'comments:entity') {
+    $entity = ossn_get_entity($comment->subject_guid);
+}
+//check if comment is based on entity then check entity ownerguid and if logged in user is entity owner delete comment
+if (($comment->owner_guid == ossn_loggedin_user()->guid) || ($group->owner_guid == ossn_loggedin_user()->guid) || ($entity->owner_guid == ossn_loggedin_user()->guid) || ossn_isAdminLoggedin()) {
+    if ($delete->deleteComment($comment->getID())) {
         if (ossn_is_xhr()) {
             echo 1;
         } else {
