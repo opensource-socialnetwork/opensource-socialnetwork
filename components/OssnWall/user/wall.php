ccb486998aef5eae9d54837d684b6090d2013f20
Arsalan Shah <manactric@gmail.com>
2015-01-24 17:20:23 +0500

    site admins are unable to access member profile threads without friendship #176

--- components/OssnWall/user/wall.php
+++ components/OssnWall/user/wall.php
@@ -10,61 +10,66 @@
  */
 $posts = new OssnWall;
 $posts = $posts->GetUserPosts($params['user']->guid);
-if (ossn_user_is_friend(ossn_loggedin_user()->guid, $params['user']->guid) || ossn_loggedin_user()->guid == $params['user']->guid) {
-    echo '<div class="ossn-wall-container">';
-    echo ossn_view_form('user/container', array(
-        'action' => ossn_site_url() . 'action/wall/post/u',
-        'component' => 'OssnWall',
-        'params' => array('user' => $params['user']),
-    ), false);
-    echo '</div>';
+
+//lastchage: site admins are unable to access member profile threads without friendship #176 
+if(ossn_user_is_friend(ossn_loggedin_user()->guid, $params['user']->guid) || ossn_loggedin_user()->guid == $params['user']->guid || ossn_isAdminLoggedin()) {
+		echo '<div class="ossn-wall-container">';
+		echo ossn_view_form('user/container', array(
+				'action' => ossn_site_url() . 'action/wall/post/u',
+				'component' => 'OssnWall',
+				'params' => array(
+						'user' => $params['user']
+				)
+		), false);
+		echo '</div>';
 }
 echo '<div class="user-activity">';
 $Pagination = new OssnPagination;
 $Pagination->setItem($posts);
 $posts = $Pagination->getItem();
-if ($posts) {
-    foreach ($posts as $post) {
-        $data = json_decode(html_entity_decode($post->description));
-        $text = $data->post;
-        $location = '';
-        if (isset($data->location)) {
-            $location = '- ' . $data->location;
-        }
-        if (!isset($data->friend)) {
-            $data->friend = '';
-        }
-        if (isset($post->{'file:wallphoto'})) {
-            $image = str_replace('ossnwall/images/', '', $post->{'file:wallphoto'});
-        } else {
-            $image = '';
-        }
-        $user = ossn_user_by_guid($post->poster_guid);
-        if ($post->access == OSSN_FRIENDS) {
-            if (ossn_user_is_friend(ossn_loggedin_user()->guid, $post->owner_guid) || ossn_loggedin_user()->guid == $post->owner_guid) {
-                echo ossn_view('components/OssnWall/templates/activity-item', array(
-                    'post' => $post,
-                    'friends' => explode(',', $data->friend),
-                    'text' => $text,
-                    'location' => $location,
-                    'user' => $user,
-                    'image' => $image,
-                ));
-            }
-        }
-        if ($post->access == OSSN_PUBLIC) {
-            echo ossn_view('components/OssnWall/templates/activity-item', array(
-                'post' => $post,
-                'friends' => explode(',', $data->friend),
-                'text' => $text,
-                'location' => $location,
-                'user' => $user,
-                'image' => $image,
-            ));
-        }
-
-
-    }
+if($posts) {
+		foreach($posts as $post) {
+				$data     = json_decode(html_entity_decode($post->description));
+				$text     = $data->post;
+				$location = '';
+				if(isset($data->location)) {
+						$location = '- ' . $data->location;
+				}
+				if(!isset($data->friend)) {
+						$data->friend = '';
+				}
+				if(isset($post->{'file:wallphoto'})) {
+						$image = str_replace('ossnwall/images/', '', $post->{'file:wallphoto'});
+				} else {
+						$image = '';
+				}
+				$user = ossn_user_by_guid($post->poster_guid);
+				if($post->access == OSSN_FRIENDS) {
+						//lastchage: site admins are unable to access member profile threads without friendship #176 
+						if(ossn_user_is_friend(ossn_loggedin_user()->guid, $post->owner_guid) || ossn_loggedin_user()->guid == $post->owner_guid || ossn_isAdminLoggedin()) {
+								echo ossn_view('components/OssnWall/templates/activity-item', array(
+										'post' => $post,
+										'friends' => explode(',', $data->friend),
+										'text' => $text,
+										'location' => $location,
+										'user' => $user,
+										'image' => $image
+								));
+						}
+				}
+				if($post->access == OSSN_PUBLIC) {
+						echo ossn_view('components/OssnWall/templates/activity-item', array(
+								'post' => $post,
+								'friends' => explode(',', $data->friend),
+								'text' => $text,
+								'location' => $location,
+								'user' => $user,
+								'image' => $image
+						));
+				}
+				
+				
+		}
 }
 echo $Pagination->pagination();
 echo '</div>';
