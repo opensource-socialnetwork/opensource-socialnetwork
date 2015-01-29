b37c9f88e846cf76335f15724142a115d640d4b3
Arsalan Shah <manactric@gmail.com>
2015-01-30 02:30:28 +0500

    localization of passed time #247

--- libraries/ossn.lib.users.php
+++ libraries/ossn.lib.users.php
@@ -227,30 +227,15 @@ function ossn_user_friendly_time($tm, $rcs = 0) {
     $cur_tm = time();
     $dif = $cur_tm - $tm;
 	// get language dependend items for display
-	$passedtime = ossn_print('site:timepassed');
+	$passedtime = ossn_print('site:timepassed:data');
 	// put them into array
 	// 0  = second
 	// 15 = decades
-	// 16 = trailing flag
-	// 17 = additional text to display
 	$pds = explode('|',$passedtime);
 	
 	// BETTER DO explode ONLY ONCE on start and when language changes
 	// and get already prepared array from there
 	// don't know how and where to do this correctly ?!?
-
-	/*
-    $pds = array(
-        'second',
-        'minute',
-        'hour',
-        'day',
-        'week',
-        'month',
-        'year',
-        'decade'
-    );
-    */
     $lngh = array(
         1,
         60,
@@ -261,15 +246,12 @@ function ossn_user_friendly_time($tm, $rcs = 0) {
         31570560,
         315705600
     );
-
     for ($v = count($lngh) - 1; ($v >= 0) && (($no = $dif / $lngh[$v]) <= 1); $v--)
         ;
     if ($v < 0)
         $v = 0;
     $_tm = $cur_tm - ($dif % $lngh[$v]);
-
     $no = ($rcs ? floor($no) : round($no)); // if last denomination, round
-
 	// since our array now has 16 time elements instead of 8, we need to skip odd entries and fetch the next even one (the singular)
 	$v = $v * 2;
 	
@@ -278,16 +260,9 @@ function ossn_user_friendly_time($tm, $rcs = 0) {
         // in case of plural we need the current element's index + 1
 		$v++;
     $x = $no . ' ' . $pds[$v];
-
     if (($rcs > 0) && ($v >= 1))
         $x .= ' ' . ossn_user_friendly_time($_tm, $rcs - 1);
-
-	// trailing or leading text?
-	if ($pds[16] == 1)
-		return $x . ' ' . $pds[17]; 	// English style
-	else
-		return $pds[17] . ' ' . $x;		// German style
-    // return $x . ' ago';
+    return ossn_print('site:timepassed:text', $x);
 }
 
 /**
