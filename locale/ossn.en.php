bcec0dea13658d4cd322ec47476921c96d7cbcb5
githubertus <github@spam-s.cked.de>
2015-01-30 00:38:19 +0100

    removed last 2 unused array elements

--- locale/ossn.en.php
+++ locale/ossn.en.php
@@ -248,8 +248,8 @@ Please click the link below to reset password or copy the link and paste in your
 	'admin:notification:email' => 'Site Notification Email',
 	'ossn:user:validation:resend' => 'Your account is not validated so the log in attempt failed. Another validation email has been sent.',
 	'site:timepassed:text' => '%s ago',
-	// localization of passed time: 16 time elements (even = singular / odd = plural) plus trailing flag and additional text
-	'site:timepassed:data' => 'second|seconds|minute|minutes|hour|hours|day|days|week|weeks|month|months|year|years|decade|decades|1|ago',
+	// localization of passed time: 16 time elements (even = singular / odd = plural)
+	'site:timepassed:data' => 'second|seconds|minute|minutes|hour|hours|day|days|week|weeks|month|months|year|years|decade|decades',
 	'ossn:notification:no:notification' => 'Nothing to show',
 	'admin:button:enable' => 'Enable',
 	'admin:button:enabled' => 'Enabled',
