79a96172ff71fcb80e105603212a9afb68872820
Arsalan Shah <manactric@gmail.com>
2015-02-05 23:27:17 +0500

    add new component

--- installation/sql/opensource-socialnetwork.sql
+++ installation/sql/opensource-socialnetwork.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS `ossn_components` (
   `com_id` varchar(20) NOT NULL,
   `active` int(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=16 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
 
 --
 -- Dumping data for table `ossn_components`
@@ -43,7 +43,8 @@ INSERT INTO `ossn_components` (`id`, `com_id`, `active`) VALUES
 (12, 'OssnBlock', 1),
 (13, 'OssnChat', 1),
 (14, 'OssnPoke', 1),
-(15, 'OssnInvite', 1);
+(15, 'OssnInvite', 1),
+(16, 'OssnEmbed', 1);
 
 -- --------------------------------------------------------
 
