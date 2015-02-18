c665620bc8978359586ca80d9930ff894f3e3db4
Arsalan Shah <manactric@gmail.com>
2015-02-18 22:50:28 +0500

    add smilies in comments

--- components/OssnSmilies/ossn_com.php
+++ components/OssnSmilies/ossn_com.php
@@ -22,6 +22,7 @@ require_once(__OSSN_SMILIES__ . 'libraries/smilify.lib.php');
  */
 function ossn_smiley_embed_init() {	
  	ossn_add_hook('wall', 'templates:item', 'ossn_embed_smiley', 100);
+	ossn_add_hook('comment:view', 'template:params', 'ossn_smiley_in_comments', 100);	
 }
 /**
  * Replace certain ascii patterns with ossn emoticons.
@@ -39,6 +40,23 @@ function ossn_smiley_embed_init() {
 function ossn_embed_smiley($hook, $type, $return, $params){
 	$params['text'] = smilify($return['text']);
 	return $params;
+}
+/**
+ * Replace certain ascii patterns with ossn emoticons in comments.
+ *
+ * @note Please don't call this function directly in your code.
+ * 
+ * @param string $hook Name of hook
+ * @param string $type Hook type
+ * @param array|object $return Array or Object
+ * @params array $params Array contatins params
+ *
+ * @return array
+ * @access private
+ */
+function ossn_smiley_in_comments($hook, $type, $return, $params){
+	$return['comment']['comments:post'] = smilify($return['comment']['comments:post']);
+	return $return;	
 }
 //initilize ossn smilies
 ossn_register_callback('ossn', 'init', 'ossn_smiley_embed_init');
