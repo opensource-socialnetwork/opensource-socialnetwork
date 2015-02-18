b87e80d8f03e7ea805c3a0af1523a69234aa06c5
bishopgodsey <godseygreg@gmail.com>
2015-02-18 00:39:02 -0500

    Long list
    Here is the long and short of it:
    
    1. Fixed the issues between OssnEmbed and OssnSmilies. Fixes #361
    partially.
    2. Added OssnSmilies to MySQL. Fixes the rest of #361. (Did not know
    what your naming convention was on the upgrade files. If you let me know
    what it is, I can use it next time.)
    3. Fixed what I saw as a spelling error in the code of the pagination
    file.

---
+++ upgrade/upgrades/1423419054.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+
+$database = new OssnDatabase;
+$Component = new OssnComponents;
+
+/**
+ * Add OssnEmbed and OssnInvite Component
+ */
+$Component->ENABLE('OssnSmilies');
+
+/**
+ * Update processed updates in database so user cannot upgrade again and again.
+ *
+ * @access private
+ */
+
+$upgrade_json = array_merge(ossn_get_upgraded_files(), array($upgrade));
+$upgrade_json = json_encode($upgrade_json);
+
+$update['table'] = 'ossn_site_settings';
+$update['names'] = array('value');
+$update['values'] = array($upgrade_json);
+$update['wheres'] = array("name='upgrades'");
+
+$upgrade = str_replace('.php', '', $upgrade);
+if ($database->update($update)) {
+    ossn_trigger_message(ossn_print('upgrade:success', array($upgrade)), 'success');
+} else {
+    ossn_trigger_message(ossn_print('upgrade:failed', array($upgrade)), 'error');
+}
