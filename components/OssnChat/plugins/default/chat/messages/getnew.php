2e50c3be8699de661533af66073bf1f048da7310
Arsalan Shah <manactric@gmail.com>
2015-07-05 02:41:00 +0500

    ossn chat syntax error #437

--- components/OssnChat/plugins/default/chat/messages/getnew.php
+++ components/OssnChat/plugins/default/chat/messages/getnew.php
@@ -12,7 +12,7 @@ $from = $params['message_to'];
 $to = ossn_loggedin_user();
 $messages = ossn_chat()->getNew($from, $to);
 if ($messages) {
-    foreach ($messages as as $message){
+    foreach ($messages as $message){
         if (ossn_loggedin_user()->guid == $message->message_from) {
             $vars['message'] = $message->message;
             echo ossn_plugin_view('chat/message-item-send', $vars);
