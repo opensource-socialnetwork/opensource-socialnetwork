d5ab76aa0c99045591dfe4d90095ae11c6f5ab58
Arsalan Shah <manactric@gmail.com>
2015-07-10 23:30:35 +0500

    What non-bundled component is doing here?

--- components/Favicon/actions/upload.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once(ossn_route()->com . 'Favicon/classes/Favicon.php');
-if(Favicon::Upload()){
-		ossn_trigger_message(ossn_print('Favicon updated'));
-		redirect(REF);
-} else {
-		ossn_trigger_message(ossn_print('Favicon update error'), 'error');
-		redirect(REF);	
-}
