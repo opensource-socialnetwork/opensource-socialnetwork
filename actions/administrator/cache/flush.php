519228313f317f3fe6dd6c33f4ff6a66694c1735
Arsalan Shah <manactric@gmail.com>
2015-07-11 22:45:15 +0500

    flush cache didn't flush the plugins path #460

--- actions/administrator/cache/flush.php
+++ actions/administrator/cache/flush.php
@@ -8,14 +8,19 @@
  * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
  * @link      http://www.opensource-socialnetwork.org/licence
  */
-
- if(ossn_site_settings('cache') == true){
-  	if(ossn_disable_cache()){
+ 
+ $disable = input('disabled');
+ if(ossn_site_settings('cache') == true || !empty($disable)){
+  	if(ossn_disable_cache() && empty($disable)){
+				//flush cache didn't flush the plugins path #460
+				$action =  ossn_add_tokens_to_url("action/admin/cache/flush?disabled=disabled");
+				redirect($action);		
+  	} elseif($disable == 'disabled'){
 		if(ossn_create_cache()){
 			    ossn_trigger_message(ossn_print('cache:flushed'));
 				redirect(REF);
 		}
-  	}
+	}
  } 
  ossn_trigger_message(ossn_print('cache:flush:error'), 'error');
  redirect(REF);
