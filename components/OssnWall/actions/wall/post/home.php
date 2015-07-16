8a7234dd9fc31b96065af7fed9e0dbd814cc92ce
Arsalan Shah <manactric@gmail.com>
2015-07-16 14:04:56 +0500

    Ossn Wall ajax post

--- components/OssnWall/actions/wall/post/home.php
+++ components/OssnWall/actions/wall/post/home.php
@@ -13,32 +13,43 @@
 $OssnWall = new OssnWall;
 
 //poster guid and owner guid is same as user is posting on home page on its own wall
-$OssnWall->owner_guid = ossn_loggedin_user()->guid;
+$OssnWall->owner_guid  = ossn_loggedin_user()->guid;
 $OssnWall->poster_guid = ossn_loggedin_user()->guid;
 
 //check if owner guid is zero then exit
-if ($OssnWall->owner_guid == 0 || $OssnWall->poster_guid == 0) {
-    ossn_trigger_message(ossn_print('post:create:error'), 'error');
-    redirect(REF);
+if($OssnWall->owner_guid == 0 || $OssnWall->poster_guid == 0) {
+		ossn_trigger_message(ossn_print('post:create:error'), 'error');
+		redirect(REF);
 }
 
 //getting some inputs that are required for wall post
-$post = input('post');
-$friends = input('friends');
+$post     = input('post');
+$friends  = input('friends');
 $location = input('location');
-$privacy = input('privacy');
+$privacy  = input('privacy');
 
 //validate wall privacy 
 $privacy = ossn_access_id_str($privacy);
-$access = '';
-if (!empty($privacy)) {
-    $access = input('privacy');
+$access  = '';
+if(!empty($privacy)) {
+		$access = input('privacy');
 }
-if ($OssnWall->Post($post, $friends, $location, $access)) {
-    //no need to show message on success
-    //ossn_trigger_message(ossn_print('post:created'), 'success');	
-    redirect(REF);
+if($OssnWall->Post($post, $friends, $location, $access)) {
+		if(ossn_is_xhr()) {
+				$guid = $OssnWall->getObjectId();
+				$get  = $OssnWall->GetPost($guid);
+				if($get) {
+						$get = ossn_wallpost_to_item($get);
+						ossn_set_ajax_data(array(
+								'post' => ossn_wall_view_template($get)
+						));
+				}
+		}
+		//no need to show message on success.
+		//3.x why not? $arsalanshah
+		ossn_trigger_message(ossn_print('post:created'));
+		redirect(REF);
 } else {
-    ossn_trigger_message(ossn_print('post:create:error'), 'error');
-    redirect(REF);
+		ossn_trigger_message(ossn_print('post:create:error'), 'error');
+		redirect(REF);
 }
