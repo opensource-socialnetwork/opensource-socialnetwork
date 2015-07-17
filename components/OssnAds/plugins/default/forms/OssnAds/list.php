101c7a9b69f4f15aac13c4f1b7942727a021a9c8
Arsalan Shah <manactric@gmail.com>
2015-07-17 21:25:00 +0500

    no need to show ads click, we didn't have feature for it #414

--- components/OssnAds/plugins/default/forms/OssnAds/list.php
+++ components/OssnAds/plugins/default/forms/OssnAds/list.php
@@ -28,7 +28,7 @@ $pagination->setItem($ads->getAds());
         <td></td>
         <td><?php echo ossn_print('ad:title'); ?></td>
         <td><?php echo ossn_print('ad:site:url'); ?></td>
-        <td><?php echo ossn_print('ad:clicks'); ?></td>
+        <!-- <td><?php echo ossn_print('ad:clicks'); ?></td> -->
         <td><?php echo ossn_print('ad:browse'); ?></td>
         <td><?php echo ossn_print('edit'); ?></td>
     </tr>
@@ -41,7 +41,7 @@ $pagination->setItem($ads->getAds());
                 <td><input type="checkbox" name="entites[]" value="<?php echo $ads->guid; ?>"/></td>
                 <td><?php echo $ads->title; ?></td>
                 <td><?php echo $ads->description; ?></td>
-                <td> 32</td>
+                <!-- <td> 32</td> -->
                 <td>
                     <a href="<?php echo ossn_site_url("administrator/component/OssnAds?settings=view&id={$ads->guid}"); ?>">
                         <?php echo ossn_print('ad:browse'); ?></a></td>
