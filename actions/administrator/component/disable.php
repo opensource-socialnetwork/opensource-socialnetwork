74ede694381cfa86d710ba82aeeaf00c1b45f05d
Arsalan Shah <manactric@gmail.com>
2015-07-23 23:48:26 +0500

    Update disable.php

--- actions/administrator/component/disable.php
+++ actions/administrator/component/disable.php
@@ -11,25 +11,16 @@
 
 $component = new OssnComponents;
 
-$com 		 = input('com');
-$cache_flush = input('flush_cache', '', false);
-$cache		 = ossn_site_settings('cache');
+$com   = input('com');
+$cache = ossn_site_settings('cache');
 
-if (!$cache_flush && $component->DISABLE($com)) {
-    ossn_trigger_message(ossn_print('com:disabled'));
-   if($cache == false){
-		redirect(REF);
-	} else {
-		//redirect and flush cache
-		$page = "action/component/disable?flush_cache=1";
-		$page = ossn_add_tokens_to_url($page);
-		redirect($page);
-	}
-}
-if($cache_flush){
-	if(ossn_disable_cache()){
-		if(ossn_create_cache()){
-				redirect('administrator/components');
+if($component->DISABLE($com)) {
+		ossn_trigger_message(ossn_print('com:disabled'));
+		if($cache == false) {
+				redirect(REF);
+		} else {
+				//redirect and flush cache
+				$action = ossn_add_tokens_to_url("action/admin/cache/flush");
+				redirect($action);
 		}
-  	}	
 }
