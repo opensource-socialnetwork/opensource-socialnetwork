0c44c23a91232928b6bd5d199f69834f96f42582
Arsalan Shah <manactric@gmail.com>
2015-07-23 23:49:00 +0500

    Update delete.php

--- actions/administrator/component/delete.php
+++ actions/administrator/component/delete.php
@@ -9,30 +9,20 @@
  * @link      http://www.opensource-socialnetwork.org/licence
  */
 
-$delete 	 = new OssnComponents;
-$com 		 = input('component');
-$cache_flush = input('flush_cache', '', false);
-$cache		 = ossn_site_settings('cache');
+$delete = new OssnComponents;
+$com    = input('component');
+$cache  = ossn_site_settings('cache');
 
-if (!$cache_flush && $delete->delete($com)) {
-    ossn_trigger_message(ossn_print('com:deleted'), 'success');
-    if($cache == false){
-		redirect(REF);
-	} else {
-		//redirect and flush cache
-		$page = "action/component/delete?flush_cache=1";
-		$page = ossn_add_tokens_to_url($page);
-		redirect($page);
-	}
-} else {
-    ossn_trigger_message(ossn_print('con:delete:error'), 'error');
-    redirect(REF);
-}
-
-if($cache_flush){
-	if(ossn_disable_cache()){
-		if(ossn_create_cache()){
-				redirect('administrator/components');
+if($delete->delete($com)) {
+		ossn_trigger_message(ossn_print('com:deleted'), 'success');
+		if($cache == false) {
+				redirect(REF);
+		} else {
+				//redirect and flush cache
+				$action = ossn_add_tokens_to_url("action/admin/cache/flush");
+				redirect($action);
 		}
-  	}	
+} else {
+		ossn_trigger_message(ossn_print('con:delete:error'), 'error');
+		redirect(REF);
 }
