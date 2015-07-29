9e66d7067a82c7d626853bbfa283f8cc094843eb
Arsalan Shah <manactric@gmail.com>
2015-07-29 21:34:53 +0500

    Update delete.php

--- components/OssnPhotos/actions/album/delete.php
+++ components/OssnPhotos/actions/album/delete.php
@@ -1,9 +1,18 @@
 <?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.org>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
 $guid = input('guid');
 $album = new OssnAlbums;
 if($album->deleteAlbum($guid)){
         ossn_trigger_message(ossn_print('photo:album:deleted'));
-        redirect(REF);	
+        redirect();	
 } else {
         ossn_trigger_message(ossn_print('photo:album:delete:error'), 'error');
         redirect(REF);	
