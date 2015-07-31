b07cc3e6564bca05b48a5d6750f221fe94af7d50
Arsalan Shah <manactric@gmail.com>
2015-07-31 12:41:55 -0700

    home page view issue #474

--- system/default/pages/index.php
+++ system/default/pages/index.php
@@ -9,5 +9,5 @@
  * @link      http://www.opensource-socialnetwork.org/licence
  */
 $title = ossn_print('site:index');
-$content = ossn_set_page_layout('startup', array('content' => ossn_view('pages/contents/index')));
+$content = ossn_set_page_layout('startup', array('content' => ossn_plugin_view('pages/contents/index')));
 echo ossn_view_page($title, $content);
