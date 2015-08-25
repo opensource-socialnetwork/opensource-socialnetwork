646645bd7ec8084ba81468ba1c717eb0123b79a8
hazyapps <hazyapps@gmail.com>
2015-08-25 22:08:46 +0200

    Initial NL language file

---
+++ components/OssnProfile/locale/ossn.nl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$nl = array(
+    'people:like:this' => 'Mensen die deze post leuk vinden',
+    'change:cover' => 'Cover veranderen',
+    'change:photo' => 'Foto veranderen',
+    'update:info' => 'Info Updaten ',
+    'message' => 'Bericht',
+    'save:position' => 'Positie opslaan',
+	'ossn:profile:picture:updated' => 'Profielfoto gewijzigd.',
+	'ossn:profile:cover:picture:updated' => 'Coverfoto gewijzigd.',
+	'language' => 'Taal',
+);
+ossn_register_languages('nl', $nl); 
