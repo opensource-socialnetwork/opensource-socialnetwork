61a9170c56ec048d9bf29e34710cb25184638653
hazyapps <hazyapps@gmail.com>
2015-08-25 21:56:41 +0200

    Initial NL language file

---
+++ components/OssnBlock/locale/ossn.nl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$nl = array(
+    'user:blocked' => 'Gebruiker is geblokkeerd!',
+    'user:block:error' => 'Kon gebruiker niet blokkeren! probeer het later opnieuw.',
+    'user:block' => 'Blokkeren',
+    'user:unblock' => 'De-blokkeren',
+);
+ossn_register_languages('nl', $nl); 
