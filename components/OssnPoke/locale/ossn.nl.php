f8e969b401cb1c36989397ea1ab81a4accaf7d40
hazyapps <hazyapps@gmail.com>
2015-08-25 22:12:47 +0200

    Initial NL language file

---
+++ components/OssnPoke/locale/ossn.nl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$nl = array(
+    'ossn:notifications:ossnpoke:poke' => "%s Heeft je gepoked!",
+    'user:poked' => 'Je hebtd %s gepoked!',
+    'user:poke:error' => 'Kon %s niet poken! probeer het later.',
+    'poke' => 'Poke',
+);
+ossn_register_languages('nl', $nl); 
