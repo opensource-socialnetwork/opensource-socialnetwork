88f136135cdcf30d646eb42cf11697c21bc2e4ab
hazyapps <hazyapps@gmail.com>
2015-08-25 21:52:54 +0200

    Initial NL language file

---
+++ components/OssnAds/locale/ossn.nl.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$nl = array(
+    'ossnads' => 'Advertentie beheer',
+    'fields:required' => 'Alle velden zijn verplicht!',
+    'ad:created' => 'Advertentis is gecreeerd!',
+    'ad:create:fail' => 'Advertenties kon niet worden gecreeerd!',
+    'ad:title' => 'Titel',
+    'ad:site:url' => 'Siteurl',
+    'ad:desc' => 'Beschrijving',
+    'ad:photo' => 'Foto',
+    'ad:browse' => 'Bladeren',
+    'ad:clicks' => 'Clicks',
+    'sponsored' => 'gesponserd',
+	'ad:deleted' => "Advertentie met de titel '%s' Is succesvol verwijderd.",
+	'ad:delete:fail' => 'Kon advertentie niet verwijderen. Probeer het later nog eens.',
+	'ad:edited' => 'Advertentie succesvol aangepast.',
+	'ad:edit:fail' => 'Kon advertentie niet aanpassen! Probeer het later nog eens.',
+);
+ossn_register_languages('nl', $nl); 
