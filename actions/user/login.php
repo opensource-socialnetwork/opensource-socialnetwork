b6139cdc31d9daa363e30a99d3fd748363ade4ea
lianglee <manactric@gmail.com>
2015-09-25 22:53:51 +0500

    add callback to action

--- actions/user/login.php
+++ actions/user/login.php
@@ -9,35 +9,41 @@
  * @link      http://www.opensource-socialnetwork.org/licence
  */
 
-if (ossn_isLoggedin()) {
-    redirect('home');
+if(ossn_isLoggedin()) {
+		redirect('home');
 }
 $username = input('username');
 $password = input('password');
 
-if (empty($username) || empty($password)) {
-    ossn_trigger_message(ossn_print('login:error'));
-    redirect();
+if(empty($username) || empty($password)) {
+		ossn_trigger_message(ossn_print('login:error'));
+		redirect();
 }
 $user = ossn_user_by_username($username);
 
 //check if username is email
-if (strpos($username, '@') !== false){
-	$user = ossn_user_by_email($username);
-	$username = $user->username;
+if(strpos($username, '@') !== false) {
+		$user     = ossn_user_by_email($username);
+		$username = $user->username;
 }
 
-if($user && !$user->isUserVALIDATED()){
-	$user->resendValidationEmail();
-	ossn_trigger_message(ossn_print('ossn:user:validation:resend'), 'error');
-	redirect(REF);
+if($user && !$user->isUserVALIDATED()) {
+		$user->resendValidationEmail();
+		ossn_trigger_message(ossn_print('ossn:user:validation:resend'), 'error');
+		redirect(REF);
 }
-$login = new OssnUser;
+$vars = array(
+		'user' => $user
+);
+ossn_trigger_callback('user', 'before:login', $vars);
+
+$login           = new OssnUser;
 $login->username = $username;
 $login->password = $password;
-if ($login->Login()) {
-    //One uneeded redirection when login #516
-    redirect('home');
+if($login->Login()) {
+		//One uneeded redirection when login #516
+		ossn_trigger_callback('login', 'success', $vars);
+		redirect('home');
 } else {
-    redirect('login?error=1');
+		redirect('login?error=1');
 }
