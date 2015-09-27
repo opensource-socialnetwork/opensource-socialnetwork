0726311e51e6cd233e512779990e72fd87740ac9
lianglee <manactric@gmail.com>
2015-09-27 17:03:10 +0500

    Poke translation issue #543

--- components/OssnPoke/actions/user/poke.php
+++ components/OssnPoke/actions/user/poke.php
@@ -11,7 +11,8 @@
 $poke = new OssnPoke;
 $user = input('user');
 if ($poke->addPoke(ossn_loggedin_user()->guid, $user)) {
-    ossn_trigger_message(ossn_print('user:poked'), 'success');
+    $user = ossn_user_by_guid($user);
+    ossn_trigger_message(ossn_print('user:poked', array($user->fullname)), 'success');
     redirect(REF);
 } else {
     ossn_trigger_message(ossn_print('user:poke:error'), 'error');
