01b05e72d7378d5570e4e2d93dfb5724f4427301
lianglee <manactric@gmail.com>
2015-10-18 01:03:54 +0500

    move css to correct component

--- components/OssnComments/plugins/default/css/comments.php
+++ components/OssnComments/plugins/default/css/comments.php
@@ -89,3 +89,72 @@ margin-top: 20px;
 margin-left: 30px;
 vertical-align: top;
 }
+/***********************
+	Likes Comments
+************************/
+.comments-likes {
+background:#F6F7F8;
+min-height:50px;
+width:100%;
+}
+.ossn-photos-comments {
+display:inline-block;
+}
+.comments-like-comment-links {
+background:#fff;
+margin-top: -10px;
+}
+.comments-likes .poster-image {
+padding: 10px;
+display:inline-table;
+}
+.comments-likes .comment-container {
+display:inline-table;
+}
+.comments-likes .comment-text {
+display: inline-table;
+vertical-align: top;
+margin-top: 10px;
+margin-left: -5px;
+font-size: 12px;
+width: 455px;
+margin-bottom: 6px;
+}
+.comments-likes .like_share {
+padding: 10px;
+border-bottom: 1px solid #EEE;
+font-size: 12px;
+}
+
+.dot-comments {
+vertical-align: top;
+margin: -3px 2px 0 0px;
+display: inline-block;
+}
+.comments-likes textarea,
+.comments-likes input[type='text'] {
+float: right;
+border: 1px solid #D3D6DB;
+padding: 7px;
+height: 16px;
+margin-top: -32px;
+margin-left: -7px;
+width: 422px;
+resize:none;
+}
+.comment-text p {
+margin-top: 1px;
+line-height: 15px;
+}
+.comment-metadata {
+margin-top: -9px;
+color:#9B9B9B;
+}
+.comments-item {
+margin-bottom: -5px;
+}
+
+.comments-likes .like_share .button-container,
+.comments-likes .like_share a{
+display:inline-block;
+}
