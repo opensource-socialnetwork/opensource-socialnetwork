ff50b6cb2ade762549d12d9733711a3059def041
Arsalan Shah <manactric@gmail.com>
2015-10-25 22:30:50 +0500

    Update profile.php

--- components/OssnProfile/plugins/default/css/profile.php
+++ components/OssnProfile/plugins/default/css/profile.php
@@ -1,13 +1,12 @@
 /**
-* Open Source Social Network
-*
-* @package   (Informatikon.com).ossn
-* @author    OSSN Core Team
-<info@opensource-socialnetwork.org>
-* @copyright 2014 iNFORMATIKON TECHNOLOGIES
-* @license   General Public Licence http://www.opensource-socialnetwork.org/licence
-* @link      http://www.opensource-socialnetwork.org/licence
-*/
+ * Open Source Social Network
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.org>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
 .ossn-profile {
 width:850px;
 margin-top: -10px;
