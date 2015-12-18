cda20bdacaf9a46f5cd9f20a0e8872f58d1fba39
lianglee <manactric@gmail.com>
2015-12-18 18:55:33 +0500

    Admin profiles should be unblockable by 'normal' members #625

--- components/OssnBlock/actions/user/block.php
+++ components/OssnBlock/actions/user/block.php
@@ -10,7 +10,14 @@
  */
 $block = new OssnBlock;
 $user = input('user');
-if ($block->addBlock(ossn_loggedin_user()->guid, $user)) {
+$user = ossn_user_by_guid($user);
+
+//Admin profiles should be unblockable by 'normal' members #625
+if(!$user || $user->isAdmin()){
+    ossn_trigger_message(ossn_print('user:block:error'), 'error');
+    redirect(REF);	
+}
+if ($block->addBlock(ossn_loggedin_user()->guid, $user->guid)) {
     ossn_trigger_message(ossn_print('user:blocked'), 'success');
     redirect(REF);
 } else {
