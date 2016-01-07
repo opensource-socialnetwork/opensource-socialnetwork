e79e1df321d781c6b717ed3a9ea9c2f1fd3513a7
Eric <ctlui@users.noreply.github.com>
2016-01-07 15:09:40 +0100

    Change button padding
    Change padding font-size for button

---
+++ themes/goblue/plugins/default/css/likes.php
@@ -0,0 +1,12 @@
+.ossn-likes-view a.right.btn.btn-danger{
+	font-size: 12px;
+  margin-top: 8px;
+	margin-right: 5px;
+  padding: 2px;
+  }
+.ossn-likes-view a.right.btn.btn-primary{
+	font-size: 12px;
+  margin-top: 8px;
+	margin-right: 5px;
+  padding: 2px;
+  }
