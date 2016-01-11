3c8b353f2737a82220902aac11a10807ca96840c
githubertus <github@spam-s.cked.de>
2016-01-12 00:08:35 +0100

    do truncation of too long fullnames by css instead of strl function
    #748

--- components/OssnChat/plugins/default/chat/friends/friend-item.php
+++ components/OssnChat/plugins/default/chat/friends/friend-item.php
@@ -19,7 +19,7 @@ if ($friend->isOnline(10)) {
      onClick="Ossn.ChatnewTab(<?php echo $friend->guid; ?>);">
     <div class="friends-item-inner">
         <div class="icon"><img src="<?php echo $params['icon']; ?>"/></div>
-        <div class="name"><?php echo strl($friend->fullname, 15); ?></div>
+        <div class="name"><?php echo $friend->fullname; ?></div>
         <div class="<?php echo $status; ?> ustatus"></div>
     </div>
 </div>
