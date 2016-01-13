74c32416b44a18bcc8a0327063b68a1361ba95f6
githubertus <github@spam-s.cked.de>
2016-01-13 14:13:14 +0100

    do truncation of too long fullnames by css instead of strl function
    #748

--- system/plugins/default/pages/administrator/contents/users/unvalidated.php
+++ system/plugins/default/pages/administrator/contents/users/unvalidated.php
@@ -44,7 +44,7 @@ if($list) {
         <tr>
             <td>
                 <div class="left image"><img src="<?php echo $user->iconURL()->smaller; ?>"/></div>
-                <div class="name"><?php echo strl($user->fullname, 20); ?></div>
+                <div class="name"><?php echo $user->fullname; ?></div>
             </td>
             <td><?php echo $user->username;?></td>
             <td><?php echo $user->email; ?></td>
