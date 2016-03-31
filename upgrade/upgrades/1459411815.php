d01400389636f607d5c9b0c3fae5bd77bda07867
lianglee <manactric@gmail.com>
2016-03-31 13:19:55 +0500

    v4.1 push

---
+++ upgrade/upgrades/1459411815.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.org>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+
+ossn_generate_server_config('apache');
+ossn_version_upgrade($upgrade, '4.1');
+
+$theme = new OssnThemes;
+$theme->Enable('goblue');
+
+$factory = new OssnFactory(array(
+		'callback' => 'installation',
+		'website' => ossn_site_url(),
+		'email' => ossn_site_settings('owner_email'),
+		'version' => '4.1'
+));
+$factory->connect;
