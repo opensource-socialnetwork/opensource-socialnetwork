38aa2f9bfba35780870fecf2b85f45d81cce894c
Arsalan Shah <manactric@gmail.com>
2016-06-12 21:43:25 +0500

    Load wall posts when page is fully loaded #898

--- components/OssnWall/plugins/default/css/wall.php
+++ components/OssnWall/plugins/default/css/wall.php
@@ -4,3 +4,11 @@
 .ossn-wall-post-delete {
     color: #EC2020 !important;
 }
+.ossn-wall-loading {
+    text-align: center;
+    padding: 10px;
+    width: 100%;
+}
+.ossn-wall-loading .ossn-loading {
+    display: inline-block;
+}
