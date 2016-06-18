1dd3876742e3c33f67a32df63c7eacecc3d1b1d1
Arsalan Shah <manactric@gmail.com>
2016-06-18 23:28:20 +0500

    Update opensource-socialnetwork.sql

--- installation/sql/opensource-socialnetwork.sql
+++ installation/sql/opensource-socialnetwork.sql
@@ -210,3 +210,63 @@ CREATE TABLE IF NOT EXISTS `ossn_users` (
   `time_created` int(11) NOT NULL,
   PRIMARY KEY (`guid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Ossn v4.2 Database improvements
+--
+
+ALTER TABLE `ossn_annotations`
+	ADD KEY `owner_guid` (`owner_guid`),
+	ADD KEY `subject_guid` (`subject_guid`),
+	ADD KEY `time_created` (`time_created`);
+  
+ALTER TABLE `ossn_annotations` ADD FULLTEXT KEY `type` (`type`);
+
+ALTER TABLE `ossn_entities`
+	ADD KEY `owner_guid` (`owner_guid`),
+	ADD KEY `time_created` (`time_created`),
+	ADD KEY `time_updated` (`time_updated`),
+	ADD KEY `active` (`active`),
+	ADD KEY `permission` (`permission`);
+  
+ALTER TABLE `ossn_entities` 
+	ADD FULLTEXT KEY `type` (`type`),
+	ADD FULLTEXT KEY `subtype` (`subtype`);
+
+ALTER TABLE `ossn_entities_metadata`
+	ADD KEY `guid` (`guid`);
+  
+ALTER TABLE `ossn_entities_metadata` ADD FULLTEXT KEY `value` (`value`);
+
+ALTER TABLE `ossn_notifications`
+	ADD KEY `poster_guid` (`poster_guid`),
+	ADD KEY `owner_guid` (`owner_guid`),
+	ADD KEY `subject_guid` (`subject_guid`), 
+	ADD KEY `time_created` (`time_created`),
+	ADD KEY `item_guid` (`item_guid`);
+ALTER TABLE `ossn_notifications` ADD FULLTEXT KEY `type` (`type`);
+
+ALTER TABLE `ossn_object`
+	ADD KEY `owner_guid` (`owner_guid`),
+	ADD KEY `time_created` (`time_created`);
+  
+ALTER TABLE `ossn_object` 
+	ADD FULLTEXT KEY `type` (`type`),
+	ADD FULLTEXT KEY `subtype` (`subtype`);
+
+ALTER TABLE `ossn_relationships`
+	ADD KEY `relation_to` (`relation_to`),
+	ADD KEY `relation_from` (`relation_from`),
+	ADD KEY `time` (`time`);
+ALTER TABLE `ossn_relationships` ADD FULLTEXT KEY `type` (`type`);
+
+ALTER TABLE `ossn_users`
+	ADD KEY `last_login` (`last_login`),
+	ADD KEY `last_activity` (`last_activity`),
+	ADD KEY `time_created` (`time_created`);
+  
+ALTER TABLE `ossn_users`
+	ADD FULLTEXT KEY `type` (`type`),
+	ADD FULLTEXT KEY `email` (`email`),
+	ADD FULLTEXT KEY `first_name` (`first_name`),
+	ADD FULLTEXT KEY `last_name` (`last_name`);
