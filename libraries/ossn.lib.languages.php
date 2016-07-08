12dc5e171e18c1ad20c0af89aefd1565843aa6bb
Arsalan Shah <manactric@gmail.com>
2016-07-09 02:50:46 +0500

    Update ossn.lib.languages.php

--- libraries/ossn.lib.languages.php
+++ libraries/ossn.lib.languages.php
@@ -105,14 +105,30 @@ function ossn_default_load_locales() {
 function ossn_load_json_locales($lcode = "") {
 		global $Ossn;
 		$code = ossn_site_settings('language');
-		if(!empty($lcode)){
-			$code = $lcode;
+		if(!empty($lcode)) {
+				$code = $lcode;
 		}
-		if(!isset($Ossn->localestr[$code])){
-			return false;
+		if(!isset($Ossn->localestr[$code])) {
+				return false;
 		}
-		foreach($Ossn->localestr[$code] as $key => $item){
-				$strings[$key] = mb_substr($item, 0, strlen($item),'utf-8');
+		$isUTF8 = function($str) {
+				return preg_match("/^(
+         [\x09\x0A\x0D\x20-\x7E]            # ASCII
+       | [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
+       |  \xE0[\xA0-\xBF][\x80-\xBF]        # excluding overlongs
+       | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
+       |  \xED[\x80-\x9F][\x80-\xBF]        # excluding surrogates
+       |  \xF0[\x90-\xBF][\x80-\xBF]{2}     # planes 1-3
+       | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+       |  \xF4[\x80-\x8F][\x80-\xBF]{2}     # plane 16
+      )*$/x", $str);
+		};
+		foreach($Ossn->localestr[$code] as $key => $item) {
+				if(!$isUTF8($item)) {
+						$strings[$key] = utf8_encode($item);
+				} else {
+						$strings[$key] = $item;
+				}
 		}
 		$json = json_encode($strings, JSON_UNESCAPED_UNICODE);
 		if($json) {
