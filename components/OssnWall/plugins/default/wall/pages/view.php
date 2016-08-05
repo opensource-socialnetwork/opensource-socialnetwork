74dbec83e16bf821c1453a1e5f5f6af0328d8c35
Arsalan Shah <manactric@gmail.com>
2016-08-05 14:51:07 +0500

    wall/view didn't escape new lines #920

--- components/OssnWall/plugins/default/wall/pages/view.php
+++ components/OssnWall/plugins/default/wall/pages/view.php
@@ -9,40 +9,15 @@
  * @link      https://www.opensource-socialnetwork.org/
  */
 echo '<div class="user-activity">';
-$data = json_decode(html_entity_decode($params['post']->description));
-$text = $data->post;
-$location = '';
-if (isset($data->location)) {
-    $location = '- ' . $data->location;
-}
-//fix missing image in comment-item #161 $githubertus
-if (isset($params['post']->{'file:wallphoto'})) {
-    $image = str_replace('ossnwall/images/', '', $params['post']->{'file:wallphoto'});
-} else {
-    unset($image);
-}
 $params['post']->full_view = true;
 $user = ossn_user_by_guid($params['post']->poster_guid);
 if ($params['post']->type == 'user') {
-	$vars =  array(
-        'post' => $params['post'],
-        'text' => $text,
-        'friends' => explode(',', $data->friend),
-        'location' => $location,
-        'user' => $user,
-        'image' => $image,
-    );
+	$vars = ossn_wallpost_to_item($params['post']);
     echo ossn_wall_view_template($vars);
 }
 if ($params['post']->type == 'group') {
-	$vars = array(
-        'post' => $params['post'],
-        'text' => $text,
-        'location' => $location,
-        'user' => $user,
-        'image' => $image,
-        'show_group' => true,
-    );
+	$vars = ossn_wallpost_to_item($params['post']);
+	$vars['show_group'] = true;
     echo ossn_wall_view_template($vars);
 }
 
