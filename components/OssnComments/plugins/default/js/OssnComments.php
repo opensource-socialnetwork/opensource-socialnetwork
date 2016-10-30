48c28283e97b3f0c500af0f3e5e08efe08f3ad5a
Arsalan Shah <manactric@gmail.com>
2016-10-31 01:35:59 +0500

    Postings and comments with same behaviour #924

--- components/OssnComments/plugins/default/js/OssnComments.php
+++ components/OssnComments/plugins/default/js/OssnComments.php
@@ -8,11 +8,37 @@
  * @link      https://www.opensource-socialnetwork.org/
  */
 Ossn.PostComment = function($container) {
+    $('#comment-box-' + $container).keypress(function(e) {
+        if (e.which == 13) {
+            if (e.shiftKey === false) {
+            	//Postings and comments with same behaviour #924
+                $replace_tags = function(input, allowed) {
+                    allowed = (((allowed || '') + '').toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join('')
+                    var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi
+                    var commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi
+                    return input.replace(commentsAndPhpTags, '').replace(tags, function($0, $1) {
+                        return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : ''
+                    })
+                };
+				
+                $text = $('#comment-box-' + $container).html();
+                $text = $replace_tags($text, '<br>').replace(/\<br\\?>/g, "\n");
+                $('#comment-container-' + $container).append("<textarea name='comment' class='hidden'>" + $text + "</textarea>");
+                $('#comment-container-' + $container).submit();
+            }
+        }
+    });
+    $('#comment-box-' + $container).on('paste', function(e) {
+        e.preventDefault();
+        var text = (e.originalEvent || e).clipboardData.getData('text/plain') || prompt('Paste something..');
+        window.document.execCommand('insertText', false, text);
+    });
     Ossn.ajaxRequest({
         url: Ossn.site_url + 'action/post/comment',
         form: '#comment-container-' + $container,
         beforeSend: function(request) {
             $('#comment-box-' + $container).attr('readonly', 'readonly');
+            $('#comment-box-' + $container).attr('contenteditable', false);
         },
         callback: function(callback) {
             if (callback['process'] == 1) {
@@ -28,6 +54,8 @@ Ossn.PostComment = function($container) {
                 $('#comment-box-' + $container).removeAttr('readonly');
                 Ossn.MessageBox('syserror/unknown');
             }
+            $('#comment-box-' + $container).attr('contenteditable', true);
+            $('#comment-box-' + $container).html("");
         }
     });
 };
@@ -130,15 +158,15 @@ Ossn.CommentImage = function($container) {
 };
 Ossn.RegisterStartupFunction(function() {
     $(document).ready(function() {
-    	$('body').delegate('.comment-post', 'click', function(){
-        	var $guid = $(this).attr('data-guid');
-            if($guid){
-            	$("#comment-box-"+$guid).focus();
+        $('body').delegate('.comment-post', 'click', function() {
+            var $guid = $(this).attr('data-guid');
+            if ($guid) {
+                $("#comment-box-" + $guid).focus();
             }
         });
-    	$('body').delegate('.ossn-edit-comment', 'click', function(){
-        	var $dataguid = $(this).attr('data-guid');
-            Ossn.MessageBox('comment/edit/'+$dataguid);
-        });        
+        $('body').delegate('.ossn-edit-comment', 'click', function() {
+            var $dataguid = $(this).attr('data-guid');
+            Ossn.MessageBox('comment/edit/' + $dataguid);
+        });
     });
 });
