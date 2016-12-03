2c32fdb2dacb4182955a73c103280bddbc035498
lianglee <manactric@gmail.com>
2016-12-03 15:15:38 +0500

    v4.3.1

---
+++ upgrade/upgrades/1480759958.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (softlab24.com).ossn
+ * @author    OSSN Core Team <info@softlab24.com>
+ * @copyright 2014-2017 SOFTLAB24 LIMITED
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+
+
+ossn_generate_server_config('apache');
+ossn_version_upgrade($upgrade, '4.3.1');
+
+$factory = new OssnFactory(array(
+		'callback' => 'installation',
+		'website' => ossn_site_url(),
+		'email' => ossn_site_settings('owner_email'),
+		'version' => '4.3.1'
+));
+$factory->connect;
