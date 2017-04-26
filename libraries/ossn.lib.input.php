b9ebd66bd87fb694814d29f5a33b964753912663
githubertus <githubertus@ongolito.net>
2017-04-26 16:38:46 +0200

    asure html escaping for complete range of 4-byte unicodes

--- libraries/ossn.lib.input.php
+++ libraries/ossn.lib.input.php
@@ -54,11 +54,8 @@ function ossn_emojis_to_entites($content) {
 				$regex = '/(
 		     \x23\xE2\x83\xA3               # Digits
 		     [\x30-\x39]\xE2\x83\xA3
-		   | \xF0\x9F[\x85-\x88][\xA6-\xBF] # Enclosed characters
-		   | \xF0\x9F[\x8C-\x97][\x80-\xBF] # Misc
-		   | \xF0\x9F\x98[\x80-\xBF]        # Smilies
-		   | \xF0\x9F\x99[\x80-\x8F]
-		   | \xF0\x9F\x9A[\x80-\xBF]        # Transport and map symbols
+		   | \xF0[\x90-\xBF][\x80-\xBF][\x80-\xBF]            # range x10000 - x3FFFF
+		   | [\xF1-\xF3][\x80-\xBF][\x80-\xBF][\x80-\xBF]     # range x40000 - xFFFFF
 		)/x';
 				
 				$matches = array();
