a9e3ae864df1a5f5f5632224de087fb89fb9ef14
Arsalan Shah <manactric@gmail.com>
2017-04-27 22:45:56 +0500

    Update ossn.lib.input.php

--- libraries/ossn.lib.input.php
+++ libraries/ossn.lib.input.php
@@ -65,7 +65,7 @@ function ossn_emojis_to_entites($content) {
 										
 										$unpacked = unpack('H*', mb_convert_encoding($emoji, 'UTF-32', 'UTF-8'));
 										if (isset($unpacked[1])) {
-												$entity  = '&#x' . ltrim($unpacked[1], '0') . ';';
+												$entity  = '<i class="ossn-emojii-output">&#x' . ltrim($unpacked[1], '0') . ';</i>';
 												$content = str_replace($emoji, $entity, $content);
 										}
 								}
