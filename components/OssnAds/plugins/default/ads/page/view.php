77693c59a8103f5b9e0f1bd097b73678c673290b
Arsalan Shah <manactric@gmail.com>
2017-05-01 12:10:56 +0500

    Update view.php

--- components/OssnAds/plugins/default/ads/page/view.php
+++ components/OssnAds/plugins/default/ads/page/view.php
@@ -11,17 +11,17 @@
 $ads = new OssnAds;
 $ads = $ads->getAds();
 if ($ads) {
-		echo '<div class="ossn-ads">';
+	echo '<div class="ossn-ads">';
         foreach ($ads as $ad) {
           	$items[] = ossn_plugin_view('ads/item', array(
-											'item' => $ad, 
-					   ));
+			'item' => $ad, 
+		 ));
         }
-		echo ossn_plugin_view('widget/view', array(
-					'title' => ossn_print('sponsored'),
-					'contents' => implode('', $items),
-		));	
-		echo '</div>';
-    }
-    ?>   
+	echo ossn_plugin_view('widget/view', array(
+			'title' => ossn_print('sponsored'),
+			'contents' => implode('', $items),
+	));	
+	echo '</div>';
+}
+?>   
        
