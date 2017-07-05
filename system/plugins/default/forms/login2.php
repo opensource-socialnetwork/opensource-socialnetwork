be3b66d51eb7d684653f015c262cef36028a40c4
Malik Umer Farooq <Lablnet@users.noreply.github.com>
2017-07-05 00:59:21 -0700

    Update login2.php
    missing forwordshash in div

--- system/plugins/default/forms/login2.php
+++ system/plugins/default/forms/login2.php
@@ -20,7 +20,7 @@
 </div>
 <div>
 	<?php echo ossn_fetch_extend_views('forms/login2/before/submit'); ?>
-<div>
+</div>
 <div>
     <input type="submit" value="<?php echo ossn_print('site:login');?>" class="btn btn-primary"/>
     <a href="<?php echo ossn_site_url('resetlogin'); ?>"><?php echo ossn_print('reset:login'); ?> </a>
