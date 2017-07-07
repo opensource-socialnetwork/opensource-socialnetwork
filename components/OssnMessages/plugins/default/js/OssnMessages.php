74b47a50c926eac84df613c590e46c8c7bb02b28
Homelancer <git@homelancer.com>
2017-07-07 18:18:56 +0800

    Update OssnMessages.php
    Added the refreshList function to refresh the receiving user's message list when the sender has removed some messages from their list
    Also updates the chat box list of the sender and the recipient when a message has been removed

--- components/OssnMessages/plugins/default/js/OssnMessages.php
+++ components/OssnMessages/plugins/default/js/OssnMessages.php
@@ -69,10 +69,40 @@ Ossn.deleteMessage = function($guid, $id) {
         url: Ossn.site_url + "messages/deletemessage/" + $id,
         action: false,
         callback: function(callback) {
-			$('#message-append-' + $guid).find('#message-'+$id).closest('div.row').remove();
             if(callback){
-            	//Unwanted refresh in message window #416 , there is no need to scroll if no new message.
-	            Ossn.message_scrollMove($guid);
+				$('#message-append-' + $guid).find('#ossn-message-item-'+$id).closest('div.row').remove();
+				$('#ossn-chat-messages-data-' + $guid).find('#ossn-message-item-'+$id).remove();
+            }
+        }
+    });
+	return true;
+};
+
+
+Ossn.refreshList = function($from,$count) {
+    Ossn.PostRequest({
+        url: Ossn.site_url + "messages/fromlist/" + $from,
+        action: false,
+        callback: function(callback) {
+			$('#message-list').html(callback);
+            if(callback){
+				var list=$("#message-list ul li").size();
+				var onlist=$("#message-append-"+$from+" .message-box-recieved").size();
+				if (list!=onlist){
+					var id=0;
+					list=[];
+					$("#message-list ul li").each(function(index, item){
+						id=$(item).attr('data-id');
+						list[index]=id;
+					});
+					$("#message-append-"+$from+" .message-box-recieved").each(function(index, item){
+						id=$(item).attr('data-id');
+						if ( (id>0) && ($.inArray(id,list)===-1) ){
+							$('#message-append-' + $from).find('#ossn-message-item-'+id).closest('div.row').remove();
+							$('#ossn-chat-messages-data-' + $from).find('#ossn-message-item-'+id).remove();
+						}
+					});
+				}
             }
         }
     });
