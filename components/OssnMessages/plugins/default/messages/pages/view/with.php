4484d3bcc01bcca30fed9006d23c884a9341618f
Homelancer <git@homelancer.com>
2017-07-07 18:22:13 +0800

    Update with.php
    Added the refreshList to the timer and streamlined the code to allow the updating/refreshing of the messages when a messages has been removed

--- components/OssnMessages/plugins/default/messages/pages/view/with.php
+++ components/OssnMessages/plugins/default/messages/pages/view/with.php
@@ -3,18 +3,21 @@
             $(document).ready(function () {
                 setInterval(function () {
                     Ossn.getMessages('<?php echo $params['user']->username;?>', '<?php echo $params['user']->guid;?>');
+					Ossn.refreshList('<?php echo $params['user']->guid;?>');
                     //Ossn.getRecent('<?php echo $params['user']->guid;?>');
                 }, 5000);
                	Ossn.message_scrollMove(<?php echo $params['user']->guid;?>);
-		    
-		$("#message-append-<?php echo $params['user']->guid;?>").on("click",".message-action", function(){
-			var id=$(this).attr('data-id');
-			Ossn.deleteMessage('<?php echo $params['user']->guid;?>', id);
-		});
-		    
-      });
+
+				$("#message-append-<?php echo $params['user']->guid;?>").on("click",".message-action", function(){
+				var id=$(this).closest(".message-box-sent").attr('data-id');
+				Ossn.deleteMessage('<?php echo $params['user']->guid;?>', id);
+			});
+    });
+	  
 </script>
 <div class="message-with">
+<div id="message-list"></div>
+
 <div class="message-inner" id="message-append-<?php echo $params['user']->guid; ?>">
 <?php
 if ($params['data']) {
@@ -24,15 +27,16 @@ if ($params['data']) {
 					?>
                     	<div class="row">
                                 <div class="col-md-10">
-                                		<div class="message-box-sent text">
+                                		<div class="message-box-sent text" style="position:relative;" id="ossn-message-item-<?php echo $message->id;?>" data-id="<?php echo $message->id;?>">
                                 			<?php
                                					 if (function_exists('smilify')) {
                                 					    echo smilify(ossn_message_print($message->message));
                              					   } else {
                                 					    echo ossn_message_print($message->message);
                             					    }
-                             				?>
-					<a id="message-<?php echo $message->id;?>" class="message-action" data-id="<?php echo $message->id;?>" href="#" title="Delete Message"><i class="fa fa-times"></i></a>
+
+                              				?>
+											<a href="#" title="Delete Message" class="message-action"><i class="fa fa-times"></i></a>
                                         <div class="time-created"><?php echo ossn_user_friendly_time($message->time);?></div>    
                                         </div>
                                 </div>
@@ -48,7 +52,7 @@ if ($params['data']) {
                                 	<img  class="user-icon" src="<?php echo $user->iconURL()->small;?>" />
                                 </div>                                
                                 <div class="col-md-10">
-                                		<div class="message-box-recieved text">
+                                		<div class="message-box-recieved text" id="ossn-message-item-<?php echo $message->id;?>" data-id="<?php echo $message->id;?>">
                                 			<?php
                                					 if (function_exists('smilify')) {
                                 					    echo smilify(ossn_message_print($message->message));
