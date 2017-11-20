e1be543e0c552ae71b2c33250aa15b6757670492
githubertus <githubertus@ongolito.net>
2017-11-21 00:42:09 +0100

    keep track of OssnSmilies backward compatibility setting

--- components/OssnMessages/plugins/default/messages/pages/view/with.php
+++ components/OssnMessages/plugins/default/messages/pages/view/with.php
@@ -18,18 +18,12 @@ if ($params['data']) {
 					?>
                     	<div class="row">
                                 <div class="col-md-10">
-                                		<div class="message-box-sent text">
-                                			<?php
-                               					 if (function_exists('smilify')) {
-                                					    echo smilify(ossn_message_print($message->message));
-                             					   } else {
-                                					    echo ossn_message_print($message->message);
-                            					    }
-                              				?>
-                                        <div class="time-created"><?php echo ossn_user_friendly_time($message->time);?></div>    
+                                	<div class="message-box-sent text">
+						<?php echo ossn_call_hook('messages', 'message:smilify', null, ossn_message_print($message->message)); ?>
+                                        	<div class="time-created"><?php echo ossn_user_friendly_time($message->time);?></div>    
                                         </div>
                                 </div>
-                        		<div class="col-md-2">
+                        	<div class="col-md-2">
                                 	<img  class="user-icon" src="<?php echo $user->iconURL()->small;?>" />
                                 </div>                                
                         </div>
@@ -37,19 +31,13 @@ if ($params['data']) {
 					} else {
 						?>
                     	<div class="row">
-                        		<div class="col-md-2">
+                        	<div class="col-md-2">
                                 	<img  class="user-icon" src="<?php echo $user->iconURL()->small;?>" />
                                 </div>                                
                                 <div class="col-md-10">
-                                		<div class="message-box-recieved text">
-                                			<?php
-                               					 if (function_exists('smilify')) {
-                                					    echo smilify(ossn_message_print($message->message));
-                             					   } else {
-                                					    echo ossn_message_print($message->message);
-                            					    }
-                              				?>
-                                        <div class="time-created"><?php echo ossn_user_friendly_time($message->time);?></div>                                                
+                                	<div class="message-box-recieved text">
+						<?php echo ossn_call_hook('messages', 'message:smilify', null, ossn_message_print($message->message)); ?>
+                                        	<div class="time-created"><?php echo ossn_user_friendly_time($message->time);?></div>                                                
                                         </div>
                                 </div>
                         </div>                       
