cf0a3e21cdc05314b54190f51c89b066159d8b9c
Arsalan Shah <manactric@gmail.com>
2017-12-25 01:02:33 +0500

    OssnNotifications improvement #1231

--- components/OssnNotifications/plugins/default/notifications/pages/all.php
+++ components/OssnNotifications/plugins/default/notifications/pages/all.php
@@ -8,16 +8,26 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
-?>
-    <h2><?php echo ossn_print('notifications'); ?></h2>
-<?php
+
 $get = new  OssnNotifications;
-$notifications = $get->get(ossn_loggedin_user()->guid);
-echo '<div class="ossn-notifications-all ossn-notification-page">';
-if ($notifications) {
-    foreach ($notifications as $not) {
-        echo "{$not}";
-    }
+$notifications = $get->searchNotifications(array(
+					'owner_guid' => ossn_loggedin_user()->guid,	
+					'offset' => input('offset', '', 1),
+					'order_by' => 'n.guid DESC',
+));
+$count = $get->searchNotifications(array(
+					'owner_guid' => ossn_loggedin_user()->guid,
+					'count' => true,
+));
+if($notifications){
+	$list = '<div class="ossn-notifications-all ossn-notification-page">';
+	foreach($notifications as $item){
+			$list .= $item->toTemplate();	
+	}
+	$list .= "</div>";
 }
-echo '</div>';
-?>
+$pagination = ossn_view_pagination($count);
+echo ossn_plugin_view('widget/view', array(
+				'title' => ossn_print('notifications'),
+				'contents' => $list . $pagination,
+));
