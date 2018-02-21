99daa141f0f55937e225575f7a2956162abc8d7e
Malik Umer Farooq <Lablnet01@gmail.com>
2018-02-21 16:50:06 +0500

    Update embed.php

--- components/OssnWall/plugins/default/wall/embed.php
+++ components/OssnWall/plugins/default/wall/embed.php
@@ -2,14 +2,23 @@
 /**
  * Open Source Social Network
  *
- * @package   (softlab24.com).ossn
- * @author    OSSN Core Team <info@softlab24.com>
+ * @package Open Source Social Network
+ * @author    Open Social Website Core Team <info@softlab24.com>
  * @copyright 2014-2017 SOFTLAB24 LIMITED
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
- 
- ?>
- <div>
-    <input type="text"   value="<iframe src='<?= ossn_site_url(); ?>/embede/<?= $params['post']->guid;?>' width='500' height='445' style='border:none;overflow:hidden' scrolling='yes' frameborder='0' allowTransparency='true'></iframe>"  style='width:100%;'/>
- </div>
+$post_guid = $params['guid'][0];
+
+$wall_obj = new OssnWall;
+
+$posts = $wall_obj->GetPost($post_guid);
+
+if($posts->access !== '3'){
+
+	$vars = ossn_wallpost_to_item($posts);
+    echo ossn_wall_view_template($vars);
+
+}else{
+	echo trigger_error("This post have friend only privacy", 'denied');
+}
