206fdfe36817a5bd6599667f1ed59a03b14b0feb
Arsalan Shah <manactric@gmail.com>
2018-07-10 22:21:23 +0500

    Update start.php

--- system/start.php
+++ system/start.php
@@ -8,13 +8,15 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      http://www.informatikon.com/
  */
-session_start();
-
+//Restrict calling start.php directly from browser #1315
+if(!defined("OSSN_ALLOW_SYSTEM_START")){
+	header("HTTP/1.0 404 Not Found");	
+	exit;
+}
 global $Ossn;
 if (!isset($Ossn)) {
     $Ossn = new stdClass;
 }
-
 include_once(dirname(dirname(__FILE__)) . '/libraries/ossn.lib.route.php');
 
 if (!is_file(ossn_route()->configs . 'ossn.config.site.php') && !is_file(ossn_route()->configs . 'ossn.config.db.php')) {
@@ -32,6 +34,8 @@ foreach ($Ossn->classes as $class) {
         throw new exception('Cannot include all classes');
     }
 }
+//Load session start after classes #1318
+session_start();
 foreach ($Ossn->libraries as $lib) {
     if (!include_once(ossn_route()->libs . "ossn.lib.{$lib}.php")) {
         throw new exception('Cannot include all libraries');
