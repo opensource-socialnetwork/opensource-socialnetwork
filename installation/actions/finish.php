e9be209b418a84720463fdcc738763cdad80aaad
Arsalan Shah <manactric@gmail.com>
2018-07-10 22:44:28 +0500

    Update finish.php

--- installation/actions/finish.php
+++ installation/actions/finish.php
@@ -8,7 +8,7 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
-
+define('OSSN_ALLOW_SYSTEM_START', TRUE);
 require_once(dirname(dirname(dirname(__FILE__))) . '/system/start.php');
 file_put_contents(ossn_installation_paths()->root . 'INSTALLED', 1);
 
