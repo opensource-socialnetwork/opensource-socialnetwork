c9985473d58d93bbc91d02fd6304b9ee5fa99115
Arsalan Shah <manactric@gmail.com>
2018-07-30 17:08:43 +0500

    Update ossn.lib.route.php

--- libraries/ossn.lib.route.php
+++ libraries/ossn.lib.route.php
@@ -18,20 +18,20 @@
  * @return object
  */
 function arrayObject($array, $class = 'stdClass') {
-    $object = new $class;
-    if (empty($array)) {
-        return false;
-    }
-    foreach ($array as $key => $value) {
-        if (strlen($key)) {
-            if (is_array($value)) {
-                $object->{$key} = arrayObject($value, $class);
-            } else {
-                $object->{$key} = $value;
-            }
-        }
-    }
-    return $object;
+		$object = new $class;
+		if(empty($array)) {
+				return false;
+		}
+		foreach($array as $key => $value) {
+				if(strlen($key)) {
+						if(is_array($value)) {
+								$object->{$key} = arrayObject($value, $class);
+						} else {
+								$object->{$key} = $value;
+						}
+				}
+		}
+		return $object;
 }
 
 /**
@@ -44,10 +44,9 @@ function arrayObject($array, $class = 'stdClass') {
  * @return object
  */
 function forceObject(&$object) {
-    if (!is_object($object) && gettype($object) == 'object'
-    )
-        return ($object = unserialize(serialize($object)));
-    return $object;
+		if(!is_object($object) && gettype($object) == 'object')
+				return ($object = unserialize(serialize($object)));
+		return $object;
 }
 
 /**
@@ -56,27 +55,27 @@ function forceObject(&$object) {
  * @return object
  */
 function ossn_route() {
-    $root = str_replace("\\", "/", dirname(dirname(__FILE__)));
-    $defaults = array(
-        'www' => "$root/",
-        'libs' => "$root/libraries/",
-        'classes' => "$root/classes/",
-        'actions' => "$root/actions/",
-        'locale' => "$root/locale/",
-        'sys' => "$root/system/",
-        'configs' => "$root/configurations/",
-        'themes' => "$root/themes/",
-        'pages' => "$root/pages/",
-        'com' => "$root/components/",
-        'admin' => "$root/admin/",
-        'forms' => "$root/forms/",
-        'upgrade' => "$root/upgrade/",
-        'cache' => "{$root}/cache/",
-        'js' => "$root/javascripts/",
-	'system' => "$root/system/",
-	'components' => "$root/components",
-    );
-    return arrayObject($defaults);
+		$root     = str_replace("\\", "/", dirname(dirname(__FILE__)));
+		$defaults = array(
+				'www' => "$root/",
+				'libs' => "$root/libraries/",
+				'classes' => "$root/classes/",
+				'actions' => "$root/actions/",
+				'locale' => "$root/locale/",
+				'sys' => "$root/system/",
+				'configs' => "$root/configurations/",
+				'themes' => "$root/themes/",
+				'pages' => "$root/pages/",
+				'com' => "$root/components/",
+				'admin' => "$root/admin/",
+				'forms' => "$root/forms/",
+				'upgrade' => "$root/upgrade/",
+				'cache' => "{$root}/cache/",
+				'js' => "$root/javascripts/",
+				'system' => "$root/system/",
+				'components' => "$root/components"
+		);
+		return arrayObject($defaults);
 }
 
 /**
@@ -87,20 +86,51 @@ function ossn_route() {
  * @return object
  */
 function current_url($uport = '') {
-    $protocol = 'http';
-    $uri = $_SERVER['REQUEST_URI'];
-    if ($uport == true) {
-        $uri = substr($uri, 0, $uri);
-    }
-    if (!empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") {
-        $protocol = 'https';
-    }
-    $port = ':' . $_SERVER["SERVER_PORT"];
-    if ($port == ':80' || $port == ':443') {
-        if ($uport == true) {
-            $port = '';
-        }
-    }
-    $url = "$protocol://{$_SERVER['SERVER_NAME']}$port{$uri}";
-    return $url;
-}		
+		$protocol = 'http';
+		$uri      = $_SERVER['REQUEST_URI'];
+		if($uport == true) {
+				$uri = substr($uri, 0, $uri);
+		}
+		if(!empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") {
+				$protocol = 'https';
+		}
+		$port = ':' . $_SERVER["SERVER_PORT"];
+		if($port == ':80' || $port == ':443') {
+				if($uport == true) {
+						$port = '';
+				}
+		}
+		$url = "$protocol://{$_SERVER['SERVER_NAME']}$port{$uri}";
+		return $url;
+}
+/**
+ * Register a class for autoloading 
+ *
+ * @param array $classes A classes list with the path
+ * 
+ * @return void
+ */
+function ossn_register_class(array $classes = array()) {
+		global $Ossn;
+		foreach($classes as $name => $class) {
+				if(!empty($name) && file_exists($class)) {
+						$Ossn->classes[$name] = $class;
+				} else {
+						throw new Exception("Unable to register a class `{$name}` with non-existing physical class file at location `{$class}`");
+				}
+		}
+}
+/**
+ * Auto loading classes
+ *
+ * @param string $name of the class
+ * 
+ * @return void
+ */
+function ossn_autoload_classes($name = '') {
+		global $Ossn;
+		if(isset($Ossn->classes[$name]) && file_exists($Ossn->classes[$name])) {
+				require_once($Ossn->classes[$name]);
+		}
+}
+spl_autoload_register('ossn_autoload_classes');
