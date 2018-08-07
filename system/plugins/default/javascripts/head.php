3f508b66eb5f91c42654b494160d81098741342c
Arsalan Shah <manactric@gmail.com>
2018-08-07 23:06:37 +0500

    Allow the cache tag for the images #1314

--- system/plugins/default/javascripts/head.php
+++ system/plugins/default/javascripts/head.php
@@ -8,14 +8,20 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
- $timestamp = time();
- $token = ossn_generate_action_token($timestamp);
- 
- $token = array(
-			'ossn_ts' => $timestamp,
-			'ossn_token' => $token,
-			);
- $configs = array('token' => $token);
+$timestamp = time();
+$token     = ossn_generate_action_token($timestamp);
+
+$token   = array(
+		'ossn_ts' => $timestamp,
+		'ossn_token' => $token
+);
+$configs = array(
+		'token' => $token,
+		'cache' => array(
+				'last_cache' => hash('crc32b', ossn_site_settings('last_cache')),
+				'ossn_cache' => ossn_site_settings('cache')
+		)
+);
 ?> 	
 	Ossn.site_url = '<?php echo ossn_site_url();?>';
 	Ossn.Config = <?php echo json_encode($configs);?>;
