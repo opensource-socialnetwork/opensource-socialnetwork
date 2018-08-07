8209aa2209945261508cb5c8a6b1aa6cdcf6bc97
Arsalan Shah <manactric@gmail.com>
2018-08-07 15:55:03 +0500

    allow to call system start in upgrade

--- upgrade/upgrade.php
+++ upgrade/upgrade.php
@@ -12,6 +12,7 @@
 $upgrading = dirname(dirname(__FILE__)). '/_upgrading_process';
 file_put_contents($upgrading, 1);
 
+define('OSSN_ALLOW_SYSTEM_START', TRUE);
 require_once(dirname(dirname(__FILE__)) . '/system/start.php');
 //redirect user after all upgrades
 if(ossn_trigger_upgrades()){
