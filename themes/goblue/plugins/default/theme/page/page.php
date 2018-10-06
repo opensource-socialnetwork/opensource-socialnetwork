7471b1714370a33ec6f338929efb49bb893a3e0a
Arsalan Shah <manactric@gmail.com>
2018-10-06 12:43:11 +0500

    Update page.php

--- themes/goblue/plugins/default/theme/page/page.php
+++ themes/goblue/plugins/default/theme/page/page.php
@@ -26,7 +26,7 @@ if (isset($params['contents'])) {
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title><?php echo $title; ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />0
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="<?php echo ossn_theme_url();?>images/favicon.ico" type="image/x-icon" />
 	
     <?php echo ossn_fetch_extend_views('ossn/endpoint'); ?>
