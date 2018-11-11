6b3480414ca83601e8395a55765e9bff7c7ff94f
githubertus <githubertus@ongolito.net>
2018-11-11 18:05:45 +0100

    handle error cases correctly
    (same way as with wall posts)

--- components/OssnComments/actions/comment/edit.php
+++ components/OssnComments/actions/comment/edit.php
@@ -12,27 +12,23 @@ $guid    = input('guid');
 $text    = input('comment');
 $comment = ossn_get_annotation($guid);
 
-if(!$comment || empty($text)) {
-		ossn_trigger_message(ossn_print('comment:edit:failed'), 'error');
-		redirect(REF);
-}
-//editing, then saving a comment gives warning #685
-$comment->data	= new stdClass;
-if($comment->type == 'comments:entity') {
-		$comment->data->{'comments:entity'} = $text;
-} elseif($comment->type == 'comments:post') {
-		$comment->data->{'comments:post'} = $text;
-}
-$user = ossn_loggedin_user();
-if(($comment->owner_guid == $user->guid || $user->canModerate()) && $comment->save()) {
-		$params               = array();
-		$params['text']       = $text;
-		$params['annotation'] = $comment;
-		ossn_trigger_callback('comment', 'edited', $params);
+if($comment && (strlen($text) || $comment->{'file:comment:photo'})) {
+		//editing, then saving a comment gives warning #685
+		$comment->data	= new stdClass;
+		if($comment->type == 'comments:entity') {
+				$comment->data->{'comments:entity'} = $text;
+		} elseif($comment->type == 'comments:post') {
+				$comment->data->{'comments:post'} = $text;
+		}
+		$user = ossn_loggedin_user();
+		if(($comment->owner_guid == $user->guid || $user->canModerate()) && $comment->save()) {
+				$params               = array();
+				$params['text']       = $text;
+				$params['annotation'] = $comment;
+				ossn_trigger_callback('comment', 'edited', $params);
 		
-		ossn_trigger_message(ossn_print('comment:edit:success'));
-		redirect(REF);
-} else {
-		ossn_trigger_message(ossn_print('comment:edit:failed'), 'error');
-		redirect(REF);
+				ossn_trigger_message(ossn_print('comment:edit:success'));
+				return;
+		}
 }
+ossn_trigger_message(ossn_print('comment:edit:failed'), 'error');
