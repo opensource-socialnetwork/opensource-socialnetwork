99a76c2dd1d867350a2e82e79743af9e3b5966bb
Arsalan Shah <manactric@gmail.com>
2018-11-15 20:56:26 +0500

    Update login.php

--- system/plugins/default/pages/administrator/contents/login.php
+++ system/plugins/default/pages/administrator/contents/login.php
@@ -10,7 +10,7 @@
  */
 
 echo '<div class="ossn-admin-login">';
-echo '<h3>' . ossn_print('administration') . '</h3>';
+echo '<div class="page-title">' . ossn_print('administration') . '</div>';
 echo ossn_view_form('admin/login', array(
     'action' => ossn_site_url('action/admin/login'),
     'class' => 'ossn-admin-form ossn-login-form',
