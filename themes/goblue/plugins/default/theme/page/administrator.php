5dce8d6bd56e636f90c01807af961f8e760b0370
Arsalan Shah <manactric@gmail.com>
2018-11-15 18:26:26 +0500

    Update administrator.php

--- themes/goblue/plugins/default/theme/page/administrator.php
+++ themes/goblue/plugins/default/theme/page/administrator.php
@@ -26,6 +26,7 @@ if (isset($params['contents'])) {
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="<?php echo ossn_add_cache_to_url(ossn_theme_url().'images/favicon.ico');?>" type="image/x-icon" />	
     
     <title><?php echo $title; ?></title>
     
