22cb51a6bdd1cb7413005ce0759019645818b892
githubertus <githubertus@ongolito.net>
2018-12-20 17:31:49 +0100

    display 'Group About' on mobile devices

---
+++ components/OssnGroups/plugins/default/groups/pages/about.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package Open Source Social Network
+ * @author    Open Social Website Core Team <info@softlab24.com>
+ * @copyright (C) SOFTLAB24 LIMITED
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+?>
+<div class="row">
+	<div class="col-md-12">
+		<?php echo nl2br($params['group']->description); ?>
+	</div>
+</div>
