3f84e5705c53e7dcbb4c7170c1613a86c0811796
Arsalan Shah <manactric@gmail.com>
2019-02-24 05:23:45 +0500

    Allow checkbox input
          echo ossn_plugin_view('input/checkbox', array(
    		 	'name' => 'check_box',
    			'value' => true,
          ));

---
+++ system/plugins/default/input/checkbox.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (softlab24.com).ossn
+ * @author    OSSN Core Team <info@softlab24.com>
+ * @copyright (C) SOFTLAB24 LIMITED
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+
+$class = 'ossn-checkbox-input';
+if(isset($params['class'])){
+	$class = $class . $params['class'];
+}
+$value = (isset($params['value'])) ? $params['value'] : '';
+$vars  = array();
+
+$defaults = array(
+	'disabled' => false,
+	'class' => $class,
+	'type' => 'checkbox',
+);
+
+$params = array_merge($defaults, $params);
+unset($params['value']);
+$attributes = ossn_args($params);
+if(!empty($value) && $value == true){
+		$vars['checked'] = 'checked';
+}
+$vars = ossn_args($vars);
+echo  "<div class='checkbox-block'><input {$attributes} {$vars} /><span>{$label}</span></div>";
