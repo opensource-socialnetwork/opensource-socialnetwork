059b653996e3a025f5caace7c0ea610fb02dc35b
githubertus <githubert@zuelsdorff.de>
2019-03-21 22:56:58 +0100

    Allow changing of group owner
    1. 'Make group owner' button added
    2. 'Are you sure' confirm dialog added to 'Remove' button
    3. Both buttons moved to separate div in order to avoid conflicts with longer member names

--- components/OssnGroups/plugins/default/groups/pages/members.php
+++ components/OssnGroups/plugins/default/groups/pages/members.php
@@ -18,27 +18,38 @@ if ($members) {
             	<div class="col-md-2 col-sm-2 hidden-xs">
     	        		<img src="<?php echo $user->iconURL()->large; ?>" width="100" height="100"/>
 				</div>
-                <div class="col-md-10 col-sm-10 col-xs-12">
-	    	        <div class="uinfo">
-                        <?php
-							echo ossn_plugin_view('output/url', array(
-									'text' => $user->fullname,
-									'href' =>  $user->profileURL(),
-									'class' => 'userlink',
-							));						
-						?>
-        	   		</div>
-                    <div class="right request-controls">
-	                    <?php
-							if ((ossn_isAdminLoggedin() || ossn_loggedin_user()->guid == $params['group']->owner_guid) && $user->guid !== $params['group']->owner_guid && $params['group']->isMember($params['group']->guid, $user->guid)) {
-									echo ossn_plugin_view('output/url', array(
-										'text' => ossn_print('group:memb:remove'),
-										'href' =>  ossn_site_url("action/group/member/decline?group={$params['group']->guid}&user={$user->guid}", true),
-										'class' => 'btn btn-danger',
-								));
-							}
-						?>		
-                   </div> 
+                <div>
+                   <div class="col-md-10 col-sm-10 col-xs-12">
+    	    	        <div class="uinfo">
+                          <?php
+	    						echo ossn_plugin_view('output/url', array(
+	    								'text' => $user->fullname,
+	    								'href' =>  $user->profileURL(),
+	    								'class' => 'userlink',
+	    						));						
+	    					?>
+             	   		</div>
+                    </div>
+                    <div class="col-md-10 col-sm-10 col-xs-12">
+                        <div class="right request-controls">
+	                        <?php
+	    						if ((ossn_isAdminLoggedin() || ossn_loggedin_user()->guid == $params['group']->owner_guid) && $user->guid !== $params['group']->owner_guid && $params['group']->isMember($params['group']->guid, $user->guid)) {
+	    								echo ossn_plugin_view('output/url', array(
+	    									'text' => ossn_print('group:memb:remove'),
+	    									'href' =>  ossn_site_url("action/group/member/decline?group={$params['group']->guid}&user={$user->guid}", true),
+		    								'class' => 'btn btn-warning btn-responsive ossn-make-sure'
+		    						));
+						        		echo ossn_plugin_view('output/url', array(
+					    					'data-new-owner' => $user->fullname,
+					    					'data-is-admin' => ossn_isAdminLoggedin(),
+						    				'text' => ossn_print('group:memb:make:owner'),
+						    				'href' =>  ossn_site_url("action/group/change_owner?group={$params['group']->guid}&user={$user->guid}", true),
+						    				'class' => 'btn btn-danger btn-responsive ossn-group-change-owner'
+						    		));
+		    					}
+		    				?>		
+                        </div>
+                    </div>
                </div>
             </div>           
         </div>
