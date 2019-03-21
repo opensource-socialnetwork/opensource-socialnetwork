80e18ddfac8b93d721e8a1e66ecea677bd36c11c
githubertus <githubert@zuelsdorff.de>
2019-03-21 21:39:03 +0100

    Allow changing of group owner
    new action added

---
+++ components/OssnGroups/actions/group/change_owner.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   Open Source Social Network
+ * @author    Open Social Website Core Team <info@softlab24.com>
+ * @copyright (C) SOFTLAB24 LIMITED
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+
+$new_owner = input('user');
+$group     = ossn_get_group_by_guid(input('group'));
+if ($group->owner_guid !== ossn_loggedin_user()->guid && !ossn_isAdminLoggedin()) {
+    ossn_trigger_message(ossn_print('group:update:fail'), 'error');
+    redirect(REF);
+}
+
+if ($group->changeOwner($new_owner, $group->guid)) {
+    ossn_trigger_message(ossn_print('group:updated'));
+    redirect("group/{$group->guid}/members");
+} else {
+    ossn_trigger_message(ossn_print('group:update:fail'), 'error');
+    redirect(REF);
+}
