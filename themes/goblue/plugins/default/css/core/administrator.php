4d7317120f8c5730e47086365ff51045d1f13030
Arsalan Shah <manactric@gmail.com>
2019-07-14 16:27:27 +0500

    Update administrator.php

--- themes/goblue/plugins/default/css/core/administrator.php
+++ themes/goblue/plugins/default/css/core/administrator.php
@@ -327,3 +327,183 @@ footer a {
     	    padding-left: 15px;
     }
 }
+
+/*************************************
+	0ssn modal box
+***************************************/
+
+.ossn-halt {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	z-index: 10000;
+	background-color: #000;
+	opacity: 0.9;
+	cursor: auto;
+	height: 100%;
+	display: none;
+}
+
+.ossn-light {
+	opacity: 0.4;
+}
+.ossn-halt {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	z-index: 10000;
+	background-color: #000;
+	opacity: 0.9;
+	cursor: auto;
+	height: 100%;
+	display: none;
+}
+
+.image-block img {
+	max-width: 700px;
+}
+
+.ossn-message-box {
+	width: 470px;
+	min-width: 470px;
+	min-height: 96px;
+	background: #fff;
+	border: 1px solid #999;
+	position: fixed;
+	top: 0px;
+	left: 0px;
+	right: 0px;
+	margin-left: auto;
+	margin-right: auto;
+	z-index: 60000;
+	margin-top: 100px;
+	border-radius: 3px;
+	display: none;
+	box-shadow: 0 2px 26px rgba(0, 0, 0, 0.3), 0 0 0 1px rgba(0, 0, 0, 0.1);
+}
+
+.ossn-message-box .close-box {
+	float: right;
+	color: #ccc;
+	cursor: pointer;
+}
+
+.ossn-message-box .title {
+	background: #F5F6F7;
+	padding: 11px;
+	border-radius: 3px;
+	border-bottom: 1px solid #E5E5E5;
+	color: #5E5656;
+	font-size: 14px;
+	font-weight: bold;
+}
+
+.ossn-message-box .contents {
+	padding: 10px;
+	min-height: 150px;
+	max-height: 420px;
+	overflow-x: auto;
+	overflow: overlay;
+	overflow-x: -moz-hidden-unscrollable
+}
+
+.ossn-message-box .control {
+	margin-left: 10px;
+	margin-right: 10px;
+	height: 45px;
+	padding: 10px;
+	border-top: 1px solid #E9EAED;
+}
+
+.ossn-message-box .control .controls {
+	float: right;
+}
+
+.ossn-message-box .control .controls .btn {
+	padding: 2px 13px;
+	border-radius: 2px;
+}
+
+.ossn-message-box .contents input[type='text'] {
+	border: 1px solid #EEE;
+	width: 292px;
+	padding: 7px;
+}
+
+.ossn-message-box .contents input[type='text'],
+.ossn-message-box .contents label {
+	display: inline-table;
+}
+
+.ossn-message-box .contents label {
+	color: #666;
+	font-weight: bold;
+	font-size: 13px;
+	margin-right: 13px;
+}
+********************************
+	Loading Icon
+    @source: https://github.com/jlong/css-spinners
+*********************************/
+
+@-moz-keyframes three-quarters-loader {
+	0% {
+		-moz-transform: rotate(0deg);
+		transform: rotate(0deg);
+	}
+	100% {
+		-moz-transform: rotate(360deg);
+		transform: rotate(360deg);
+	}
+}
+
+@-webkit-keyframes three-quarters-loader {
+	0% {
+		-webkit-transform: rotate(0deg);
+		transform: rotate(0deg);
+	}
+	100% {
+		-webkit-transform: rotate(360deg);
+		transform: rotate(360deg);
+	}
+}
+
+@keyframes three-quarters-loader {
+	0% {
+		-moz-transform: rotate(0deg);
+		-ms-transform: rotate(0deg);
+		-webkit-transform: rotate(0deg);
+		transform: rotate(0deg);
+	}
+	100% {
+		-moz-transform: rotate(360deg);
+		-ms-transform: rotate(360deg);
+		-webkit-transform: rotate(360deg);
+		transform: rotate(360deg);
+	}
+}
+
+
+/* :not(:required) hides this rule from IE9 and below */
+
+.ossn-loading:not(:required) {
+	-moz-animation: three-quarters-loader 1250ms infinite linear;
+	-webkit-animation: three-quarters-loader 1250ms infinite linear;
+	animation: three-quarters-loader 1250ms infinite linear;
+	border: 8px solid #38e;
+	border-right-color: transparent;
+	border-radius: 16px;
+	box-sizing: border-box;
+	position: relative;
+	overflow: hidden;
+	text-indent: -9999px;
+	width: 24px;
+	height: 24px;
+}
+
+.ossn-box-loading {
+	margin-left: 216px;
+	margin-top: 37px;
+}
