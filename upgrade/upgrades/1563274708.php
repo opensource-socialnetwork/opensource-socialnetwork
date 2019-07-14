b535779e262b91fda34ac927e4c443dc422ffc84
Arsalan Shah <manactric@gmail.com>
2019-07-14 16:03:51 +0500

    Create 1563274708.php

---
+++ upgrade/upgrades/1563274708.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (softlab24.com).ossn
+ * @author    OSSN Core Team <info@softlab24.com>
+ * @copyright (C) SOFTLAB24 LIMITED
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+ossn_generate_server_config('apache');
+ossn_version_upgrade($upgrade, '5.1');
+
+$new_components = array(
+		'OssnAutoPagination'
+);
+
+$factory = new OssnFactory(array(
+		'callback' => 'installation',
+		'website' => ossn_site_url(),
+		'email' => ossn_site_settings('owner_email'),
+		'version' => '5.1'
+));
+$factory->connect;
