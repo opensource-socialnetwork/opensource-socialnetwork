603042b26f4cc1580cbe7b92b276c150387f1633
Arsalan Shah <manactric@gmail.com>
2019-08-20 17:03:08 +0500

    Update opensource-socialnetwork.sql

--- installation/sql/opensource-socialnetwork.sql
+++ installation/sql/opensource-socialnetwork.sql
@@ -188,7 +188,7 @@ INSERT INTO `ossn_site_settings` (`setting_id`, `name`, `value`) VALUES
 (9, 'display_errors', 'off'),
 (10, 'site_key', '<<secret>>'),
 (11, 'last_cache', ''),
-(12, 'site_version', '5.0');
+(12, 'site_version', '5.1');
 
 
 -- --------------------------------------------------------
