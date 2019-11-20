565859e8fc53546995a9afbfd66453af46bed772
Arsalan Shah <manactric@gmail.com>
2019-11-20 18:58:26 +0500

    Update system_messages.php

--- themes/goblue/plugins/default/theme/page/elements/system_messages.php
+++ themes/goblue/plugins/default/theme/page/elements/system_messages.php
@@ -8,15 +8,17 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
- 	$col = "col-md-11";
-	if($params['admin'] === true){
-		$col = "col-md-12";
-	}
+$col = "col-md-11";
+if($params['admin'] === true){
+	$col = "col-md-12";
+}
  ?>
 <div class="ossn-system-messages">
-   <div class="row">
-	   <div class="<?php echo $col;?> ossn-system-messages-inner">
-    		<?php echo ossn_display_system_messages(); ?>
-   		</div>
+	   <div class="row">
+	  	 <div class="<?php echo $col;?>">
+       			<div class="ossn-system-messages-inner">
+    				<?php echo ossn_display_system_messages(); ?>
+            		</div>
+	   	</div>
 	</div>
 </div>    
