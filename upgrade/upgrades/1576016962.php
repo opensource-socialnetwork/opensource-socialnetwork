f500a8795fe4c011d1d27224cc24e9e917f425af
Arsalan Shah <manactric@gmail.com>
2019-12-01 03:32:45 +0500

    1576016962.php

---
+++ upgrade/upgrades/1576016962.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (softlab24.com).ossn
+ * @author    OSSN Core Team <info@softlab24.com>
+ * @copyright (C) SOFTLAB24 LIMITED
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+$database = new OssnDatabase;
+$database->statement("ALTER TABLE `ossn_likes` ADD `subtype` VARCHAR(10) NOT NULL AFTER `type`;");
+$database->execute();
+
+$factory = new OssnFactory(array(
+		'callback' => 'installation',
+		'website' => ossn_site_url(),
+		'email' => ossn_site_settings('owner_email'),
+		'version' => '5.2'
+));
+$factory->connect;
