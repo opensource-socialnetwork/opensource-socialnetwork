5b136ff344510861dbd07d596367af552d93c0a2
githubertus <githubert@zuelsdorff.de>
2019-12-06 11:37:46 +0100

    Update ossn.fr.php

--- locale/ossn.fr.php
+++ locale/ossn.fr.php
@@ -292,7 +292,9 @@ S'il vous plaît cliquez sur le lien ci-dessous pour réinitialiser le mot de pa
 	'female:gendercolor' => '#ED008C',
 	'ossn:admin:settings:off' => 'désactivé',
 	'ossn:admin:settings:on' => 'activée',
-    /*
+  	'ossn:admin:settings:saved' => 'Paramètres enregistrés!',
+	'ossn:admin:settings:save:error' => 'Vous ne pouvez pas enregistrer les paramètres! Veuillez vérifier votre fichier error_log.',
+  /*
        * List of ISO 639-1 language codes
        * http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
        */
