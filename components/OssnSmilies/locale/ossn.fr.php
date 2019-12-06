20189567493f3e09bbac71d7819a532d718f326b
githubertus <githubert@zuelsdorff.de>
2019-12-06 14:35:36 +0100

    added strings for 'Close box on clicking anywhere' setting

--- components/OssnSmilies/locale/ossn.fr.php
+++ components/OssnSmilies/locale/ossn.fr.php
@@ -10,16 +10,14 @@
  */
 $en = array(
 		'ossnsmilies' => 'Smilies',
-		'ossn:smilies:admin:settings:compat:title' => 'Backward compatibility mode',
-		'ossn:smilies:admin:settings:compat:note' => '<i class="fa fa-info-circle"></i><br><i>
-		Leave this mode <b><u>disabled</u></b> when starting a new social network from scratch with Ossn 4.5 and higher.<br>
-		A complete set of more than 1000 unicode emojis is available now, thus using old style smilies like :) has become obsolete.<br>
-		With backward compatibility mode <b><u>enabled</u></b> the following list of old style smilies will be replaced as before:</i><br>
-		:(&#x1f641;&nbsp;&nbsp;:)&#x1f642;&nbsp;&nbsp;=D&#x1f600;&nbsp;&nbsp;;)&#x1f609;&nbsp;&nbsp;:p&#x1f61b;&nbsp;&nbsp;8)&#x1f60e;&nbsp;&nbsp;o.O&#x1f62f;&nbsp;&nbsp;:O&#x1f632;&nbsp;&nbsp;:*&#x1f618;&nbsp;&nbsp;a:&#x1f607;&nbsp;&nbsp;:h:&#x2764;&nbsp;&nbsp;3:|&#x1f608;&nbsp;&nbsp;u:&#x1f620;&nbsp;&nbsp;:v&#x1f47b;&nbsp;&nbsp;g:&#x1f61f;&nbsp;&nbsp;c:&#x1f62a;<i><br>
-		<br></i>',
-		'ossn:smilies:admin:settings:compat:off' => 'disabled',
-		'ossn:smilies:admin:settings:compat:on' => 'enabled',
-		'ossn:smilies:admin:settings:save:error' => 'Cannot save setting! Check your error_log file.',
-		'ossn:smilies:admin:settings:saved' => 'Setting saved!',
+		'ossn:smilies:admin:settings:compat:title' => 'Mode de compatibilité ascendante',
+		'ossn:smilies:admin:settings:compat:note' => "<i class='fa fa-info-circle'></i>
+		Laissez ce mode <b><u>désactivé</u></b> lors du démarrage d'un nouveau réseau social avec Ossn 4.5 et versions supérieures.<br>
+		Un ensemble complet de plus de 1000 émoticônes Unicode est disponible, vous permettant d'utiliser des smileys à l'ancienne, comme :) est devenu obsolète.<br>
+		Avec le mode de compatibilité descendante <b><u>activé</u></b> la liste suivante de smileys à l'ancienne sera remplacée comme avant:<br>
+		:(&#x1f641;&nbsp;&nbsp;:)&#x1f642;&nbsp;&nbsp;=D&#x1f600;&nbsp;&nbsp;;)&#x1f609;&nbsp;&nbsp;:p&#x1f61b;&nbsp;&nbsp;8)&#x1f60e;&nbsp;&nbsp;o.O&#x1f62f;&nbsp;&nbsp;:O&#x1f632;&nbsp;&nbsp;:*&#x1f618;&nbsp;&nbsp;a:&#x1f607;&nbsp;&nbsp;:h:&#x2764;&nbsp;&nbsp;3:|&#x1f608;&nbsp;&nbsp;u:&#x1f620;&nbsp;&nbsp;:v&#x1f47b;&nbsp;&nbsp;g:&#x1f61f;&nbsp;&nbsp;c:&#x1f62a;<br>
+		<br>",
+		'ossn:smilies:admin:settings:close_anywhere:title' => "Boîte Smiley se fermant en cliquant n'importe où",
+		'ossn:smilies:admin:settings:close_anywhere:note' => "<i class='fa fa-info-circle'></i> ferme la boîte de sélection de smiley en cliquant n'importe où sur la page<br><br>",
 );
 ossn_register_languages('fr', $en);
