c7530784fe759bca9d8a5df5936ab1f406480256
githubertus <githubert@zuelsdorff.de>
2019-12-06 13:28:25 +0100

    Update ossn.en.php

--- components/OssnSmilies/locale/ossn.en.php
+++ components/OssnSmilies/locale/ossn.en.php
@@ -17,7 +17,7 @@ $en = array(
 		With backward compatibility mode <b><u>enabled</u></b> the following list of old style smilies will be replaced as before:<br>
 		:(&#x1f641;&nbsp;&nbsp;:)&#x1f642;&nbsp;&nbsp;=D&#x1f600;&nbsp;&nbsp;;)&#x1f609;&nbsp;&nbsp;:p&#x1f61b;&nbsp;&nbsp;8)&#x1f60e;&nbsp;&nbsp;o.O&#x1f62f;&nbsp;&nbsp;:O&#x1f632;&nbsp;&nbsp;:*&#x1f618;&nbsp;&nbsp;a:&#x1f607;&nbsp;&nbsp;:h:&#x2764;&nbsp;&nbsp;3:|&#x1f608;&nbsp;&nbsp;u:&#x1f620;&nbsp;&nbsp;:v&#x1f47b;&nbsp;&nbsp;g:&#x1f61f;&nbsp;&nbsp;c:&#x1f62a;<br>
 		<br>',
-		'ossn:smilies:admin:settings:close_anywhere:title' => 'Close box on clicking anywhere',
+		'ossn:smilies:admin:settings:close_anywhere:title' => 'Smiley box closing by clicking anywhere',
 		'ossn:smilies:admin:settings:close_anywhere:note' => '<i class="fa fa-info-circle"></i> closes the smiley selector box by clicking anywhere on the page<br><br>',
 );
 ossn_register_languages('en', $en);
