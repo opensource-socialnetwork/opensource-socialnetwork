60578eac0a53818d8b3c6513562affe15a77c3b8
githubertus <githubert@zuelsdorff.de>
2019-12-06 16:24:34 +0100

    added strings for 'Close box on clicking anywhere' setting

--- components/OssnSmilies/locale/ossn.ru.php
+++ components/OssnSmilies/locale/ossn.ru.php
@@ -11,15 +11,13 @@
 $ru = array(
 		'ossnsmilies' => 'Улыбашки',
 		'ossn:smilies:admin:settings:compat:title' => 'Режим обратной совместимости',
-		'ossn:smilies:admin:settings:compat:note' => '<i class="fa fa-info-circle"></i><br><i>
+		'ossn:smilies:admin:settings:compat:note' => '<i class="fa fa-info-circle"></i><br>
 		Покиньте этот режим <b><u>выключить</u></b> когда запускаете новую сеть с нуля начиная с версии 4.5 и выше.<br>
 		Полный пакет более 1000 эмох теперь доступен. Это делает использование старых улыбашек типа ":)" анахронизмом.<br>
-		В режиме обратной совместимости <b><u>включить</u></b> следующей список старых улыбашек будет заменён на те которые были ранее:</i><br>
-		:(&#x1f641;&nbsp;&nbsp;:)&#x1f642;&nbsp;&nbsp;=D&#x1f600;&nbsp;&nbsp;;)&#x1f609;&nbsp;&nbsp;:p&#x1f61b;&nbsp;&nbsp;8)&#x1f60e;&nbsp;&nbsp;o.O&#x1f62f;&nbsp;&nbsp;:O&#x1f632;&nbsp;&nbsp;:*&#x1f618;&nbsp;&nbsp;a:&#x1f607;&nbsp;&nbsp;:h:&#x2764;&nbsp;&nbsp;3:|&#x1f608;&nbsp;&nbsp;u:&#x1f620;&nbsp;&nbsp;:v&#x1f47b;&nbsp;&nbsp;g:&#x1f61f;&nbsp;&nbsp;c:&#x1f62a;<i><br>
-		<br></i>',
-		'ossn:smilies:admin:settings:compat:off' => 'выключить',
-		'ossn:smilies:admin:settings:compat:on' => 'включить',
-		'ossn:smilies:admin:settings:save:error' => 'Не получилось сохранить настройки. Проверь файл error_log.',
-		'ossn:smilies:admin:settings:saved' => 'Настройки сохранены!',
+		В режиме обратной совместимости <b><u>включить</u></b> следующей список старых улыбашек будет заменён на те которые были ранее:<br>
+		:(&#x1f641;&nbsp;&nbsp;:)&#x1f642;&nbsp;&nbsp;=D&#x1f600;&nbsp;&nbsp;;)&#x1f609;&nbsp;&nbsp;:p&#x1f61b;&nbsp;&nbsp;8)&#x1f60e;&nbsp;&nbsp;o.O&#x1f62f;&nbsp;&nbsp;:O&#x1f632;&nbsp;&nbsp;:*&#x1f618;&nbsp;&nbsp;a:&#x1f607;&nbsp;&nbsp;:h:&#x2764;&nbsp;&nbsp;3:|&#x1f608;&nbsp;&nbsp;u:&#x1f620;&nbsp;&nbsp;:v&#x1f47b;&nbsp;&nbsp;g:&#x1f61f;&nbsp;&nbsp;c:&#x1f62a;<br>
+		<br>',
+		'ossn:smilies:admin:settings:close_anywhere:title' => 'Закрытие окна смайлика нажатием в любом месте',
+		'ossn:smilies:admin:settings:close_anywhere:note' => '<i class="fa fa-info-circle"></i> закрывает окно выбора смайликов, щелкая в любом месте страницы<br><br>',
 );
 ossn_register_languages('ru', $ru);
