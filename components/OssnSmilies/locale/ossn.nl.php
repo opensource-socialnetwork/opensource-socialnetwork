84f040f8dc631be4b1569c48dcf1adfede95e99e
githubertus <githubert@zuelsdorff.de>
2019-12-06 15:36:12 +0100

    added strings for 'Close box on clicking anywhere' setting

--- components/OssnSmilies/locale/ossn.nl.php
+++ components/OssnSmilies/locale/ossn.nl.php
@@ -11,15 +11,13 @@
 $nl = array(
 		'ossnsmilies' => 'Smilies',
 		'ossn:smilies:admin:settings:compat:title' => 'Compatibel met eerdere versies',
-		'ossn:smilies:admin:settings:compat:note' => '<i class="fa fa-info-circle"></i><br><i>
+		'ossn:smilies:admin:settings:compat:note' => '<i class="fa fa-info-circle"></i><br>
 		Laat deze modus <b><u>uitgeschakeld</u></b> als je met een nieuwe installatie van Ossn 4.5 en hoger.<br>
 		Complete set van meer dan 1000 unicode emojis zijn nu beschikbaar, zodoende is het gebruik van oude stijl smilies zoals :) onbruikbaatr.<br>
-		Met de modus voor eerdere versies <b><u>ingeschakeld</u></b> onderstaande lijst met smilies oude stijl worden vervangen zoals eerder:</i><br>
-		:(&#x1f641;&nbsp;&nbsp;:)&#x1f642;&nbsp;&nbsp;=D&#x1f600;&nbsp;&nbsp;;)&#x1f609;&nbsp;&nbsp;:p&#x1f61b;&nbsp;&nbsp;8)&#x1f60e;&nbsp;&nbsp;o.O&#x1f62f;&nbsp;&nbsp;:O&#x1f632;&nbsp;&nbsp;:*&#x1f618;&nbsp;&nbsp;a:&#x1f607;&nbsp;&nbsp;:h:&#x2764;&nbsp;&nbsp;3:|&#x1f608;&nbsp;&nbsp;u:&#x1f620;&nbsp;&nbsp;:v&#x1f47b;&nbsp;&nbsp;g:&#x1f61f;&nbsp;&nbsp;c:&#x1f62a;<i><br>
-		<br></i>',
-		'ossn:smilies:admin:settings:compat:off' => 'uitgeschakeld',
-		'ossn:smilies:admin:settings:compat:on' => 'ingeschakeld',
-		'ossn:smilies:admin:settings:save:error' => 'Kan de instellingen niet opslaan! Controleer error_log file.',
-		'ossn:smilies:admin:settings:saved' => 'Instellingen opgeslagen!',
+		Met de modus voor eerdere versies <b><u>ingeschakeld</u></b> onderstaande lijst met smilies oude stijl worden vervangen zoals eerder:<br>
+		:(&#x1f641;&nbsp;&nbsp;:)&#x1f642;&nbsp;&nbsp;=D&#x1f600;&nbsp;&nbsp;;)&#x1f609;&nbsp;&nbsp;:p&#x1f61b;&nbsp;&nbsp;8)&#x1f60e;&nbsp;&nbsp;o.O&#x1f62f;&nbsp;&nbsp;:O&#x1f632;&nbsp;&nbsp;:*&#x1f618;&nbsp;&nbsp;a:&#x1f607;&nbsp;&nbsp;:h:&#x2764;&nbsp;&nbsp;3:|&#x1f608;&nbsp;&nbsp;u:&#x1f620;&nbsp;&nbsp;:v&#x1f47b;&nbsp;&nbsp;g:&#x1f61f;&nbsp;&nbsp;c:&#x1f62a;<br>
+		<br>',
+		'ossn:smilies:admin:settings:close_anywhere:title' => 'Smiley-vak sluit door ergens te klikken',
+		'ossn:smilies:admin:settings:close_anywhere:note' => '<i class="fa fa-info-circle"></i> sluit de smiley-selector door ergens op de pagina te klikken<br><br>',
 );
 ossn_register_languages('nl', $nl);
