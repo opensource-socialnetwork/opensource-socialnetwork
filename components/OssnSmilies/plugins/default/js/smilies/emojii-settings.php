2bb0e1e12731faac98ba510c1368b07832b90afd
Arsalan Shah <manactric@gmail.com>
2019-12-09 01:48:54 +0500

    Allow to open close smiles box in chat window

--- components/OssnSmilies/plugins/default/js/smilies/emojii-settings.php
+++ components/OssnSmilies/plugins/default/js/smilies/emojii-settings.php
@@ -6,7 +6,7 @@ if($settings && $settings->close_anywhere == 'on'){
 ?>
 $(document).ready(function() {
 	$('body').click(function(e) {
-		if($('#master-moji .emojii-container-main').is(':visible') && e.target.className != 'fa fa-smile-o'  && e.target.className != 'emojii' && e.target.innerHTML != '😉') {
+		if($('#master-moji .emojii-container-main').is(':visible') &&  e.target.className != 'ossn-chat-icon-smile' && e.target.className != 'fa fa-smile-o'  && e.target.className != 'emojii' && e.target.innerHTML != 'ðŸ˜‰') {
 		    $('#master-moji .emojii-container-main').hide();
 		}
 	});
