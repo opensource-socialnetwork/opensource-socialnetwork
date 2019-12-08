04dba86b652c1211d24ad185754488ed0dddd79b
githubertus <githubert@zuelsdorff.de>
2019-12-08 10:43:36 +0100

    update to latest changes

--- components/OssnSmilies/locale/ossn.el.php
+++ components/OssnSmilies/locale/ossn.el.php
@@ -11,15 +11,13 @@
 $el = array(
 		'ossnsmilies' => 'Smilies',
 		'ossn:smilies:admin:settings:compat:title' => 'Λειτουργία συμβατότητας',
-		'ossn:smilies:admin:settings:compat:note' => '<i class="fa fa-info-circle"></i><br><i>
+		'ossn:smilies:admin:settings:compat:note' => '<i class="fa fa-info-circle"></i><br>
 		Αφήστε αυτή τη λειτουργία <b><u>απενεργοποιημένη</u></b> κατά την εκκίνηση ενός νέου Social Network από Ossn 4.5 και άνω.<br>
 		Ένα πλήρες σετ με πάνω από 1000 emojis είναι διαθέσιμο τώρα, κατά συνέπεια το να χρησιμοποιείς παλαιού τύπου emojis οπως :) είναι ξεπερασμένο.<br>
-		Με <b><u>ενεργοποιημένη</u></b> τη λειτουργία συμβατότητας η ακόλουθη λίστα των smilies θα αντικατασταθεί με τα ακόλουθα emoticons:</i><br>
-		:(&#x1f641;&nbsp;&nbsp;:)&#x1f642;&nbsp;&nbsp;=D&#x1f600;&nbsp;&nbsp;;)&#x1f609;&nbsp;&nbsp;:p&#x1f61b;&nbsp;&nbsp;8)&#x1f60e;&nbsp;&nbsp;o.O&#x1f62f;&nbsp;&nbsp;:O&#x1f632;&nbsp;&nbsp;:*&#x1f618;&nbsp;&nbsp;a:&#x1f607;&nbsp;&nbsp;:h:&#x2764;&nbsp;&nbsp;3:|&#x1f608;&nbsp;&nbsp;u:&#x1f620;&nbsp;&nbsp;:v&#x1f47b;&nbsp;&nbsp;g:&#x1f61f;&nbsp;&nbsp;c:&#x1f62a;<i><br>
-		<br></i>',
-		'ossn:smilies:admin:settings:close_anywhere:title' => 'Smiley box closing by clicking anywhere',
-		'ossn:smilies:admin:settings:close_anywhere:note' => '<i class="fa fa-info-circle"></i> closes the smiley selector box by clicking anywhere on the page<br><br>',
-		'ossn:smilies:admin:settings:closeanywhere:title' => 'Κλείσιμο πλαισίου κάνοντας κλικ οπουδήποτε',
-		'ossn:smilies:admin:settings:closeanywhere:note' => '<i class="fa fa-info-circle"></i> Κλείνει το πλαίσιο επιλογής smiley κάνοντας κλικ οπουδήποτε στη σελίδα.<br><br>',		
+		Με <b><u>ενεργοποιημένη</u></b> τη λειτουργία συμβατότητας η ακόλουθη λίστα των smilies θα αντικατασταθεί με τα ακόλουθα emoticons:<br>
+		:(&#x1f641;&nbsp;&nbsp;:)&#x1f642;&nbsp;&nbsp;=D&#x1f600;&nbsp;&nbsp;;)&#x1f609;&nbsp;&nbsp;:p&#x1f61b;&nbsp;&nbsp;8)&#x1f60e;&nbsp;&nbsp;o.O&#x1f62f;&nbsp;&nbsp;:O&#x1f632;&nbsp;&nbsp;:*&#x1f618;&nbsp;&nbsp;a:&#x1f607;&nbsp;&nbsp;:h:&#x2764;&nbsp;&nbsp;3:|&#x1f608;&nbsp;&nbsp;u:&#x1f620;&nbsp;&nbsp;:v&#x1f47b;&nbsp;&nbsp;g:&#x1f61f;&nbsp;&nbsp;c:&#x1f62a;<br>
+		<br>',
+		'ossn:smilies:admin:settings:close_anywhere:title' => 'Κλείσιμο πλαισίου κάνοντας κλικ οπουδήποτε',
+		'ossn:smilies:admin:settings:close_anywhere:note' => '<i class="fa fa-info-circle"></i> Κλείνει το πλαίσιο επιλογής smiley κάνοντας κλικ οπουδήποτε στη σελίδα<br><br>',
 );
 ossn_register_languages('el', $el);
