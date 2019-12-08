d8a54af1ec14b0f0228142335f7c051a5175a5d5
Arsalan Shah <manactric@gmail.com>
2019-12-08 22:57:11 +0500

    Update ossn.el.php

--- components/OssnLikes/locale/ossn.el.php
+++ components/OssnLikes/locale/ossn.el.php
@@ -8,5 +8,14 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
-$el = array('people:like:this' => 'Άτομα που τους άρεσε αυτή η δημοσίευση',);
+$el = array(
+	'people:like:this' => 'Άτομα που τους άρεσε αυτή η δημοσίευση',
+	'ossn:like:this' => '%s άρεσε αυτό',
+	'ossn:like:you:and:this' => 'Εσείς και %s ακόμη',
+	'ossn:like:people' => '%s Άτομα',
+	'ossn:like:person' => '%s Άτομο',
+	'ossn:liked:you' => 'Σου άρεσε αυτό',
+	'ossn:unlike' => 'Αφαίρεση αντιδράσεις',
+	'ossn:like' => 'Μου αρέσει!',
+);
 ossn_register_languages('el', $el); 
