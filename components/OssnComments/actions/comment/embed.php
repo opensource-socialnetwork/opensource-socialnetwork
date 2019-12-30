5549c06c043aadaeca5d495cfe146ab65c7c60f0
githubertus <githubert@zuelsdorff.de>
2019-12-30 16:22:05 +0100

    fix double-backslash display error after editing
    see #1625

--- components/OssnComments/actions/comment/embed.php
+++ components/OssnComments/actions/comment/embed.php
@@ -11,7 +11,7 @@
 //Post on edit not returning JSON type callback #1506
 header('Content-Type: application/json'); 
 $data   = str_replace('\n', '<br/>', input('content'));
-error_log('$data ' . $data);
+$data	= str_replace("\\\\", "\\", $data);
 $return = ossn_call_hook('comment:view', 'template:params', NULL, array('comment' => array('comments:post' => $data)));
 $embed  = $return['comment']['comments:post'];
 echo json_encode(array(
