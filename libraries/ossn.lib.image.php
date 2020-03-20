8c7bfa5d682031fd73b94af4a29ff8b9ec124a7c
Arsalan Shah <manactric@gmail.com>
2020-03-20 14:50:45 +0500

    Update ossn.lib.image.php

--- libraries/ossn.lib.image.php
+++ libraries/ossn.lib.image.php
@@ -5,7 +5,7 @@
  * @package   (softlab24.com).ossn
  * @author    OSSN Core Team <info@softlab24.com>
  * @copyright (C) SOFTLAB24 LIMITED
- * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @license   Open Source Social Network License (OSSN LICENSE)  https://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
 /**
