ba90e2a991a69adcc93dc7e2d5476a9f060c0895
Arsalan Shah <manactric@gmail.com>
2020-04-08 01:08:26 +0500

    Remove mailer languages as they were never used

--- installation/pages/license.php
+++ installation/pages/license.php
@@ -11,7 +11,7 @@
 
 
 echo '<div><div class="layout-installation"><h2>' . ossn_installation_print('ossn:check') . '</h2><div style="margin:0 auto; width:900px;"><div style="background: #fff; padding: 20px; border-radius: 3px; border: 2px dashed #eee;">';
-echo nl2br(file_get_contents(dirname(dirname(dirname(__FILE__))) . '/LICENSE.txt'));
+echo nl2br(file_get_contents(dirname(dirname(dirname(__FILE__))) . '/LICENSE.md'));
 echo '</div><br />';
 echo '<a href="' . ossn_installation_paths()->url . '?page=settings" class="button-blue primary">'.ossn_installation_print('ossn:install:next').'</a>';
 echo '</div><br /><br /></div>';
