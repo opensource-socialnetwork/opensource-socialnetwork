70dbcea2d89ba78eace28f16e0778e0a7b226cad
Arsalan Shah <manactric@gmail.com>
2020-04-08 23:47:38 +0500

    Update ossn.de.php

--- installation/locales/ossn.de.php
+++ installation/locales/ossn.de.php
@@ -40,7 +40,7 @@ $de = array(
 	'all:files:required' => 'Die Installation ist unvollständig - bitte prüfe, ob es beim Hochladen kein Fehler aufgetreten ist.',
 	
 	'ossn:install:php' => 'PHP ',
-	'ossn:install:old:php' => "Auf dem Server läuft die PHP-Version " . PHP_VERSION . " OSSN braucht PHP 5.6 oder PHP 7.x",
+	'ossn:install:old:php' => "Auf dem Server läuft die PHP-Version " . PHP_VERSION . " OSSN braucht PHP 7.0 oder PHP 7.x",
 	
 	'ossn:install:mysqli' => 'PHP ERWEITERUNG MYSQLI VORHANDEN',
 	'ossn:install:mysqli:required' => 'PHP ERWEITERUNG MYSQLI FEHLT',
