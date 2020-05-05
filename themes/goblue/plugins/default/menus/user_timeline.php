e581c5b1609bb6d453fbea185092b6f3fa8b10d9
Arsalan Shah <manactric@gmail.com>
2020-05-05 17:56:15 +0500

    [E] Merge profile menu from GreenByGreen theme #1718

--- themes/goblue/plugins/default/menus/user_timeline.php
+++ themes/goblue/plugins/default/menus/user_timeline.php
@@ -8,10 +8,25 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
+
+// get menu_width of profile menu
+$menu_width = $params['menu_width'];
+
+global $Ossn;
+$ossnmenu = new OssnMenu;
+$ossnmenu->sortMenu('user_timeline');
+$params['menu'] = $Ossn->menu['user_timeline'];
+ 
 echo '<ul>';
 $i = 0;
+$dropdown = false;
 foreach($params['menu'] as $menu) {
-		if($i <= 10) {
+		if($menu_width != 1) {
+			// ignore string length in bootstrap xs mode
+			// and unconditionally display dropdown menu
+			$i = $i + mb_strlen(ossn_print($menu[0]['text']));
+		}
+		if($menu_width != 1 && $i <= $menu_width) {
 				foreach($menu as $name => $link) {
 						$class = "menu-user-timeline-" . $link['name'];
 						if(isset($link['class'])) {
@@ -25,8 +40,18 @@ foreach($params['menu'] as $menu) {
 						echo "<li>{$link}</li>";
 				}
 		} else {
-				echo "<li class='dropdown'><a href='javascript:void(0);' data-toggle='dropdown' class='dropdown-toggle'>" . ossn_print('more') . "<i class='fa fa-caret-down'></i></a>
-		  <ul class='dropdown-menu'>";
+				if(!$dropdown) {
+						if($menu_width == 1) {
+								// special case for mobile devices
+								echo "<li class='dropdown'><a href='javascript:void(0);' data-toggle='dropdown' class='dropdown-toggle'>" . "<i class='fa fa-bars fa-2x'></i></a>
+								<ul class='dropdown-menu'>";
+						}
+						else {
+								echo "<li class='dropdown'><a href='javascript:void(0);' data-toggle='dropdown' class='dropdown-toggle'>" . ossn_print('more') . "<i class='fa fa-caret-down'></i></a>
+								<ul class='dropdown-menu'>";
+						}
+						$dropdown = true;
+				}
 				foreach($menu as $name => $link) {
 						$class = "menu-user-timeline-" . $link['name'];
 						if(isset($link['class'])) {
@@ -39,9 +64,9 @@ foreach($params['menu'] as $menu) {
 						$link         = ossn_plugin_view('output/url', $link);
 						echo "<li>{$link}</li>";
 				}
-				echo "</ul>
-		 </li>";
 		}
-		$i++;
+}
+if($dropdown) {
+	echo "</ul></li>";
 }
 echo '</ul>';
