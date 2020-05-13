8261c39da5dd4db74b9b4042c3c5a7ce3713a692
Arsalan Shah <manactric@gmail.com>
2020-05-14 00:59:18 +0500

    [E] Remove unsupported videos from embed #1724

--- components/OssnEmbed/libraries/ossnembed.lib.php
+++ components/OssnEmbed/libraries/ossnembed.lib.php
@@ -25,12 +25,8 @@
  * youtube/youtu.be
  * vimeo
  * metacafe
- * veoh
  * dailymotion
- * blip.tv
- * teacher tube
  * hulu
- *
  */
 
 
@@ -55,20 +51,12 @@ function ossn_embed_create_embed_object($url, $guid, $videowidth=0) {
 		return ossn_embed_youtube_handler($url, $guid, $videowidth);		
 	} else if (strpos($url, 'youtu.be') != false) {
 		return ossn_embed_youtube_shortener_parse_url($url, $guid, $videowidth);
-	} else if (strpos($url, 'video.google.com') != false) {
-		return ossn_embed_google_handler($url, $guid, $videowidth);
 	} else if (strpos($url, 'vimeo.com') != false) {
 		return ossn_embed_vimeo_handler($url, $guid, $videowidth);
 	} else if (strpos($url, 'metacafe.com') != false) {
 		return ossn_embed_metacafe_handler($url, $guid, $videowidth);
-	} else if (strpos($url, 'veoh.com') != false) {
-		return ossn_embed_veoh_handler($url, $guid, $videowidth);
 	} else if (strpos($url, 'dailymotion.com') != false) {
 		return ossn_embed_dm_handler($url, $guid, $videowidth);
-	} else if (strpos($url, 'blip.tv') != false) {
-		return ossn_embed_blip_handler($url, $guid, $videowidth);
-	} else if (strpos($url, 'teachertube.com') != false) {
-		return ossn_embed_teachertube_handler($url, $guid, $videowidth);
 	} else if (strpos($url, 'hulu.com') != false) {
 		return ossn_embed_hulu_handler($url, $guid, $videowidth);
 	} else {
@@ -115,23 +103,14 @@ function ossn_embed_add_object($type, $url, $guid, $width, $height) {
 			//youtube https in ossnembed.lib.php #519
 			$videodiv .= "<iframe src=\"https://{$url}\" allowfullscreen></iframe>";
 			break;
-		case 'google':
-			$videodiv .= "<embed class='embed-responsive-item' id=\"VideoPlayback\" src=\"http://video.google.com/googleplayer.swf?docid={$url}&hl=en&fs=true\" style=\"width:{$width}px;height:{$height}px\" allowFullScreen=\"true\" allowScriptAccess=\"always\" type=\"application/x-shockwave-flash\"> </embed>";
-			break;
 		case 'vimeo':
 			$videodiv .= "<iframe src=\"https://player.vimeo.com/video/{$url}\" allowfullscreen></iframe>";
 			break;
 		case 'metacafe':
-			$videodiv .= "<embed class='embed-responsive-item' src=\"http://www.metacafe.com/fplayer/{$url}.swf\" width=\"$width\" height=\"$height\" wmode=\"transparent\" pluginspage=\"http://www.macromedia.com/go/getflashplayer\" type=\"application/x-shockwave-flash\"></embed>";
-			break;
-		case 'veoh':
-			$videodiv .= "<embed class='embed-responsive-item' src=\"http://www.veoh.com/veohplayer.swf?permalinkId={$url}&player=videodetailsembedded&videoAutoPlay=0\" allowFullScreen=\"true\" width=\"$width\" height=\"$height\" bgcolor=\"#FFFFFF\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.macromedia.com/go/getflashplayer\"></embed>";
+			$videodiv .= "<iframe class='embed-responsive-item' src=\"http://www.metacafe.com/embed/{$url}\" width=\"$width\" height=\"$height\" wmode=\"transparent\" pluginspage=\"http://www.macromedia.com/go/getflashplayer\"></iframe>";
 			break;
 		case 'dm':
 			$videodiv .= "<iframe src=\"//www.dailymotion.com/embed/video/{$url}\" width=\"$width\" height=\"$height\" allowFullScreen></iframe>"; 
-			break;
-		case 'teacher':
-			$videodiv .= "<embed class='embed-responsive-item' src=\"http://www.teachertube.com/embed/player.swf\" width=\"$width\" height=\"$height\" type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" flashvars=\"file=http://www.teachertube.com/embedFLV.php?pg=video_{$url}&menu=false&&frontcolor=ffffff&lightcolor=FF0000&logo=http://www.teachertube.com/www3/images/greylogo.swf&skin=http://www.teachertube.com/embed/overlay.swf&volume=80&controlbar=over&displayclick=link&viral.link=http://www.teachertube.com/viewVideo.php?video_id={$url}&stretching=exactfit&plugins=viral-2&viral.callout=none&viral.onpause=false\"></embed>";
 			break;
 		case 'hulu':
 			$videodiv .= "<object class='embed-responsive-item' width=\"{$width}\" height=\"{$height}\"><param name=\"movie\" value=\"http://www.hulu.com/embed/{$url}\"></param><param name=\"allowFullScreen\" value=\"true\"></param><embed src=\"http://www.hulu.com/embed/{$url}\" type=\"application/x-shockwave-flash\" allowFullScreen=\"true\"  width=\"{$width}\" height=\"{$height}\"></embed></object>";
@@ -232,85 +211,6 @@ function ossn_embed_youtube_shortener_parse_url($url) {
 
 	return $embed_object;
 }
-
-/**
- * main google interface
- *
- * @param string $url
- * @param integer $guid unique identifier of the widget
- * @param integer $videowidth  optional override of admin set width
- * @return string css style, video div, and flash <object>
- */
-function ossn_embed_google_handler($url, $guid, $videowidth) {
-	// this extracts the core part of the url needed for embeding
-	$videourl = ossn_embed_google_parse_url($url);
-	if (!isset($videourl)) {
-		return false;
-	}
-
-	ossn_embed_calc_size($videowidth, $videoheight, 400/300, 27);
-
-	$embed_object = ossn_embed_add_css($guid, $videowidth, $videoheight);
-
-	$embed_object .= ossn_embed_add_object('google', $videourl, $guid, $videowidth, $videoheight);
-
-	return $embed_object;
-}
-
-/**
- * parse google url
- *
- * @param string $url
- * @return string hash
- */
-function ossn_embed_google_parse_url($url) {
-	// separate parsing embed url
-	if (strpos($url, 'embed') != false) {
-		return ossn_embed_google_parse_embed($url);
-	}
-
-	if (!preg_match('/(https?:\/\/)(video\.google\.com\/videoplay)(.*)/', $url, $matches)) {
-		//echo "malformed google url";
-		return;
-	}
-
-	$path = $matches[3];
-	//echo $path;
-
-	// forces rest of url to start with "?docid=", followed by hash, and rest of options start with &
-	if (!preg_match('/^(\?docid=)([0-9-]*)#?(&.*)?$/',$path, $matches)) {
-		//echo "bad hash";
-		return;
-	}
-
-	$hash = $matches[2];
-	//echo $hash;
-
-	return $hash;
-}
-
-/**
- * parse google embed code
- *
- * @param string $url
- * @return string hash
- */
-function ossn_embed_google_parse_embed($url) {
-
-	if (!preg_match('/(src=)(https?:\/\/video\.google\.com\/googleplayer\.swf\?docid=)([0-9-]*)(&hl=[a-zA-Z]{2})(.*)/', $url, $matches)) {
-		//echo "malformed embed google url";
-		return;
-	}
-
-	$hash   = $matches[3];
-	//echo $hash;
-
-	// need to pull out language here
-	//echo $matches[4];
-
-	return $hash;
-}
-
 /**
  * main vimeo interface
  *
@@ -441,7 +341,7 @@ function ossn_embed_metacafe_parse_url($url) {
  * @return string hash
  */
 function ossn_embed_metacafe_parse_embed($url) {
-	if (!preg_match('/(src="https?:\/\/)(www\.)?(metacafe\.com\/fplayer\/)([0-9]*)(\/[0-9a-zA-Z_-]*)(\.swf)/', $url, $matches)) {
+	if (!preg_match('/(src="https?:\/\/)(www\.)?(metacafe\.com\/embed\/)([0-9]*)(\/[0-9a-zA-Z_-]*)/', $url, $matches)) {
 		//echo "malformed embed metacafe url";
 		return;
 	}
@@ -451,73 +351,6 @@ function ossn_embed_metacafe_parse_embed($url) {
 
 	return $hash;
 }
-
-/**
- * main veoh interface
- *
- * @param string $url
- * @param integer $guid unique identifier of the widget
- * @param integer $videowidth  optional override of admin set width
- * @return string css style, video div, and flash <object>
- */
-function ossn_embed_veoh_handler($url, $guid, $videowidth) {
-	// this extracts the core part of the url needed for embeding
-	$videourl = ossn_embed_veoh_parse_url($url);
-	if (!isset($videourl)) {
-		return false;
-	}
-
-	ossn_embed_calc_size($videowidth, $videoheight, 410/311, 30);
-
-	$embed_object = ossn_embed_add_css($guid, $videowidth, $videoheight);
-
-	$embed_object .= ossn_embed_add_object('veoh', $videourl, $guid, $videowidth, $videoheight);
-
-	return $embed_object;
-}
-
-/**
- * parse veoh url
- *
- * @param string $url
- * @return string hash
- */
-function ossn_embed_veoh_parse_url($url) {
-	// separate parsing embed url
-	if (strpos($url, 'embed') != false) {
-		return ossn_embed_veoh_parse_embed($url);
-	}
-
-	if (!preg_match('/(http:\/\/www\.veoh\.com\/.*\/videos#watch%3D)([0-9a-zA-Z]*)/', $url, $matches)) {
-		//echo "malformed veoh url";
-		return;
-	}
-
-	$hash = $matches[2];
-
-	//echo $hash;
-
-	return $hash;
-}
-
-/**
- * parse veoh embed code
- *
- * @param string $url
- * @return string hash
- */
-function ossn_embed_veoh_parse_embed($url) {
-	if (!preg_match('/(src="https?:\/\/)(www\.)?(veoh\.com\/static\/swf\/webplayer\/WebPlayer\.swf\?version=)([0-9a-zA-Z.]*)&permalinkId=([a-zA-Z0-9]*)&(.*)/', $url, $matches)) {
-		//echo "malformed embed veoh url";
-		return;
-	}
-
-	$hash   = $matches[5];
-	//echo $hash;
-
-	return $hash;
-} 
-
 /**
  * main dm interface
  *
@@ -549,160 +382,20 @@ function ossn_embed_dm_handler($url, $guid, $videowidth) {
  * @return string hash
  */
 function ossn_embed_dm_parse_url($url) {
-	// separate parsing embed url
 	if (strpos($url, 'embed') != false) {
-		return ossn_embed_dm_parse_embed($url);
+		return false;
 	}
-
-	if (!preg_match('/(http:\/\/www\.dailymotion\.com\/.*\/)([0-9a-z]*)/', $url, $matches)) {
+	if (!preg_match('/(https:\/\/www\.dailymotion\.com\/(video).*\/)([0-9a-z]*)/', $url, $matches)) {
 		//echo "malformed daily motion url";
 		return;
 	}
 
-	$hash = $matches[2];
+	$hash = $matches[3];
 
 	//echo $hash;
 
 	return $hash;
 }
-
-/**
- * parse dm embed code
- *
- * @param string $url
- * @return string hash
- */
-function ossn_embed_dm_parse_embed($url) {
-	if (!preg_match('/(value="http:\/\/)(www\.)?dailymotion\.com\/swf\/video\/([a-zA-Z0-9]*)/', $url, $matches)) {
-		//echo "malformed embed daily motion url";
-		return;
-	}
-
-	$hash   = $matches[3];
-	//echo $hash;
-
-	return $hash;
-} 
-
-/**
- * main blip interface
- *
- * @param string $url
- * @param integer $guid unique identifier of the widget
- * @param integer $videowidth  optional override of admin set width
- * @return string css style, video div, and flash <object>
- */
-function ossn_embed_blip_handler($url, $guid, $videowidth) {
-	// this extracts the core part of the url needed for embeding
-	$videourl = ossn_embed_blip_parse_url($url);
-	if (!is_array($videourl)) {
-		return false;
-	}
-
-	$width = $videourl[1];
-	$height = $videourl[2] - 30;
-
-	ossn_embed_calc_size($videowidth, $videoheight, $width/$height, 30);
-
-	$embed_object = ossn_embed_add_css($guid, $videowidth, $videoheight);
-
-	$embed_object .= ossn_embed_add_object('blip', $videourl[0], $guid, $videowidth, $videoheight);
-
-	return $embed_object;
-}
-
-/**
- * parse blip url
- *
- * @param string $url
- * @return string hash
- */
-function ossn_embed_blip_parse_url($url) {
-	// separate parsing embed url
-	if (strpos($url, 'embed') === false) {
-		return 1;
-	}
-
-	if (!preg_match('/(src="https?:\/\/blip\.tv\/play\/)([a-zA-Z0-9%]*)(.*width=")([0-9]*)(.*height=")([0-9]*)/', $url, $matches)) {
-		//echo "malformed blip.tv url";
-		return 2;
-	}
-
-	$hash[0] = $matches[2];
-	$hash[1] = $matches[4];
-	$hash[2] = $matches[6];
-
-	//echo $hash[0];
-
-	return $hash;
-}
-
-/**
- * main teacher tube interface
- *
- * @param string $url
- * @param integer $guid unique identifier of the widget
- * @param integer $videowidth  optional override of admin set width
- * @return string css style, video div, and flash <object>
- */
-function ossn_embed_teachertube_handler($url, $guid, $videowidth) {
-	// this extracts the core part of the url needed for embeding
-	$videourl = ossn_embed_teachertube_parse_url($url);
-	if (!is_numeric($videourl)) {
-		return false;
-	}
-
-	ossn_embed_calc_size($videowidth, $videoheight, 425/330, 20);
-
-	$embed_object = ossn_embed_add_css($guid, $videowidth, $videoheight);
-
-	$embed_object .= ossn_embed_add_object('teacher', $videourl, $guid, $videowidth, $videoheight);
-
-	return $embed_object;
-}
-
-/**
- * parse teachertube url
- *
- * @param string $url
- * @return string hash
- */
-function ossn_embed_teachertube_parse_url($url) {
-	// separate parsing embed url
-	if (strpos($url, 'embed') !== false) {
-		return ossn_embed_teachertube_parse_embed($url);;
-	}
-
-	if (!preg_match('/(https?:\/\/www\.teachertube\.com\/viewVideo\.php\?video_id=)([0-9]*)&(.*)/', $url, $matches)) {
-		//echo "malformed teacher tube url";
-		return;
-	}
-
-	$hash = $matches[2];
-
-	echo $hash;
-
-	return $hash;
-}
-
-/**
- * parse teacher tube embed code
- *
- * @param string $url
- * @return string hash
- */
-function ossn_embed_teachertube_parse_embed($url) {
-	if (!preg_match('/(flashvars="file=https?:\/\/www\.teachertube\.com\/embedFLV.php\?pg=video_)([0-9]*)&(.*)/', $url, $matches)) {
-		//echo "malformed teacher tube embed code";
-		return;
-	}
-
-	$hash   = $matches[2];
-	//echo $hash;
-
-	return $hash;
-}
-
 /**
  * main hulu interface
  *
