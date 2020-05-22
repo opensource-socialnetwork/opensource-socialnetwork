ea3efbe5388559b97f685b1a8e414fcaa3d49c37
Arsalan Shah <manactric@gmail.com>
2020-05-22 15:32:32 +0500

    Delete profile photo  #1647
    It should not effect current photo

--- components/OssnPhotos/actions/photo/profile/delete.php
+++ components/OssnPhotos/actions/photo/profile/delete.php
@@ -15,11 +15,13 @@ $delete->photoid = $photoid;
 $photo           = $delete->GetPhoto($delete->photoid);
 if(($photo->owner_guid == ossn_loggedin_user()->guid) || ossn_isAdminLoggedin()) {
 		if($delete->deleteProfilePhoto()) {
-				
-				$user                  = ossn_user_by_guid($photo->owner_guid);
-				$user->data->icon_time = time();
-				$user->save();
-				
+				$user = ossn_user_by_guid($photo->owner_guid);
+				if(isset($user->icon_guid) && $user->icon_guid == $photoid){				
+						$user->data->icon_time = time();
+						//[E] Default profile picture #1647
+						$user->data->icon_guid = false;
+						$user->save();
+				}
 				ossn_trigger_message(ossn_print('photo:deleted:success'), 'success');
 				redirect("album/profile/{$photo->owner_guid}");
 		} else {
