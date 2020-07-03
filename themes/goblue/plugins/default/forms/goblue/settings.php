381c7602512b2e4168f38d4e4fe639cf24b9be46
Arsalan Shah <manactric@gmail.com>
2020-07-03 23:52:56 +0500

    logo cache

--- themes/goblue/plugins/default/forms/goblue/settings.php
+++ themes/goblue/plugins/default/forms/goblue/settings.php
@@ -6,14 +6,22 @@
     	<label><?php echo ossn_print('theme:goblue:logo:site');?> (450x90 - 500 KB PNG) </label>
         <input type="file" name="logo_site" />
         <div class="logo-container-goblue">
-	        <img src="<?php echo ossn_theme_url();?>images/logo.png" />
+            	<?php if(ossn_site_settings('cache') == true){?>
+            	<img src="<?php echo ossn_theme_url();?>images/logo.png" />
+                <?php } else { ?>
+            	<img src="<?php echo ossn_theme_url();?>images/logo.png?v=<?php echo time();?>" />                
+                <?php } ?>
         </div>
     </div>
   	<div>	
     	<label><?php echo ossn_print('theme:goblue:logo:admin');?> (180x45 - 500 KB JPG)</label>
         <input type="file" name="logo_admin" />
         <div class="logo-container-goblue">
-	        <img src="<?php echo ossn_theme_url();?>images/logo_admin.jpg" />
+            			<?php if(ossn_site_settings('cache') == true){?>
+            			<img src="<?php echo ossn_theme_url(); ?>images/logo_admin.jpg"/>
+                        <?php } else { ?>
+            			<img src="<?php echo ossn_theme_url(); ?>images/logo_admin.jpg?ver=<?php echo time();?>"/>                        
+                        <?php } ?> 
         </div>
     </div>   
 	<input type="submit" class="btn btn-success btn-sm" value="<?php echo ossn_print('save');?>"/>
