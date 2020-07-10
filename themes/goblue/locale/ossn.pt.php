d7f526daed522316ec359d75ceb9c0c67f4cace0
ElBrujo <alex.mora@outlook.com>
2020-07-10 15:29:22 -0500

    Update ossn.pt.php
    Small fix to mistake in locale, show an error when "user settings" click

--- themes/goblue/locale/ossn.pt.php
+++ themes/goblue/locale/ossn.pt.php
@@ -8,13 +8,13 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
-ossn_register_languages('pt', rray(
+ossn_register_languages('pt', array(
 	'goblue:settings' => 'GoBlue',
 	'admin:theme:goblue' => 'GoBlue',
 	'theme:goblue:logo:site' => 'Logotipo do site',
-	'theme:goblue:logo:admin' => 'Logotipo da administração',
-	'theme:goblue:logo:large' => 'O arquivo do logotipo é muito grande!',
+	'theme:goblue:logo:admin' => 'Logotipo da administraÃ§Ã£o',
+	'theme:goblue:logo:large' => 'O arquivo do logotipo Ã© muito grande!',
 	'theme:goblue:logo:failed' => 'O upload do logotipo falhou',
 	'theme:goblue:logo:changed' => 'O logotipo foi alterado.',
-	'theme:goblue:browercache' => 'No caso de não aparecerem imagens. Por favor, limpe o cache do seu navegador para fazer as imagens aparecerem'
+	'theme:goblue:browercache' => 'No caso de nÃ£o aparecerem imagens. Por favor, limpe o cache do seu navegador para fazer as imagens aparecerem'
 ));
