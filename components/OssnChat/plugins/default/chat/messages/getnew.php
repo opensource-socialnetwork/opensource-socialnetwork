7b1ca8d5757c0abe745649e6891c652a56f612d1
Arsalan Shah <manactric@gmail.com>
2020-08-06 23:20:20 +0500

    Update getnew.php

--- components/OssnChat/plugins/default/chat/messages/getnew.php
+++ components/OssnChat/plugins/default/chat/messages/getnew.php
@@ -8,27 +8,27 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
-$from = $params['message_to'];
-$to = ossn_loggedin_user();
+$from     = $params['message_to'];
+$to       = ossn_loggedin_user();
 $messages = ossn_chat()->getNew($from, $to);
 if ($messages) {
-    foreach ($messages as $message){
-		$deleted = false;
-		$class = '';
-		if(isset($message->is_deleted) && $message->is_deleted == true){
-				$deleted = true;
-				$class = ' ossn-message-deleted';
-		}							
-        $vars['message'] = linkify_chat($message->message);
-        $vars['time'] = $message->time;
-        $vars['id'] = $message->id;
-		$vars['deleted'] = $deleted;
-		$vars['class'] = $class;			
-        if (ossn_loggedin_user()->guid == $message->message_from) {
-            echo ossn_plugin_view('chat/message-item-send', $vars);
-        } else {
-            $vars['reciever'] = ossn_user_by_guid($message->message_from);
-            echo ossn_plugin_view('chat/message-item-received', $vars);
-        }
-    }
+		foreach ($messages as $message) {
+				$deleted = false;
+				$class   = '';
+				if (isset($message->is_deleted) && $message->is_deleted == true) {
+						$deleted = true;
+						$class   = ' ossn-message-deleted';
+				}
+				$vars['message'] = linkify_chat($message->message);
+				$vars['time']    = $message->time;
+				$vars['id']      = $message->id;
+				$vars['deleted'] = $deleted;
+				$vars['class']   = $class;
+				if (ossn_loggedin_user()->guid == $message->message_from) {
+						echo ossn_plugin_view('chat/message-item-send', $vars);
+				} else {
+						$vars['reciever'] = ossn_user_by_guid($message->message_from);
+						echo ossn_plugin_view('chat/message-item-received', $vars);
+				}
+		}
 }
