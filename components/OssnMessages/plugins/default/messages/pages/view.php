6e2dbc9c8c18fae0a3f06143733ac831b6d46d43
Arsalan Shah <manactric@gmail.com>
2020-08-07 01:10:29 +0500

    Update view.php

--- components/OssnMessages/plugins/default/messages/pages/view.php
+++ components/OssnMessages/plugins/default/messages/pages/view.php
@@ -23,6 +23,8 @@
             	      <?php
 	  					echo ossn_plugin_view('widget/view', array(
 							'title' => $params['user']->fullname,
+							'id' => 'message-with-user-widget',
+							'data-guid' => $params['user']->guid,
 							'contents' => ossn_plugin_view('messages/pages/view/with', $params),
 							'class' => 'messages-with',
 						));
