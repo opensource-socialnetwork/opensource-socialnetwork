2cec2f75d89f0f113dc0862adfb0d8940bde30a5
Arsalan Shah <manactric@gmail.com>
2020-08-07 01:09:16 +0500

    Update view.php

--- themes/goblue/plugins/default/widget/view.php
+++ themes/goblue/plugins/default/widget/view.php
@@ -8,17 +8,29 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
- $class = '';
+ $class = 'ossn-widget';
  if(isset($params['class'])){ 
- 	$class = $params['class'];
+ 	$class = 'ossn-widget '.$params['class'];
  }
+ unset($params['class']);
  if(empty($params['title'])){
 	 return;
- }  
+ } 
+$defaults = array(
+	'class' => $class,
+);
+
+$title 	  = $params['title'];
+$contents = $params['contents'];
+
+$params   = array_merge($defaults, $params);
+unset($params['title']);
+unset($params['contents']);
+$attributes = ossn_args($params); 
 ?>
-<div class="ossn-widget <?php echo $class;?>">
-	<div class="widget-heading"><?php echo $params['title'];?></div>
+<div <?php echo $attributes;?>>
+	<div class="widget-heading"><?php echo $title;?></div>
 	<div class="widget-contents">
-		<?php echo $params['contents'];?>
+		<?php echo $contents;?>
 	</div>
 </div>
