377ecdf3752cec4bcc0a6ee21cf430f27315220f
Arsalan Shah <manactric@gmail.com>
2020-08-17 06:06:14 +0500

    Show the loading box if clicked on fa
    Because some notification or messages takes time to load users may click again and again, show loading instead!

--- components/OssnNotifications/plugins/default/js/notifications-settings.php
+++ components/OssnNotifications/plugins/default/js/notifications-settings.php
@@ -5,15 +5,13 @@ $settings = $component->getComSettings('OssnNotifications');
 if($settings && $settings->close_anywhere == 'on'){
 ?>
 $(document).ready(function() {
-	$('body').click(function(e) {
+	$('body').click(function(e){
 		var clicked_target = e.target.className.substring(0, 6);
-		if (clicked_target != 'btn bt') {
+		if (clicked_target != 'btn bt' && clicked_target != 'ossn-n' && clicked_target != 'ossn-i' && clicked_target != 'fa fa-') {
 			Ossn.NotificationBoxClose();
-			if (clicked_target != 'fa fa-') {
-				$('.ossn-notifications-notification').attr('onClick', 'Ossn.NotificationShow(this)');
-				$('.ossn-notifications-messages').attr('onClick', 'Ossn.NotificationMessagesShow(this)');
-				$('.ossn-notifications-friends').attr('onClick', 'Ossn.NotificationFriendsShow(this)');
-			}
+			$('.ossn-notifications-notification').attr('onClick', 'Ossn.NotificationShow(this)');
+			$('.ossn-notifications-messages').attr('onClick', 'Ossn.NotificationMessagesShow(this)');
+			$('.ossn-notifications-friends').attr('onClick', 'Ossn.NotificationFriendsShow(this)');
 		}
 	});
 });
