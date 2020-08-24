5282c025caf48f5465958d236bc9b22d70b7088f
Arsalan Shah <manactric@gmail.com>
2020-08-25 02:57:13 +0500

    Update contents.php

--- themes/goblue/plugins/default/theme/page/layout/contents.php
+++ themes/goblue/plugins/default/theme/page/layout/contents.php
@@ -9,7 +9,7 @@
  * @link      https://www.opensource-socialnetwork.org/
  */
  ?>
-<div class="container-fluid">
+<div class="container">
        <div class="row">
        		<div class="ossn-layout-contents">
             	 <?php echo ossn_plugin_view('theme/page/elements/system_messages'); ?>
