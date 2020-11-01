0228b708df5e98883274989de658393a83b3f887
Arsalan Shah <manactric@gmail.com>
2020-11-01 23:23:16 +0500

    PHPMailer 6.x updated You can now changed mail settings using 'email', 'config' hook

--- classes/OssnMail.php
+++ classes/OssnMail.php
@@ -8,14 +8,16 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
-
 //get phpmailer autload
 require_once(ossn_route()->classes . 'mail/PHPMailerAutoload.php');
-
 class OssnMail extends PHPMailer {
 		/**
 		 * Send email to user.
 		 *
+		 * To compltely  ovveride the email you may use
+		 * ossn_add_hook('email', 'send:policy', 'ossn_deny_default_mailer', 1); function ossn_deny_default_mailer: return false;
+		 * ossn_add_hook('email', 'send', 'ossn_my_custom_email', 1);
+		 * 
 		 * @param string $email User email address
 		 * @param string $subject Email subject
 		 * @param string $body Email body
@@ -27,22 +29,25 @@ class OssnMail extends PHPMailer {
 				if(empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)){
 					error_log('Can not send email to empty email address', 0);
 				}
-				$this->setFrom(ossn_site_settings('notification_email'), ossn_site_settings('site_name'));
-				$this->addAddress($email);
+				//params contain initial params, while return may contain changed values
+				$mail = ossn_call_hook('email', 'config', $this, $this); 
 				
-				$this->Subject = $subject;
-				$this->Body    = $body;
-				$this->CharSet = "UTF-8";
-				$this->XMailer = " "; //disable the exposure of x-mailer
+				$mail->setFrom(ossn_site_settings('notification_email'), ossn_site_settings('site_name'));
+				$mail->addAddress($email);
+				
+				$mail->Subject = $subject;
+				$mail->Body    = $body;
+				$mail->CharSet = "UTF-8";
+				$mail->XMailer = " "; //disable the exposure of x-mailer
 				try {	
-						$send = ossn_call_hook('email', 'send:policy', true, $this);
+						$send = ossn_call_hook('email', 'send:policy', true, $mail);
 						if($send) {
-								if($this->send()){
+								if($mail->send()){
 									return true;
 								}
 						} else {
 							//allow system to intract with mail
-							return ossn_call_hook('email', 'send', false, $this);
+							return ossn_call_hook('email', 'send', false, $mail);
 						}
 				}
 				catch(phpmailerException $e) {
--- classes/mail/PHPMailerAutoload.php
+++ classes/mail/PHPMailerAutoload.php
@@ -1,28 +1,13 @@
 <?php
 /**
  * PHPMailer SPL autoloader.
- * PHP Version 5
- * @package PHPMailer
- * @link https://github.com/PHPMailer/PHPMailer/ The PHPMailer GitHub project
- * @author Marcus Bointon (Synchro/coolbru) <phpmailer@synchromedia.co.uk>
- * @author Jim Jagielski (jimjag) <jimjag@gmail.com>
- * @author Andy Prevost (codeworxtech) <codeworxtech@users.sourceforge.net>
- * @author Brent R. Matzelle (original founder)
- * @copyright 2012 - 2014 Marcus Bointon
- * @copyright 2010 - 2012 Jim Jagielski
- * @copyright 2004 - 2009 Andy Prevost
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- * @note This program is distributed in the hope that it will be useful - WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.
- */
-
-/**
+ * PHP Version 6
+ * 
+ & Modified  Arsalan Shah
  * PHPMailer SPL autoloader.
  * @param string $classname The name of the class to load
  */
-function PHPMailerAutoload($classname)
-{
+function PHPMailerAutoload($classname){
     //Can't use __DIR__ as it's only in PHP 5.3+
     $filename = dirname(__FILE__).DIRECTORY_SEPARATOR.'class.'.strtolower($classname).'.php';
     if (is_readable($filename)) {
