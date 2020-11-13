ec1423b30d2c02dd2bec0047f987629454addc6a
Arsalan Shah <manactric@gmail.com>
2020-11-14 00:14:08 +0500

    Update 1605286634.php

--- upgrade/upgrades/1605286634.php
+++ upgrade/upgrades/1605286634.php
@@ -8,6 +8,7 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
+ossn_generate_server_config('apache');
 ossn_version_upgrade($upgrade, '5.6');
 $factory = new OssnFactory(array(
 		'callback' => 'installation',
