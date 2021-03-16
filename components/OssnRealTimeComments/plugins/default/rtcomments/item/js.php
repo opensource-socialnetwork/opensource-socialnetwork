0e1340e9557c510857f6a9aec742927681ae9cdd
Arsalan Shah <manactric@gmail.com>
2021-03-16 12:58:41 +0500

    Update js.php

--- components/OssnRealTimeComments/plugins/default/rtcomments/item/js.php
+++ components/OssnRealTimeComments/plugins/default/rtcomments/item/js.php
@@ -1,5 +1,14 @@
-<script>$(document).ready(function(){Ossn.commentTyping(<?php echo $params->guid;?>, 'post');});</script>
-<div class="comments-realtime-status ctyping-post-<?php echo $params->guid;?>" data-type="post" data-guid="<?php echo $params->guid;?>" data-time="<?php echo time();?>">
+<?php
+$guid = false;
+if(isset($params->guid) && !empty($params->guid)){
+	$guid = $params->guid;
+} 
+if(isset($params['post']) && !empty($params['post']->guid)){
+	$guid = $params['post']->guid;
+}
+?>
+<script>$(document).ready(function(){Ossn.commentTyping(<?php echo $guid?>, 'post');});</script>
+<div class="comments-realtime-status ctyping-post-<?php echo $guid;?>" data-type="post" data-guid="<?php echo $params->guid;?>" data-time="<?php echo time();?>">
 	<div class="comments-item ctyping-c-item ctyping-hide">
     	<div class="ctyping-c-item-container">
     	<div class="row">
