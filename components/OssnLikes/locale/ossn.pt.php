171846a02159e6f164ed1c3581b8a80c1c67521f
githubertus <githubert@zuelsdorff.de>
2021-04-06 22:30:56 +0200

    changed encoding to utf-8

--- components/OssnLikes/locale/ossn.pt.php
+++ components/OssnLikes/locale/ossn.pt.php
@@ -11,11 +11,11 @@
 $pt = array(
 	'people:like:this' => 'Pessoas que curtiram essa postagem',
 	'ossn:like:this' => '%s curtiu isso',
-	'ossn:like:you:and:this' => 'Você e %s curtiram isso',
+	'ossn:like:you:and:this' => 'VocÃª e %s curtiram isso',
 	'ossn:like:people' => '%s Pessoas',
 	'ossn:like:person' => '%s Pessoa',
-	'ossn:liked:you' => 'Você curtiu isso',
+	'ossn:liked:you' => 'VocÃª curtiu isso',
 	'ossn:unlike' => 'Descurtir',
 	'ossn:like' => 'Curtir',		
 );
-ossn_register_languages('pt', $pt); 
+ossn_register_languages('pt', $pt);
