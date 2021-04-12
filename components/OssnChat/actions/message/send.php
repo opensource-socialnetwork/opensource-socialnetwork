5552853ba6966b8c0f84cac11e34d25f52d6a91f
Arsalan Shah <manactric@gmail.com>
2021-04-12 13:58:56 +0500

    New Message missing message id in div ... id='message-item-' #1841

--- components/OssnChat/actions/message/send.php
+++ components/OssnChat/actions/message/send.php
@@ -16,9 +16,16 @@ $from = ossn_loggedin_user()->guid;
 header('Content-Type: application/json');
 if ($to && $from && strlen($message)) {
 	$send = ossn_chat();
-	if ($send->send($from, $to, $message)) {
-		$vars['message'] = $message;
-		$vars['time'] = time();
+	if ($message_id = $send->send($from, $to, $message)) {
+		$instance = ossn_get_message($message_id);
+		
+		$vars['message']     = $message;
+		$vars['time'] 	 	 = time();
+		$vars['instance']  = $instance;
+		$vars['view_type'] = 'actions/chat/send';
+		//New Message missing message id in div ... id='message-item-' #1841
+		$vars['id'] = $message_id;
+	
 		echo json_encode(array(
 			'type' => 1,
 			'message' => ossn_plugin_view('chat/message-item-send', $vars),
