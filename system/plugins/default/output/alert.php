8b8f084988dba779255d15913d3832d41b24ab45
Arsalan Shah <manactric@gmail.com>
2021-04-16 16:05:56 +0500

    output/alert display

---
+++ system/plugins/default/output/alert.php
@@ -0,0 +1,4 @@
+<div class='alert alert-dismissible alert-<?php echo $params['type'];?>'>
+<?php echo $params['message'];?>
+<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>                        
