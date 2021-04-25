171d2149b3d27c26807f9f26ee11757338414bf9
Arsalan Shah <manactric@gmail.com>
2021-04-25 14:30:07 +0500

    Site pages layout fix

--- components/OssnSitePages/pages/page.php
+++ components/OssnSitePages/pages/page.php
@@ -15,13 +15,17 @@ if (!isset($params['contents'])) {
     $params['contents'] = '';
 }
 ?>
-<div class="row ossn-site-pages">
-    <div class="col-md-12 ossn-site-pages-inner  ossn-page-contents">
-        <div class="ossn-site-pages-title">
-            <?php echo $params['title']; ?>
-        </div>
-        <div class="ossn-site-pages-body">
-            <?php echo $params['contents']; ?>
-        </div>
-    </div>
+<div class="row">
+	<div class="col-md-11">
+		<div class="ossn-site-pages">
+			<div class="ossn-site-pages-inner  ossn-page-contents">
+				<div class="ossn-site-pages-title">
+					<?php echo $params['title']; ?>
+				</div>
+				<div class="ossn-site-pages-body">
+					<?php echo $params['contents']; ?>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
--- components/OssnSitePages/plugins/default/css/pages.php
+++ components/OssnSitePages/plugins/default/css/pages.php
@@ -7,17 +7,3 @@
 .ossn-site-pages-body {
     padding: 10px;
 }
-@media only screen
-and (max-width : 1360px) {
-.ossn-site-pages{
-    margin-left: 15px;
-    margin-right: 15px
-    }
-}
-@media only screen
-and (min-width : 1360px) {
-.ossn-site-pages{
-    margin-left: 80px;
-    margin-right: 80px
-    }
-}
