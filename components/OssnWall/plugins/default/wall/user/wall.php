67677b9e9c4095a98ac3813986b48d86b22b6248
Arsalan Shah <manactric@gmail.com>
2021-05-13 20:12:21 +0500

    Update wall.php

--- components/OssnWall/plugins/default/wall/user/wall.php
+++ components/OssnWall/plugins/default/wall/user/wall.php
@@ -8,10 +8,8 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
-$posts = new OssnWall;
-$count = $posts->GetUserPosts($params['user']->guid, true);
-$posts = $posts->GetUserPosts($params['user']->guid);
-
+$OssnWall = new OssnWall;
+$posts = $OssnWall->GetUserPosts($params['user']);
 //lastchage: site admins are unable to access member profile threads without friendship #176 
 if(ossn_loggedin_user() && (ossn_user_is_friend(ossn_loggedin_user()->guid, $params['user']->guid) || ossn_loggedin_user()->guid == $params['user']->guid || ossn_isAdminLoggedin())) {
 		echo '<div class="ossn-wall-container">';
@@ -27,50 +25,12 @@ if(ossn_loggedin_user() && (ossn_user_is_friend(ossn_loggedin_user()->guid, $par
 }
 echo '<div class="user-activity">';
 if($posts) {
+		$count = $OssnWall->GetUserPosts($params['user'], array(
+			'count' => true,													 
+		));	
 		foreach($posts as $post) {
-				$data     = json_decode(html_entity_decode($post->description));
-				if(!is_object($data)){
-					$data = new stdClass;
-				}
-				$text     = ossn_restore_new_lines($data->post, true);
-				$location = '';
-				if(isset($data->location)) {
-						$location = '- ' . $data->location;
-				}
-				if(!isset($data->friend)) {
-						$data->friend = '';
-				}
-				if(isset($post->{'file:wallphoto'})) {
-						$image = str_replace('ossnwall/images/', '', $post->{'file:wallphoto'});
-				} else {
-						$image = '';
-				}
-				$user = ossn_user_by_guid($post->poster_guid);
-				if($post->access == OSSN_FRIENDS) {
-						//lastchage: site admins are unable to access member profile threads without friendship #176 
-						if(ossn_loggedin_user() && (ossn_user_is_friend($post->owner_guid, ossn_loggedin_user()->guid) || ossn_loggedin_user()->guid == $post->owner_guid || ossn_isAdminLoggedin())) {
-								echo ossn_plugin_view('wall/templates/activity-item', array(
-										'post' => $post,
-										'friends' => explode(',', $data->friend),
-										'text' => $text,
-										'location' => $location,
-										'user' => $user,
-										'image' => $image
-								));
-						}
-				}
-				if($post->access == OSSN_PUBLIC) {
-						echo ossn_plugin_view('wall/templates/activity-item', array(
-								'post' => $post,
-								'friends' => explode(',', $data->friend),
-								'text' => $text,
-								'location' => $location,
-								'user' => $user,
-								'image' => $image
-						));
-				}
-				
-				
+				$item = ossn_wallpost_to_item($post);
+				echo ossn_wall_view_template($item);
 		}
 }
 echo ossn_view_pagination($count);
