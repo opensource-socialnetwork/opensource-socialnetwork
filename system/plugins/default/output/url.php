2d234720f968d0b1da5e51cbabf065bc0a0dcf33
Arsalan Shah <manactric@gmail.com>
2021-06-19 00:15:36 +0500

    Update url.php

--- system/plugins/default/output/url.php
+++ system/plugins/default/output/url.php
@@ -8,7 +8,7 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
- 
+
 /**
  * Ossn Url Display
  * Displays a URL as a link
@@ -17,21 +17,20 @@
  * @uses string $params['href']        The unencoded url string
  * @uses bool   $params['action']   Is this a link to an action (false)
  */
-	if(isset($params['href'])){
+if(isset($params['href'])){
 		$url = $params['href'];
-	}
-	$text = $params['text'];
+}
+$text = $params['text'];
 
-	
-	if(isset($params['action']) && $params['action'] == true){
+if(isset($params['action']) && $params['action'] == true){
 		$url = ossn_add_tokens_to_url($url);
-	}
-	unset($params['text']);
-	unset($params['action']);
-    unset($params['href']);
-	if(isset($url)){
+}
+unset($params['text']);
+unset($params['action']);
+unset($params['href']);
+if(isset($url)){
 		$params['href'] = $url;
-	}
-	$attributes = ossn_args($params);
-		
-	echo "<a {$attributes}>{$text}</a>";
+}
+$attributes = ossn_args($params);
+
+echo "<a {$attributes}>{$text}</a>";
