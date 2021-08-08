4d608dd25a09eeff1f49561afdef273c1cff4f10
Arsalan Shah <manactric@gmail.com>
2021-08-08 11:42:51 +0500

    Update view.php

--- system/plugins/default/entity/comment/like/share/view.php
+++ system/plugins/default/entity/comment/like/share/view.php
@@ -9,23 +9,27 @@
  * @link      https://www.opensource-socialnetwork.org/
  */
  ossn_trigger_callback('entity', 'load:comment:share:like', $params);
+ $entity['params'] = $params;
+ $entity['entity_guid'] = $params['entity']->guid;
  ?>
 <div class="comments-likes ossn-photos-comments">
 	<div class="menu-likes-comments-share">
             	<?php echo ossn_view_menu('entityextra');?>
 	</div>     
- 	<?php
-    if (ossn_is_hook('post', 'likes:entity')) {
-		$entity['params'] = $params;
-        $entity['entity_guid'] = $params['entity']->guid;
+    <?php
+    if(ossn_is_hook('post', 'likes:entity')) {
+ 	if(isset($params['allow_like'])){
+                $entity['allow_like'] = $params['allow_like'];	
+ 	}		
         echo ossn_call_hook('post', 'likes:entity', $entity);
     }
     ?>
     <div class="comments-list">
     <?php
     if (ossn_is_hook('post', 'comments:entity')) {
-		$entity['params'] = $params;
-        $entity['entity_guid'] =  $params['entity']->guid;
+ 	if(isset($params['allow_comment'])){
+                 $entity['allow_comment'] = $params['allow_comment'];	
+ 	}				
         echo ossn_call_hook('post', 'comments:entity', $entity);
     }
     ?>
