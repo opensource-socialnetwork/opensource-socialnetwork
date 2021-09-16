bfa9a06473152cb8be5603ab691bc466d7abc02c
Arsalan Shah <manactric@gmail.com>
2021-09-17 02:28:03 +0500

    Update item.php

--- components/OssnAds/plugins/default/ads/item.php
+++ components/OssnAds/plugins/default/ads/item.php
@@ -1,12 +1,14 @@
 <div class="ossn-ad-item">
-<div class="row">
-	<a target="_blank" href="<?php echo $params['item']->site_url;?>">
-		<div class="col-md-12">
-			<div class="ad-title"><?php echo $params['item']->title;?></div>
-			<div class="ad-link"><?php echo $params['item']->site_url;?></div>
-			<img class="ad-image" src="<?php echo ossn_ads_image_url($params['item']->guid); ?>" />
-			<p><?php echo $params['item']->description;?></p>
-		</div>
-	</a>
-</div>
+	<div class="row">
+		<a target="_blank" href="<?php echo $params['item']->site_url;?>">
+			<div class="col-md-12">
+				<div class="ad-title"><?php echo $params['item']->title;?></div>
+				<div class="ad-link"><?php echo $params['item']->site_url;?></div>
+				<div class="ad-image-container">
+					<img class="ad-image" src="<?php echo ossn_ads_image_url($params['item']->guid); ?>" />
+				</div>
+				<p><?php echo $params['item']->description;?></p>
+			</div>
+		</a>
+	</div>
 </div>
