fdcf1879e0871e211a7d70aaf6956b15f6662df7
Arsalan Shah <manactric@gmail.com>
2021-09-23 20:55:57 +0500

    Update delete.php

--- components/OssnComments/actions/comment/delete.php
+++ components/OssnComments/actions/comment/delete.php
@@ -9,49 +9,60 @@
  * @link      https://www.opensource-socialnetwork.org/
  */
 $comment = input('comment');
-$delete = new OssnComments;
+$delete  = new OssnComments();
 $comment = $delete->GetComment($comment);
 
+$object = false;
+$entity = false;
+$post   = false;
 //group admins must be able to delete ANY comment in their own group #170
 //first get wall post then get group and check if loggedin user is group owner
-if ($comment->type == 'comments:post') {
-    $post = ossn_get_object($comment->subject_guid);
-    if ($post && $post->type == 'group') {
-        $group = ossn_get_group_by_guid($post->owner_guid);
-    }
+if($comment->type == 'comments:post') {
+		$post = ossn_get_object($comment->subject_guid);
+		if($post && $post->type == 'group') {
+				$group = ossn_get_group_by_guid($post->owner_guid);
+		}
 }
-if ($comment->type == 'comments:object') {
-    $object = ossn_get_object($comment->subject_guid);
+
+if($comment->type == 'comments:object') {
+		$object = ossn_get_object($comment->subject_guid);
 }
 
 $user = ossn_loggedin_user();
-if ($comment->type == 'comments:entity') {
-    $entity = ossn_get_entity($comment->subject_guid);
+if($comment->type == 'comments:entity') {
+		$entity = ossn_get_entity($comment->subject_guid);
 }
 //Post owner can not delete others comments #607
-if (($comment->owner_guid == $user->guid) ||($object && $object->type == 'user' && $user->guid == $object->owner_guid) || ($post->type == 'user' && $user->guid == $post->owner_guid) || ($group->owner_guid == $user->guid) || ($entity->owner_guid == $user->guid) || ossn_isAdminLoggedin()) {
-    if ($delete->deleteComment($comment->getID())) {
-        if (ossn_is_xhr()) {
-            echo 1;
-            exit;
-        } else {
-            ossn_trigger_message(ossn_print('comment:deleted'), 'success');
-            redirect(REF);
-        }
-    } else {
-        if (ossn_is_xhr()) {
-            echo 0;
-            exit;
-        } else {
-            ossn_trigger_message(ossn_print('comment:delete:error'), 'error');
-            redirect(REF);
-        }
-    }
+if(
+		$comment->owner_guid == $user->guid ||
+		($object && $object->type == 'user' && $user->guid == $object->owner_guid) ||
+		($post->type == 'user' && $user->guid == $post->owner_guid) ||
+		$group->owner_guid == $user->guid ||
+		$entity->owner_guid == $user->guid ||
+		ossn_isAdminLoggedin()
+) {
+		if($delete->deleteComment($comment->getID())) {
+				if(ossn_is_xhr()) {
+						echo 1;
+						exit();
+				} else {
+						ossn_trigger_message(ossn_print('comment:deleted'), 'success');
+						redirect(REF);
+				}
+		} else {
+				if(ossn_is_xhr()) {
+						echo 0;
+						exit();
+				} else {
+						ossn_trigger_message(ossn_print('comment:delete:error'), 'error');
+						redirect(REF);
+				}
+		}
 } else {
-    if (ossn_is_xhr()) {
-        echo 0;
-    } else {
-        ossn_trigger_message(ossn_print('comment:delete:error'), 'error');
-        redirect(REF);
-    }
+		if(ossn_is_xhr()) {
+				echo 0;
+		} else {
+				ossn_trigger_message(ossn_print('comment:delete:error'), 'error');
+				redirect(REF);
+		}
 }
