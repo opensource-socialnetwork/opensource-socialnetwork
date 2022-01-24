57b8ba8c9b8c91c5d9e772a7cb45a4248f7441e5
Arsalan Shah <manactric@gmail.com>
2022-01-24 23:14:39 +0500

    [E] Enhance group menu entries in sidebar #2072

--- components/OssnGroups/ossn_com.php
+++ components/OssnGroups/ossn_com.php
@@ -12,8 +12,8 @@
 define('__OSSN_GROUPS__', ossn_route()->com . 'OssnGroups/');
 
 /* Include group class and library */
-require_once(__OSSN_GROUPS__ . 'classes/OssnGroup.php');
-require_once(__OSSN_GROUPS__ . 'libraries/groups.php');
+require_once __OSSN_GROUPS__ . 'classes/OssnGroup.php';
+require_once __OSSN_GROUPS__ . 'libraries/groups.php';
 /**
  * Initialize Groups Component
  *
@@ -23,10 +23,10 @@ require_once(__OSSN_GROUPS__ . 'libraries/groups.php');
 function ossn_groups() {
 		//group css
 		ossn_extend_view('css/ossn.default', 'css/groups');
-		
+
 		//group js
 		ossn_extend_view('js/ossn.site', 'js/groups');
-		
+
 		//group pages
 		ossn_register_page('group', 'ossn_group_page');
 		ossn_register_page('groups', 'ossn_groups_page');
@@ -34,18 +34,18 @@ function ossn_groups() {
 		ossn_group_subpage('members');
 		ossn_group_subpage('edit');
 		ossn_group_subpage('requests');
-		
+
 		//group hooks
 		ossn_add_hook('group', 'subpage', 'group_about_page');
 		ossn_add_hook('group', 'subpage', 'group_members_page');
 		ossn_add_hook('group', 'subpage', 'group_edit_page');
 		ossn_add_hook('group', 'subpage', 'group_requests_page');
-		ossn_add_hook('newsfeed', "left", 'ossn_add_groups_to_newfeed');
+		ossn_add_hook('newsfeed', 'left', 'ossn_add_groups_to_newfeed');
 		ossn_add_hook('search', 'type:groups', 'groups_search_handler');
 		ossn_add_hook('notification:add', 'comments:post:group:wall', 'ossn_notificaiton_groups_comments_hook');
 		ossn_add_hook('notification:add', 'like:post:group:wall', 'ossn_notificaiton_groups_comments_hook');
 		ossn_add_hook('notification:view', 'group:joinrequest', 'ossn_group_joinrequest_notification');
-		
+
 		//group actions
 		if(ossn_isLoggedin()) {
 				ossn_register_action('group/add', __OSSN_GROUPS__ . 'actions/group/add.php');
@@ -56,58 +56,64 @@ function ossn_groups() {
 				ossn_register_action('group/member/approve', __OSSN_GROUPS__ . 'actions/group/member/request/approve.php');
 				ossn_register_action('group/member/cancel', __OSSN_GROUPS__ . 'actions/group/member/request/cancel.php');
 				ossn_register_action('group/member/decline', __OSSN_GROUPS__ . 'actions/group/member/request/decline.php');
-				
+
 				ossn_register_action('group/cover/upload', __OSSN_GROUPS__ . 'actions/group/cover/upload.php');
 				ossn_register_action('group/cover/reposition', __OSSN_GROUPS__ . 'actions/group/cover/reposition.php');
 				ossn_register_action('group/cover/delete', __OSSN_GROUPS__ . 'actions/group/cover/delete.php');
+
+				//[E] Enhance group menu entries in sidebar #2072
+				//only for loggedin users.
+				ossn_register_callback('menu', 'section:before:view', 'ossn_group_sidebar_entries');
 		}
-		
-		
+
 		//callbacks
 		ossn_register_callback('page', 'load:group', 'ossn_group_load_event');
 		ossn_register_callback('page', 'load:search', 'ossn_group_search_link');
 		ossn_register_callback('user', 'delete', 'ossn_user_groups_delete');
-		
+}
+
+function ossn_group_sidebar_entries($event, $type, $params) {
+		// using priority hack here to make the groups menu appear at the old known position :)
+		// otherwise it's moved to the end because other component menus register earlier the old way
+		// of course in longer terms those components may use the same callback too,
+		// so things will be arranged as before
+		$priority = 100;
+
 		//group list in newsfeed sidebar mebu
 		$groups_user = ossn_get_user_groups(ossn_loggedin_user());
 		if($groups_user) {
 				foreach($groups_user as $group) {
 						$icon = ossn_site_url('components/OssnGroups/images/group.png');
 						ossn_register_sections_menu('newsfeed', array(
-								'text' => $group->title,
-								'name' => "groups",
-								'url' => ossn_group_url($group->guid),
-								'parent' => 'groups',
-								'icon' => $icon
+								'text'     => $group->title,
+								'name'     => 'groups',
+								'url'      => ossn_group_url($group->guid),
+								'parent'   => 'groups',
+								'icon'     => $icon,
+								'priority' => $priority++,
 						));
 						unset($icon);
 				}
 		}
 		//add gorup link in sidebar
 		ossn_register_sections_menu('newsfeed', array(
-				'name' => 'addgroup',
-				'text' => ossn_print('add:group'),
-				'url' => 'javascript:void(0);',
-				'id' => 'ossn-group-add',
-				'parent' => 'groups',
-				'icon' => ossn_site_url('components/OssnGroups/images/add.png')
+				'name'     => 'addgroup',
+				'text'     => ossn_print('add:group'),
+				'url'      => 'javascript:void(0);',
+				'id'       => 'ossn-group-add',
+				'parent'   => 'groups',
+				'icon'     => ossn_site_url('components/OssnGroups/images/add.png'),
+				'priority' => $priority++,
 		));
 		//Create link in nav to list all groups #990
 		ossn_register_sections_menu('newsfeed', array(
-				'name' => 'allgroups',
-				'text' => ossn_print('groups'),
-				'url' => ossn_site_url('search?type=groups&q='),
-				'parent' => 'groups',
-				'icon' => true
+				'name'     => 'allgroups',
+				'text'     => ossn_print('groups'),
+				'url'      => ossn_site_url('search?type=groups&q='),
+				'parent'   => 'groups',
+				'icon'     => true,
+				'priority' => $priority++,
 		));
-		//my groups link
-		/* ossn_register_sections_menu('newsfeed', array(
-		'text' => 'My Groups',
-		'url' => 'javascript:void(0);',
-		'section' => 'groups',
-		'icon' => ossn_site_url('components/OssnGroups/images/manages.png')
-		));*/
-		
 }
 
 /**
@@ -117,12 +123,12 @@ function ossn_groups() {
  * @access private
  */
 function groups_search_handler($hook, $type, $return, $params) {
-		$groups = new OssnGroup;
+		$groups = new OssnGroup();
 		$data   = $groups->searchGroups($params['q']);
 		$count  = $groups->searchGroups($params['q'], array(
-				'count' => true
+				'count' => true,
 		));
-		
+
 		$group['groups'] = $data;
 		$search          = ossn_plugin_view('groups/search/view', $group);
 		$search .= ossn_view_pagination($count);
@@ -145,10 +151,11 @@ function ossn_group_load_event($event, $type, $params) {
 		ossn_register_menu_link('members', 'members', ossn_group_url($owner) . 'members', 'groupheader');
 		// show 'Requests' menu tab only on pending requests
 		$group = ossn_get_group_by_guid($owner);
-		if(ossn_isLoggedin() && $group){
-			if ($group->countRequests() && ($group->owner_guid == ossn_loggedin_user()->guid || ossn_isAdminLoggedin() || $group->isModerator(ossn_loggedin_user()->guid))) {
-				ossn_register_menu_link('requests', 'requests', ossn_group_url($owner) . 'requests', 'groupheader');
-			}
+		// removed && $group->countRequests()
+		if($group) {
+				if($group->owner_guid == ossn_loggedin_user()->guid || ossn_isAdminLoggedin() || $group->isModerator(ossn_loggedin_user()->guid)) {
+						ossn_register_menu_link('requests', 'requests', ossn_group_url($owner) . 'requests', 'groupheader');
+				}
 		}
 }
 
@@ -160,7 +167,7 @@ function ossn_group_load_event($event, $type, $params) {
  */
 function ossn_group_search_link($event, $type, $params) {
 		$url = OssnPagination::constructUrlArgs(array(
-				'type'
+				'type',
 		));
 		ossn_register_menu_link('groups', 'groups', "search?type=groups{$url}", 'search');
 }
@@ -180,50 +187,50 @@ function ossn_groups_page($pages) {
 				return false;
 		}
 		switch($page) {
-				case 'add':
-						$params = array(
-								'action' => ossn_site_url() . 'action/group/add',
-								'component' => 'OssnGroups',
-								'class' => 'ossn-form'
-						);
-						$form   = ossn_view_form('add', $params, false);
-						echo ossn_plugin_view('output/ossnbox', array(
-								'title' => ossn_print('add:group'),
-								'contents' => $form,
-								'callback' => '#ossn-group-submit'
-						));
-						break;
-				case 'cover':
-						if(isset($pages[1]) && !empty($pages[1])) {
-								$File          = new OssnFile;
-								$File->file_id = $pages[1];
-								$File          = $File->fetchFile();
-								
-								$etag = $File->guid . $File->time_created;
-								
-								if(isset($_SERVER['HTTP_IF_NONE_MATCH']) && trim($_SERVER['HTTP_IF_NONE_MATCH']) == "\"$etag\"") {
-										header("HTTP/1.1 304 Not Modified");
-										exit;
-								}
-								if(isset($File->guid)) {
-										$Cover    = ossn_get_userdata("object/{$File->owner_guid}/{$File->value}");
-										$filesize = filesize($Cover);
-										header("Content-type: image/jpeg");
-										header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', strtotime("+6 months")), true);
-										header("Pragma: public");
-										header("Cache-Control: public");
-										header("Content-Length: $filesize");
-										header("ETag: \"$etag\"");
-										readfile($Cover);
-										return;
-								} else {
-										ossn_error_page();
-								}
+			case 'add':
+				$params = array(
+						'action'    => ossn_site_url() . 'action/group/add',
+						'component' => 'OssnGroups',
+						'class'     => 'ossn-form',
+				);
+				$form = ossn_view_form('add', $params, false);
+				echo ossn_plugin_view('output/ossnbox', array(
+						'title'    => ossn_print('add:group'),
+						'contents' => $form,
+						'callback' => '#ossn-group-submit',
+				));
+				break;
+			case 'cover':
+				if(isset($pages[1]) && !empty($pages[1])) {
+						$File          = new OssnFile();
+						$File->file_id = $pages[1];
+						$File          = $File->fetchFile();
+
+						$etag = $File->guid . $File->time_created;
+
+						if(isset($_SERVER['HTTP_IF_NONE_MATCH']) && trim($_SERVER['HTTP_IF_NONE_MATCH']) == "\"$etag\"") {
+								header('HTTP/1.1 304 Not Modified');
+								exit();
 						}
-						break;
-				default:
-						echo ossn_error_page();
-						break;
+						if(isset($File->guid)) {
+								$Cover    = ossn_get_userdata("object/{$File->owner_guid}/{$File->value}");
+								$filesize = filesize($Cover);
+								header('Content-type: image/jpeg');
+								header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', strtotime('+6 months')), true);
+								header('Pragma: public');
+								header('Cache-Control: public');
+								header("Content-Length: $filesize");
+								header("ETag: \"$etag\"");
+								readfile($Cover);
+								return;
+						} else {
+								ossn_error_page();
+						}
+				}
+				break;
+		default:
+				echo ossn_error_page();
+				break;
 		}
 }
 
@@ -249,7 +256,7 @@ function ossn_group_page($pages) {
 				} else {
 						$params['subpage'] = '';
 				}
-				
+
 				if(!ossn_is_group_subapge($params['subpage']) && !empty($params['subpage'])) {
 						ossn_error_page();
 				}
@@ -259,12 +266,11 @@ function ossn_group_page($pages) {
 				}
 				ossn_set_page_owner_guid($group->guid);
 				ossn_trigger_callback('page', 'load:group');
-				
-				
-				$params['group']     = $group;
-				$title               = $group->title;
-				$view                = ossn_plugin_view('groups/pages/profile', $params);
-				$content             = ossn_group_layout($view);
+
+				$params['group'] = $group;
+				$title           = $group->title;
+				$view            = ossn_plugin_view('groups/pages/profile', $params);
+				$content         = ossn_group_layout($view);
 				echo ossn_view_page($title, $content);
 		}
 }
@@ -283,8 +289,8 @@ function group_about_page($hook, $type, $return, $params) {
 		if($page == 'about') {
 				$mod_content = ossn_plugin_view('groups/pages/about', $params);
 				$mod         = array(
-						'title' => ossn_print('about:group'),
-						'content' => $mod_content
+						'title'   => ossn_print('about:group'),
+						'content' => $mod_content,
 				);
 				echo ossn_set_page_layout('module', $mod);
 		}
@@ -304,8 +310,8 @@ function group_members_page($hook, $type, $return, $params) {
 		if($page == 'members') {
 				$mod_content = ossn_plugin_view('groups/pages/members', $params);
 				$mod         = array(
-						'title' => ossn_print('members'),
-						'content' => $mod_content
+						'title'   => ossn_print('members'),
+						'content' => $mod_content,
 				);
 				echo ossn_set_page_layout('module', $mod);
 		}
@@ -323,24 +329,24 @@ function group_members_page($hook, $type, $return, $params) {
 function group_edit_page($hook, $type, $return, $params) {
 		$page  = $params['subpage'];
 		$group = ossn_get_group_by_guid(ossn_get_page_owner_guid());
-		if(ossn_isLoggedin()){
-			if($group->owner_guid !== ossn_loggedin_user()->guid && !ossn_isAdminLoggedin()) {
-				return false;
-			}
+		if(ossn_isLoggedin()) {
+				if($group->owner_guid !== ossn_loggedin_user()->guid && !ossn_isAdminLoggedin()) {
+						return false;
+				}
 		}
 		if($page == 'edit') {
 				$params = array(
-						'action' => ossn_site_url() . 'action/group/edit',
+						'action'    => ossn_site_url() . 'action/group/edit',
 						'component' => 'OssnGroups',
-						'class' => 'ossn-edit-form',
-						'params' => array(
-								'group' => $group
-						)
+						'class'     => 'ossn-edit-form',
+						'params'    => array(
+								'group' => $group,
+						),
 				);
-				$form   = ossn_view_form('edit', $params, false);
+				$form = ossn_view_form('edit', $params, false);
 				echo ossn_set_page_layout('module', array(
-						'title' => ossn_print('edit'),
-						'content' => $form
+						'title'   => ossn_print('edit'),
+						'content' => $form,
 				));
 		}
 }
@@ -362,11 +368,11 @@ function group_requests_page($hook, $type, $return, $params) {
 						redirect("group/{$group->guid}");
 				}
 				$mod_content = ossn_plugin_view('groups/pages/requests', array(
-						'group' => $group
+						'group' => $group,
 				));
-				$mod         = array(
-						'title' => ossn_print('requests'),
-						'content' => $mod_content
+				$mod = array(
+						'title'   => ossn_print('requests'),
+						'content' => $mod_content,
 				);
 				echo ossn_set_page_layout('module', $mod);
 		}
@@ -382,8 +388,10 @@ function group_requests_page($hook, $type, $return, $params) {
  * @access private
  */
 function ossn_user_groups_delete($callback, $type, $params) {
-		$deleteGroup = new OssnGroup;
-		$groups      = $deleteGroup->getUserGroups($params['entity']->guid, array('page_limit' => false));
+		$deleteGroup = new OssnGroup();
+		$groups      = $deleteGroup->getUserGroups($params['entity']->guid, array(
+				'page_limit' => false,
+		));
 		if($groups) {
 				foreach($groups as $group) {
 						$deleteGroup->deleteGroup($group->guid);
@@ -401,7 +409,7 @@ function ossn_user_groups_delete($callback, $type, $params) {
  * @access public
  */
 function ossn_notificaiton_groups_comments_hook($hook, $type, $return, $params) {
-		$object              = new OssnObject;
+		$object              = new OssnObject();
 		$object->object_guid = $params['subject_guid'];
 		$object              = $object->getObjectById();
 		if($object) {
@@ -411,7 +419,6 @@ function ossn_notificaiton_groups_comments_hook($hook, $type, $return, $params) 
 		return false;
 }
 
-
 // #186 group join request hook
 function ossn_group_joinrequest_notification($name, $type, $return, $params) {
 		$baseurl        = ossn_site_url();
@@ -420,21 +427,23 @@ function ossn_group_joinrequest_notification($name, $type, $return, $params) {
 		$group          = ossn_get_group_by_guid($params->subject_guid);
 		$img            = "<div class='notification-image'><img src='{$baseurl}avatar/{$user->username}/small' /></div>";
 		$type           = "<div class='ossn-groups-notification-icon'></div>";
-		if($params->viewed !== NULL) {
+		if($params->viewed !== null) {
 				$viewed = '';
-		} elseif($params->viewed == NULL) {
+		} elseif($params->viewed == null) {
 				$viewed = 'class="ossn-notification-unviewed"';
 		}
 		// lead directly to groups request page
 		$url               = "{$baseurl}group/{$params->subject_guid}/requests";
 		$notification_read = "{$baseurl}notification/read/{$params->guid}?notification=" . urlencode($url);
 		return "<a href='{$notification_read}' class='ossn-group-notification-item'>
-	       <li {$viewed}> {$img} 
+	       <li {$viewed}> {$img}
 		   <div class='notfi-meta'> {$type}
-		   <div class='data'>" . ossn_print("ossn:notifications:{$params->type}", array(
+		   <div class='data'>" .
+		ossn_print("ossn:notifications:{$params->type}", array(
 				$user->fullname,
-				$group->title
-		)) . '</div>
+				$group->title,
+		)) .
+				'</div>
 		   </div></li></a>';
 }
 ossn_register_callback('ossn', 'init', 'ossn_groups');
