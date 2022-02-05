9aefe1bbb2125b6391cbac228bd7aaf3d3ad43c6
Arsalan Shah <manactric@gmail.com>
2022-02-05 12:06:47 +0500

    Update ossn.install.php

--- installation/libraries/ossn.install.php
+++ installation/libraries/ossn.install.php
@@ -8,7 +8,7 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
- 
+
 /**
  * Ossn Installation Url
  * Get a installation path url
@@ -16,20 +16,20 @@
  * @return string
  */
 function ossn_installation_url() {
-    $type = true;
-    $protocol = 'http';
-    $uri = $_SERVER['REQUEST_URI'];
-    if (!empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") {
-        $protocol = 'https';
-    }
-    $port = ':' . $_SERVER["SERVER_PORT"];
-    if ($port == ':80' || $port == ':443') {
-        if ($type == true) {
-            $port = '';
-        }
-    }
-    $url = "$protocol://{$_SERVER['SERVER_NAME']}$port{$uri}";
-    return preg_replace('/\\?.*/', '', $url);
+		$type     = true;
+		$protocol = 'http';
+		$uri      = $_SERVER['REQUEST_URI'];
+		if(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+				$protocol = 'https';
+		}
+		$port = ':' . $_SERVER['SERVER_PORT'];
+		if($port == ':80' || $port == ':443') {
+				if($type == true) {
+						$port = '';
+				}
+		}
+		$url = "$protocol://{$_SERVER['SERVER_NAME']}$port{$uri}";
+		return preg_replace('/\\?.*/', '', $url);
 }
 /**
  * Ossn Url
@@ -38,7 +38,7 @@ function ossn_installation_url() {
  * @return string
  */
 function ossn_url() {
-    return str_replace('installation/', '', ossn_installation_url());
+		return str_replace('installation/', '', ossn_installation_url());
 }
 /**
  * Ossn instalaltion paths
@@ -47,37 +47,35 @@ function ossn_url() {
  * @return object
  */
 function ossn_installation_paths() {
-    global $OssnInstall;
-    $path = str_replace("\\", "/", dirname(dirname(__FILE__)));
-    $defaults = array(
-        'root' => "{$path}/",
-        'url' => ossn_installation_url(),
-        'ossn_url' => ossn_url(),
-
-    );
-    foreach ($defaults as $name => $value) {
-        if (empty($OssnInstall->$name)) {
-            $OssnInstall->$name = $value;
-        }
-    }
-    return $OssnInstall;
+		global $OssnInstall;
+		$path     = str_replace('\\', '/', dirname(dirname(__FILE__)));
+		$defaults = array(
+				'root'     => "{$path}/",
+				'url'      => ossn_installation_url(),
+				'ossn_url' => ossn_url(),
+		);
+		foreach($defaults as $name => $value) {
+				if(empty($OssnInstall->$name)) {
+						$OssnInstall->$name = $value;
+				}
+		}
+		return $OssnInstall;
 }
 /**
  * Ossn Instalaltion Include
- * Include a file 
+ * Include a file
  *
  * @return string|null data
  */
 function ossn_installation_include($file = '', $params = array()) {
-    $file = ossn_installation_paths()->root . $file;
-    if (!empty($file) && is_file($file)) {
-        ob_start();
-        $params = $params;
-        include($file);
-        $contents = ob_get_clean();
-        return $contents;
-    }
-
+		$file = ossn_installation_paths()->root . $file;
+		if(!empty($file) && is_file($file)) {
+				ob_start();
+				$params = $params;
+				include $file;
+				$contents = ob_get_clean();
+				return $contents;
+		}
 }
 /**
  * Ossn Installation Register Languages
@@ -86,8 +84,8 @@ function ossn_installation_include($file = '', $params = array()) {
  * @return void
  */
 function ossn_installation_register_languages($strings = array()) {
-    global $OssnInstall;
-    $OssnInstall->langStrings = $strings;
+		global $OssnInstall;
+		$OssnInstall->langStrings = $strings;
 }
 /**
  * Ossn load a installation language
@@ -95,7 +93,7 @@ function ossn_installation_register_languages($strings = array()) {
  * @return arrays
  */
 function ossn_installation_languages() {
-    include_once(ossn_installation_paths()->root . 'locales/ossn.en.php');
+		include_once ossn_installation_paths()->root . 'locales/ossn.en.php';
 }
 ossn_installation_languages();
 /**
@@ -104,12 +102,12 @@ ossn_installation_languages();
  * @return string
  */
 function ossn_installation_print($string) {
-    global $OssnInstall;
-    if (isset($OssnInstall->langStrings[$string])) {
-        return $OssnInstall->langStrings[$string];
-    } else {
-        return $string;
-    }
+		global $OssnInstall;
+		if(isset($OssnInstall->langStrings[$string])) {
+				return $OssnInstall->langStrings[$string];
+		} else {
+				return $string;
+		}
 }
 /**
  * Ossn view instalaltion page
@@ -119,10 +117,10 @@ function ossn_installation_print($string) {
  * @return string|null
  */
 function ossn_installation_view_page($content, $title) {
-    return ossn_installation_include("templates/page.php", array(
-        'contents' => $content,
-        'title' => ossn_installation_print($title),
-    ));
+		return ossn_installation_include('templates/page.php', array(
+				'contents' => $content,
+				'title'    => ossn_installation_print($title),
+		));
 }
 /**
  * Handle insallation pages
@@ -130,34 +128,34 @@ function ossn_installation_view_page($content, $title) {
  * @return mixed data
  */
 function ossn_installation_page() {
-    if (isset($_REQUEST['page'])) {
-        $page = $_REQUEST['page'];
-    }
-    if (!isset($page)) {
-        $page = 'requirments';
-    }
-    switch ($page) {
-        case 'requirments':
-            $data = ossn_installation_include('pages/check.php');
-            echo ossn_installation_view_page($data, 'ossn:check');
-            break;
-        case 'license':
-            $data = ossn_installation_include('pages/license.php');
-            echo ossn_installation_view_page($data, 'ossn:check');
-            break;
-        case 'settings':
-            $data = ossn_installation_include('pages/settings.php');
-            echo ossn_installation_view_page($data, 'ossn:settings');
-            break;
-        case 'account':
-            $data = ossn_installation_include('pages/account.php');
-            echo ossn_installation_view_page($data, 'ossn:setting:account');
-            break;
-        case 'installed':
-            $data = ossn_installation_include('pages/installed.php');
-            echo ossn_installation_view_page($data, 'ossn:installed');
-            break;
-    }
+		if(isset($_REQUEST['page'])) {
+				$page = $_REQUEST['page'];
+		}
+		if(!isset($page)) {
+				$page = 'requirments';
+		}
+		switch($page) {
+			case 'requirments':
+				$data = ossn_installation_include('pages/check.php');
+				echo ossn_installation_view_page($data, 'ossn:check');
+				break;
+			case 'license':
+				$data = ossn_installation_include('pages/license.php');
+				echo ossn_installation_view_page($data, 'ossn:check');
+				break;
+			case 'settings':
+				$data = ossn_installation_include('pages/settings.php');
+				echo ossn_installation_view_page($data, 'ossn:settings');
+				break;
+			case 'account':
+				$data = ossn_installation_include('pages/account.php');
+				echo ossn_installation_view_page($data, 'ossn:setting:account');
+				break;
+			case 'installed':
+				$data = ossn_installation_include('pages/installed.php');
+				echo ossn_installation_view_page($data, 'ossn:installed');
+				break;
+		}
 }
 /**
  * Handle insallation actions
@@ -165,23 +163,23 @@ function ossn_installation_page() {
  * @return false|null data
  */
 function ossn_installation_actions() {
-    if (isset($_REQUEST['action'])) {
-        $page = $_REQUEST['action'];
-    }
-    if (!isset($page)) {
-        return false;
-    }
-    switch ($page) {
-        case 'install':
-            include_once(ossn_installation_paths()->root . 'actions/install.php');
-            break;
-        case 'account':
-            include_once(ossn_installation_paths()->root . 'actions/account.php');
-            break;
-        case 'finish':
-            include_once(ossn_installation_paths()->root . 'actions/finish.php');
-            break;
-    }
+		if(isset($_REQUEST['action'])) {
+				$page = $_REQUEST['action'];
+		}
+		if(!isset($page)) {
+				return false;
+		}
+		switch($page) {
+			case 'install':
+				include_once ossn_installation_paths()->root . 'actions/install.php';
+				break;
+			case 'account':
+				include_once ossn_installation_paths()->root . 'actions/account.php';
+				break;
+			case 'finish':
+				include_once ossn_installation_paths()->root . 'actions/finish.php';
+				break;
+		}
 }
 /**
  * Handle insallation error massages
@@ -189,7 +187,7 @@ function ossn_installation_actions() {
  * @return void
  */
 function ossn_installation_message($message, $type) {
-    $_SESSION['ossn-installation-messages']["ossn-installation-{$type}"][] = $message;
+		$_SESSION['ossn-installation-messages']["ossn-installation-{$type}"][] = $message;
 }
 /**
  * View installation error messages
@@ -197,45 +195,52 @@ function ossn_installation_message($message, $type) {
  * @return false|string data
  */
 function ossn_installation_messages() {
-    if (!isset($_SESSION['ossn-installation-messages'])) {
-        return false;
-    }
-    foreach ($_SESSION['ossn-installation-messages'] as $message => $data) {
-        foreach ($data as $msg) {
-            $msgs[] = "<div class='ossn-installation-message {$message}'>{$msg}</div>";
-        }
-    }
-    unset($_SESSION['ossn-installation-messages']);
-    return implode('', $msgs);
+		if(!isset($_SESSION['ossn-installation-messages'])) {
+				return false;
+		}
+		foreach($_SESSION['ossn-installation-messages'] as $message => $data) {
+				foreach($data as $msg) {
+						$msgs[] = "<div class='ossn-installation-message {$message}'>{$msg}</div>";
+				}
+		}
+		unset($_SESSION['ossn-installation-messages']);
+		return implode('', $msgs);
 }
 /**
  * Simple curl, get content of url
  *
  * @return mixed data
  */
-function ossn_installation_simple_curl($url = '') {	
-if(isset($url)){
-	$curlinit = curl_init();
-	curl_setopt($curlinit, CURLOPT_URL, $url);
-	curl_setopt($curlinit, CURLOPT_RETURNTRANSFER, 1);
-	$result = curl_exec($curlinit);
-	curl_close($curlinit);
-}
-return $result;
+function ossn_installation_simple_curl($url = '') {
+		if(isset($url)) {
+				$curlinit = curl_init();
+				curl_setopt($curlinit, CURLOPT_URL, $url);
+				curl_setopt($curlinit, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt($curlinit, CURLOPT_SSL_VERIFYPEER, false);
+				$result = curl_exec($curlinit);
+				if(curl_errno($curlinit)) {
+						$error = curl_error($curlinit);
+						return array(
+								$error,
+						);
+				}
+				curl_close($curlinit);
+		}
+		return (int)$result;
 }
 /**
  * Generate .htaccess file
  *
  * @return ooolean;
  */
-function ossn_generate_server_config_setup($type){
-	if($type == 'apache'){
-		$path = str_replace('installation/', '', ossn_installation_paths()->root);
-		$file = ossn_installation_paths()->root . 'configs/htaccess.dist';
-		$file = file_get_contents($file);
-		return file_put_contents($path . '.htaccess', $file);
-	}elseif($type == 'nginx'){
+function ossn_generate_server_config_setup($type) {
+		if($type == 'apache') {
+				$path = str_replace('installation/', '', ossn_installation_paths()->root);
+				$file = ossn_installation_paths()->root . 'configs/htaccess.dist';
+				$file = file_get_contents($file);
+				return file_put_contents($path . '.htaccess', $file);
+		} elseif($type == 'nginx') {
+				return false;
+		}
 		return false;
-	}
-	return false;
 }
