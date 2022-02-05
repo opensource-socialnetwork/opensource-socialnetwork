813d2c55de4c3449c76106d688f5b36f29e86df1
Arsalan Shah <manactric@gmail.com>
2022-02-05 12:16:17 +0500

    Replace translation #2075

--- locale/ossn.pt.php
+++ locale/ossn.pt.php
@@ -2,9 +2,9 @@
 /**
  * Open Source Social Network
  *
- * @package   (Informatikon.com).ossn
- * @author    OSSN Core Team <info@opensource-socialnetwork.org>
- * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @package   ossn.pt
+ * @author    OSSN Core Team <admin@opensource-socialnetwork.org>
+ * @copyright (C) Open Source Social Network
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      http://www.opensource-socialnetwork.org/licence
  */
