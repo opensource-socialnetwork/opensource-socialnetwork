aae8bc2e9612bda4de1848ae863b1152d32eb704
Arsalan Shah <manactric@gmail.com>
2022-06-16 21:47:23 +0500

    UI changes

--- components/OssnPhotos/plugins/default/forms/OssnPhotos/photos/add.php
+++ components/OssnPhotos/plugins/default/forms/OssnPhotos/photos/add.php
@@ -11,7 +11,7 @@
 ?>
 <div class="ossn-photos-add-button">
 	<input type="file" name="ossnphoto[]" multiple class="hidden"/>
-	<button type="button" id="ossn-photos-add-button-inner" class="btn btn-default btn-lg"><i class="fa fa-copy"></i> <?php echo ossn_print('photo:select'); ?></button>
+	<button type="button" id="ossn-photos-add-button-inner" class="btn btn-default"><i class="fa fa-copy"></i> <?php echo ossn_print('photo:select'); ?></button>
     <div class="images"><i class="fa fa-image"></i> <span class="count">0</span></div>
 </div>
 
