2fa92d4a26048d1fd403b6d6293d8bc657372afc
Arsalan Shah <manactric@gmail.com>
2022-11-27 00:09:54 +0500

    OssnLocation

--- components/OssnLocation/plugins/default/js/ossn_location.php
+++ components/OssnLocation/plugins/default/js/ossn_location.php
@@ -8,36 +8,40 @@
   });
 **/
 function ossn_location($obj) {
-	if ($($obj.container).length && $($obj.input).length) {
-		if(!Ossn.isset($obj.placeholder)){
-				$obj.placeholder = Ossn.Print('enter:location');
+	$(document).ready(function() {
+		if (Ossn.LocationAPIKey && Ossn.LocationAPIKey != '') {
+			if ($($obj.container).length && $($obj.input).length) {
+				if (!Ossn.isset($obj.placeholder)) {
+					$obj.placeholder = Ossn.Print('enter:location');
+				}
+				//hide input field 
+				if (!Ossn.isset($obj.hide_input)) {
+					$obj.hide_input = true;
+				}
+				if ($obj.hide_input === true) {
+					$($obj.input).hide();
+				}
+				mapboxgl.accessToken = Ossn.LocationAPIKey;
+				const geocoder = new MapboxGeocoder({
+					accessToken: mapboxgl.accessToken,
+					language: Ossn.Config.lang,
+					types: 'country,region,place,postcode,locality,neighborhood',
+					placeholder: $obj.placeholder,
+				});
+
+				geocoder.addTo($obj.container);
+				const results = $($obj.input);
+
+				//insert data into input
+				geocoder.on('result', (e) => {
+					results.val(e.result.place_name);
+				});
+
+				// Clear results container when search is cleared.
+				geocoder.on('clear', () => {
+					results.val("");
+				});
+			}
 		}
-		//hide input field 
-		if(!Ossn.isset($obj.hide_input)){
-				$obj.hide_input = true;	
-		}
-		if($obj.hide_input === true){
-			$($obj.input).hide();
-		} 
-		mapboxgl.accessToken = '<?php echo ossn_location_api_key(); ?>';
-		const geocoder = new MapboxGeocoder({
-			accessToken: mapboxgl.accessToken,
-			language: Ossn.Config.lang,
-			types: 'country,region,place,postcode,locality,neighborhood',
-			placeholder: $obj.placeholder,
-		});
-
-		geocoder.addTo($obj.container);
-		const results = $($obj.input);
-
-		//insert data into input
-		geocoder.on('result', (e) => {
-			results.val(e.result.place_name);
-		});
-
-		// Clear results container when search is cleared.
-		geocoder.on('clear', () => {
-			results.val("");
-		});
-	}
+	});
 }
