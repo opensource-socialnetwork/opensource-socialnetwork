01da5e1a068813af178233b0760b608a860fdd9a
Arsalan Shah <manactric@gmail.com>
2022-12-22 14:32:17 +0500

    [E] Update chart js library #2220

--- system/plugins/default/javascripts/dynamic/admin/dashboard/users/online/classfied.php
+++ system/plugins/default/javascripts/dynamic/admin/dashboard/users/online/classfied.php
@@ -1,28 +1,41 @@
 <script>
-		/**
-		 * Get data for last two years
-		 * Translation for gender in dashboard users & users online #511
-		 */
-		var OnlineUsersPieData = [
+
 <?php
 			$genders = array_combine($params['genders'], $params['total']);
+			$types   = array();
+			$types_total = array();
+			$colors = array();
 			foreach($genders as $gender => $total) {
-				if($total > 0) {
-					echo	"{\n";
-					echo	"value: $total,\n";
-					echo	"color: '" . ossn_print($gender . ':gendercolor') . "',\n";
-					echo	"highlight: '" . ossn_print($gender . ':gendercolor') . "',\n";
-					echo    "label: '" . ucfirst(ossn_print($gender)) . "'\n";
-					echo	"},\n";
-				}
+				 	$types[] = ucfirst(ossn_print($gender));
+					$colors[] = ossn_print($gender . ':gendercolor');
+					$types_total[] = $total;
 			}
 ?>
-			];
-			$(window).on('load', function(){
-				var online_users_graph = $('#onlineusers-classified-graph')[0].getContext("2d");
-				this.OnlinemyPie = new Chart(online_users_graph).Pie(OnlineUsersPieData);
-			    //don't you want lagends ? $arsalanshah
-				//comment line below if you want to hide legends
-				chart_js_legend($('#onlineuserclassified-lineLegend')[0], OnlineUsersPieData);				
-			});			
+$(window).on('load', function() {
+	var online_users_graph = $('#onlineusers-classified-graph')[0].getContext("2d");
+
+	this.OnlinemyPie = new Chart(online_users_graph, {
+		type: 'pie',
+		options: {
+			responsive: true,
+			maintainAspectRatio: false,
+			plugins: {
+				legend: {
+					display: true,
+					labels: {
+						usePointStyle: true,
+						boxWidth: 10
+					}
+				}
+			},
+		},
+		data: {
+			labels: <?php echo json_encode($types); ?> ,
+			datasets : [{
+				data: <?php echo json_encode($types_total); ?> ,
+				backgroundColor : <?php echo json_encode($colors); ?> ,
+			}]
+		},
+	});
+});
 	</script>
