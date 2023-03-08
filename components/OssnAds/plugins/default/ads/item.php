33d4c617c66fabdb03c8985c13239884537b691e
Arsalan Shah <manactric@gmail.com>
2023-03-08 13:59:01 +0500

    [E] OssnAds remove link in view #2242

--- components/OssnAds/plugins/default/ads/item.php
+++ components/OssnAds/plugins/default/ads/item.php
@@ -3,7 +3,6 @@
 		<a target="_blank" href="<?php echo $params['item']->site_url;?>">
 			<div class="col-md-12">
 				<div class="ad-title"><?php echo $params['item']->title;?></div>
-				<div class="ad-link"><?php echo $params['item']->site_url;?></div>
 				<div class="ad-image-container">
 					<img class="ad-image" src="<?php echo $params['item']->getPhotoURL() ?>" />
 				</div>
