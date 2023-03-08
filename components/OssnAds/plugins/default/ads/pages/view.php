aa922f3eb6ac1c4153258077bd0facb2a285ccad
Arsalan Shah <manactric@gmail.com>
2023-03-08 14:00:44 +0500

    Update view.php

--- components/OssnAds/plugins/default/ads/pages/view.php
+++ components/OssnAds/plugins/default/ads/pages/view.php
@@ -9,7 +9,8 @@
  * @link      https://www.opensource-socialnetwork.org/
  */
 ?>
-<h4><?php echo $params['entity']->title;?></h4>
-<a href="<?php echo $params['entity']->site_url;?>"><?php echo $params['entity']->site_url;?></a>
-<p><?php echo $params['entity']->description;?></p>
-<div class="ossn-ad-image" style="background:url('<?php echo $params['entity']->getPhotoURL();?>') no-repeat;background-size: contain;"></div>
+<strong><?php echo $params['entity']->title;?></strong>
+<a href="<?php echo $params['entity']->site_url;?>">
+	<p class="text-black"><?php echo $params['entity']->description;?></p>
+	<div class="ossn-ad-image" style="background:url('<?php echo $params['entity']->getPhotoURL();?>') no-repeat;background-size: contain;"></div>
+</a>
