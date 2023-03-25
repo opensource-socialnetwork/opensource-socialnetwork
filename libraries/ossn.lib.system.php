408d95f97edd31b17a556d0923031ac17c2a142f
Arsalan Shah <manactric@gmail.com>
2023-03-25 14:31:05 +0500

    Update ossn.lib.system.php

--- libraries/ossn.lib.system.php
+++ libraries/ossn.lib.system.php
@@ -841,6 +841,7 @@ function _ossn_php_error_handler($errno, $errmsg, $filename, $linenum) {
  */
 function ossn_check_update() {
 	$url             = 'https://api.github.com/repos/opensource-socialnetwork/opensource-socialnetwork/contents/opensource-socialnetwork.xml';
+	$url             = ossn_call_hook('admin','update:check:url', false, $url);
 	$args['method']  = 'GET';
 	$args['header']  = "Accept-language: en\r\n" . "Cookie: opensourcesocialnetwork=system\r\n" . "User-Agent: Mozilla/5.0\r\n";
 	$options['http'] = $args;
