a5d84db8132668934ebeca375d294ded27414df6
Arsalan Shah <manactric@gmail.com>
2023-07-31 16:44:44 +0500

    [E] Default search result type confusing #2279

--- themes/goblue/plugins/default/menus/search.php
+++ themes/goblue/plugins/default/menus/search.php
@@ -8,6 +8,10 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
+$type = input('type');
+if(empty($type)) {
+	$type = 'users';	
+}
 $menus = $params['menu'];
 echo "<div class='ossn-menu-search'>";
 echo '<div class="title">' . ossn_print('result:type') . '</div>';
@@ -16,7 +20,11 @@ foreach ($menus as $menu => $val) {
         $text = ossn_print($link['text']);
 		$link = $link['href'];
 		$class = OssnTranslit::urlize($menu);
-        echo "<li class='ossn-menu-search-{$class}'>
+		$active = '';
+		if($class == $type){
+			$active = ' ossn-search-active-item';	
+		}
+        echo "<li class='ossn-menu-search-{$class} {$active}'>
 				<a href='{$link}'>
 					<div class='text'>{$text}</div>
 				</a>
