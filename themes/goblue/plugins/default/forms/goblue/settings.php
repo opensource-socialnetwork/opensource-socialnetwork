23de62bf83cc05d13acdc02f6cf31cba5b113a55
Arsalan Shah <manactric@gmail.com>
2023-10-19 18:31:06 +0500

    [E] Changing logo should add timestamp to invalidate cache #2302

--- themes/goblue/plugins/default/forms/goblue/settings.php
+++ themes/goblue/plugins/default/forms/goblue/settings.php
@@ -1,3 +1,6 @@
+ <?php
+ $time = time();
+ ?>
  <fieldset class="titleform">
  	<div class="alert alert-warning">
     	<?php echo ossn_print('theme:goblue:browercache');?>
@@ -6,22 +9,20 @@
     	<label><?php echo ossn_print('theme:goblue:logo:site');?> (450x90 - 500 KB PNG) </label>
         <input type="file" name="logo_site" />
         <div class="logo-container-goblue">
-            	<?php if(ossn_site_settings('cache') == true){?>
-            	<img src="<?php echo ossn_theme_url();?>images/logo.png" />
-                <?php } else { ?>
-            	<img src="<?php echo ossn_theme_url();?>images/logo.png?v=<?php echo time();?>" />                
-                <?php } ?>
+        		<?php
+					$logo_url = ossn_add_cache_to_url(ossn_theme_url("images/logo.png?v={$time}"));
+				?>
+            	<img src="<?php echo $logo_url;?>" />
         </div>
     </div>
   	<div>	
     	<label><?php echo ossn_print('theme:goblue:logo:admin');?> (180x45 - 500 KB JPG)</label>
         <input type="file" name="logo_admin" />
         <div class="logo-container-goblue">
-            			<?php if(ossn_site_settings('cache') == true){?>
-            			<img src="<?php echo ossn_theme_url(); ?>images/logo_admin.jpg"/>
-                        <?php } else { ?>
-            			<img src="<?php echo ossn_theme_url(); ?>images/logo_admin.jpg?ver=<?php echo time();?>"/>                        
-                        <?php } ?> 
+        		<?php
+					$logo_url = ossn_add_cache_to_url(ossn_theme_url("images/logo_admin.jpg?v={$time}"));
+				?>
+            	<img src="<?php echo $logo_url;?>" />                        
         </div>
     </div>   
 	<input type="submit" class="btn btn-success btn-sm" value="<?php echo ossn_print('save');?>"/>
