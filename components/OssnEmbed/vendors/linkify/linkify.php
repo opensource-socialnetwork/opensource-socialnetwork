6542d1a77263a9de6a8f3278109b82807739695a
Arsalan Shah <manactric@gmail.com>
2023-12-18 05:52:08 +0500

    [B] URL with a special character in a comment post #2332

--- components/OssnEmbed/vendors/linkify/linkify.php
+++ components/OssnEmbed/vendors/linkify/linkify.php
@@ -1,55 +1,31 @@
 <?php
-/* File:        linkify.php
- * Version:     20101010_1000
- * Copyright:   (c) 2010 Jeff Roberson - http://jmrware.com
- * MIT License: http://www.opensource.org/licenses/mit-license.php
+/* Linkify the URLs in text
  *
- * Summary: This script linkifys http URLs on a page.
- *
- * Usage:   See example page: linkify.html
+ * License : GNU Public License version 2
+ * Taken from hypeJunction
+ * https://github.com/hypeJunction/hypeScraper/blob/master/classes/hypeJunction/Scraper/Linkify.php
+ * By Ismayil Khayredinov - (ismayil@hypejunction.com)
  */
 function linkify($text) {
-    $url_pattern = '/# Rev:20100913_0900 github.com\/jmrware\/LinkifyURL
-    # Match http & ftp URL that is not already linkified.
-      # Alternative 1: URL delimited by (parentheses).
-      (\()                     # $1  "(" start delimiter.
-      ((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&\'()*+,;=:\/?#[\]@%]+)  # $2: URL.
-      (\))                     # $3: ")" end delimiter.
-    | # Alternative 2: URL delimited by [square brackets].
-      (\[)                     # $4: "[" start delimiter.
-      ((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&\'()*+,;=:\/?#[\]@%]+)  # $5: URL.
-      (\])                     # $6: "]" end delimiter.
-    | # Alternative 3: URL delimited by {curly braces}.
-      (\{)                     # $7: "{" start delimiter.
-      ((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&\'()*+,;=:\/?#[\]@%]+)  # $8: URL.
-      (\})                     # $9: "}" end delimiter.
-    | # Alternative 4: URL delimited by <angle brackets>.
-      (<|&(?:lt|\#60|\#x3c);)  # $10: "<" start delimiter (or HTML entity).
-      ((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&\'()*+,;=:\/?#[\]@%]+)  # $11: URL.
-      (>|&(?:gt|\#62|\#x3e);)  # $12: ">" end delimiter (or HTML entity).
-    | # Alternative 5: URL not delimited by (), [], {} or <>.
-      (                        # $13: Prefix proving URL not already linked.
-        (?: ^                  # Can be a beginning of line or string, or
-        | [^=\s\'"\]]          # a non-"=", non-quote, non-"]", followed by
-        ) \s*[\'"]?            # optional whitespace and optional quote;
-      | [^=\s]\s+              # or... a non-equals sign followed by whitespace.
-      )                        # End $13. Non-prelinkified-proof prefix.
-      ( \b                     # $14: Other non-delimited URL.
-        (?:ht|f)tps?:\/\/      # Required literal http, https, ftp or ftps prefix.
-        [a-z0-9\-._~!$\'()*+,;=:\/?#[\]@%]+ # All URI chars except "&" (normal*).
-        (?:                    # Either on a "&" or at the end of URI.
-          (?!                  # Allow a "&" char only if not start of an...
-            &(?:gt|\#0*62|\#x0*3e);                  # HTML ">" entity, or
-          | &(?:amp|apos|quot|\#0*3[49]|\#x0*2[27]); # a [&\'"] entity if
-            [.!&\',:?;]?        # followed by optional punctuation then
-            (?:[^a-z0-9\-._~!$&\'()*+,;=:\/?#[\]@%]|$)  # a non-URI char or EOS.
-          ) &                  # If neg-assertion true, match "&" (special).
-          [a-z0-9\-._~!$\'()*+,;=:\/?#[\]@%]* # More non-& URI chars (normal*).
-        )*                     # Unroll-the-loop (special normal*)*.
-        [a-z0-9\-_~$()*+=\/#[\]@%]  # Last char can\'t be [.!&\',;:?]
-      )                        # End $14. Other non-delimited URL.
-    /imx';
-	//Open link in new tab (enhancement) #518
-    $url_replace = '$1$4$7$10$13<a href="$2$5$8$11$14" \\1 target="_blank">$2$5$8$11$14</a>$3$6$9$12';
-    return preg_replace($url_pattern, $url_replace, $text);
+		// match entire anchor <a></a> so we can exclude it from matches
+		$REGEX_MATCH_ANCHOR = '<a[^>]*?>.*?<\/a>';
+	
+		// non-greedy match of a single tag name and attributes
+		// we need to exclude e.g. hex color codes when matchin hashes
+		$REGEX_MATCH_TAG = '<.*?>';
+	
+		// we want at least one non space or punctuation character before the match
+		$REGEX_CHAR_BACK = '(^|\s|\!|\.|\?|>|\G)+';
+		$REGEX_URL       = '(h?[t|f]??tps*:\/\/[^\s\r\n\t<>"\'\)\(]+)';
+
+		$regex = '/' . $REGEX_MATCH_ANCHOR . '|' . $REGEX_MATCH_TAG . '|' . $REGEX_CHAR_BACK . $REGEX_URL . '/i';
+
+		$callback = function ($matches) {
+				if(empty($matches[2])) {
+						return $matches[0];
+				}
+				$text = $matches[2];
+				return $matches[1] . "<a rel='noreferrer' target='_blank' href='" . $matches[2] . "'>" . $text . '</a>';
+		};
+		return preg_replace_callback($regex, $callback, $text);
 }
