113af333599988b6b25491d413794bc61cb96dde
lianglee <manactric@gmail.com>
2024-02-22 21:08:20 +0500

    [E] Fix the distorted 404 error page #2353

--- system/plugins/default/pages/contents/error.php
+++ system/plugins/default/pages/contents/error.php
@@ -8,13 +8,15 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
+ //removing <row> because its added by contents layout
+ //[E] Fix the distorted 404 error page #2353
 ?>
-<div class="row">
-	<div class="col-lg-11 ossn-page-contents">
- 			<div class="ossn-error-page">
-				<i class="fa fa-exclamation-triangle"></i>
-    			<div class="error-heading"><?php echo ossn_print('page:error:heading'); ?></div>
-    			<div class="error-text"><?php echo ossn_print('page:error:text'); ?></div>
-			</div>   
-    </div>
+<div class="col-lg-11">
+			<div class="ossn-page-contents">
+	 			<div class="ossn-error-page">
+					<i class="fa fa-exclamation-triangle"></i>
+    				<div class="error-heading"><?php echo ossn_print('page:error:heading'); ?></div>
+    				<div class="error-text"><?php echo ossn_print('page:error:text'); ?></div>
+				</div>
+             </div>      
 </div>
