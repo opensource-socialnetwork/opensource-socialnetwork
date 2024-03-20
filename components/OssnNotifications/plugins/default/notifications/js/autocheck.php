e5f3c2c8c6c5e0ccb957a1506466bbea75f18e72
Arsalan Shah <manactric@gmail.com>
2024-03-20 20:07:38 +0500

    [E] Notification autocheck causing unresponsive page #2363

--- components/OssnNotifications/plugins/default/notifications/js/autocheck.php
+++ components/OssnNotifications/plugins/default/notifications/js/autocheck.php
@@ -2,12 +2,13 @@
 $component = new OssnComponents;
 $settings = $component->getComSettings('OssnNotifications');
 
-$auto_check = 60;	
-if($settings){
+$auto_check = 60;
+//[E] Notification autocheck causing unresponsive page #2363
+if($settings && isset($settings->autocheck_time)){
 	$auto_check = $settings->autocheck_time;	
 }
 //[E] Configure able autocheck OssnNotifications #2329
-$milliseconds = intval($auto_check) * 1000; //milliseconds = seconds × 1000
+$milliseconds = intval($auto_check) * 1000; //milliseconds = seconds Ã— 1000
 
 ?>
 //<script>
