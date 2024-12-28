8f7a2755935bb1d2704e6c42dbe6c64c2d68ca51
lianglee <manactric@gmail.com>
2024-12-28 15:09:20 +0500

    [B] Chat small window shows distorted friends if theme other than goblue #2371

--- components/OssnChat/plugins/default/chat/friends/friend-item.php
+++ components/OssnChat/plugins/default/chat/friends/friend-item.php
@@ -16,10 +16,8 @@ if ($friend->isOnline(10)) {
 }
 ?>
 <div class="friends-list-item" id="friend-list-item-<?php echo $friend->guid; ?>"
-     onClick="Ossn.ChatnewTab(<?php echo $friend->guid; ?>);">
+     onClick="Ossn.ChatnewTab(<?php echo $friend->guid; ?>);" data-toggle="tooltip" title="<?php  echo $friend->fullname;?>">
     <div class="friends-item-inner">
-        <div class="icon"><img class="user-icon-small" src="<?php echo $params['icon']; ?>"/></div>
-        <div class="name"><?php echo $friend->fullname; ?></div>
-        <div class="<?php echo $status; ?> ustatus"></div>
+        <div class="icon"><img class="user-icon-small <?php echo $status; ?> ustatus" src="<?php echo $params['icon']; ?>"/></div>
     </div>
 </div>
