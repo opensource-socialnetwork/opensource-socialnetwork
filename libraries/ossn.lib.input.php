347c0115b4f53d471dfa63286a69ac10fd997f6e
lianglee <manactric@gmail.com>
2025-08-31 22:30:38 +0500

    input update

--- libraries/ossn.lib.input.php
+++ libraries/ossn.lib.input.php
@@ -37,7 +37,7 @@ function ossn_input_escape($str, $newlines = true) {
 		return false;
 }
 /*
- * Encode the emojiis before it goes it database. 
+ * Encode the emojiis before it goes it database.
  *
  * UTF-32's hex encoding is the same as HTML's hex encoding.
  * So, by converting the emoji from UTF-8 to UTF-32, we magically
@@ -45,26 +45,25 @@ function ossn_input_escape($str, $newlines = true) {
  *
  * The below function is taken from wordpress , please use it under following license :
  * https://wordpress.org/about/license/
- * 
+ *
  * @param string $content The content to encode.
  * @return string The encoded content
  */
 function ossn_emojis_to_entites($content) {
-		if (function_exists('mb_convert_encoding')) {
+		if(function_exists('mb_convert_encoding')) {
 				$regex = '/(
 		     \x23\xE2\x83\xA3               # Digits
 		     [\x30-\x39]\xE2\x83\xA3
 		   | \xF0[\x90-\xBF][\x80-\xBF][\x80-\xBF]            # range x10000 - x3FFFF
 		   | [\xF1-\xF3][\x80-\xBF][\x80-\xBF][\x80-\xBF]     # range x40000 - xFFFFF
 		)/x';
-				
+
 				$matches = array();
-				if (preg_match_all($regex, $content, $matches)) {
-						if (!empty($matches[1])) {
+				if(preg_match_all($regex, $content, $matches)) {
+						if(!empty($matches[1])) {
 								foreach ($matches[1] as $emoji) {
-										
 										$unpacked = unpack('H*', mb_convert_encoding($emoji, 'UTF-32', 'UTF-8'));
-										if (isset($unpacked[1])) {
+										if(isset($unpacked[1])) {
 												$entity  = '&#x' . ltrim($unpacked[1], '0') . ';';
 												$content = str_replace($emoji, $entity, $content);
 										}
@@ -72,7 +71,7 @@ function ossn_emojis_to_entites($content) {
 						}
 				}
 		}
-		
+
 		return $content;
 }
 /**
@@ -87,53 +86,56 @@ function ossn_emojis_to_entites($content) {
  * @return false|string
  */
 function input($input, $noencode = '', $default = false, $strip = true) {
-		$str  = false;
-		//#1230 breaks when array is input #1474
-		if(isset($_REQUEST[$input]) && !is_array($_REQUEST[$input])){
-			$data_hook = ((strlen($_REQUEST[$input]) > 0) ? preg_replace('/\x20+/', ' ', $_REQUEST[$input]) : null);
+		$str = false;
+
+		if(isset($_REQUEST[$input]) && !is_array($_REQUEST[$input])) {
+				$data_hook = strlen($_REQUEST[$input]) > 0 ? preg_replace('/\x20+/', ' ', $_REQUEST[$input]) : null;
+		} elseif(!empty($_REQUEST[$input])) {
+				$data_hook = preg_replace('/\x20+/', ' ', $_REQUEST[$input]);
 		} else {
-			if(!empty($_REQUEST[$input])){
-				$data_hook = preg_replace('/\x20+/', ' ', $_REQUEST[$input]); 	
-			} else {
-				$data_hook = null;	
-			}
+				$data_hook = null;
 		}
+
 		$hook = ossn_call_hook('ossn', 'input', false, array(
-				'input' => $input,
+				'input'    => $input,
 				'noencode' => $noencode,
-				'default' => $default,
-				'strip' => $strip,
-				'data' => $data_hook,
+				'default'  => $default,
+				'strip'    => $strip,
+				'data'     => $data_hook,
 		));
-		if ($hook) {
+
+		if($hook) {
 				$input    = $hook['input'];
 				$noencode = $hook['noencode'];
 				$default  = $hook['default'];
 				$strip    = $hook['strip'];
-				if (isset($hook['data']) && is_array($hook['data'])) {
+
+				if(isset($hook['data']) && is_array($hook['data'])) {
 						foreach ($hook['data'] as $key => $value) {
 								$hook['data'][$key] = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 						}
 						return $hook['data'];
 				}
-				if (!isset($hook['data']) && $default) {
+
+				if(!isset($hook['data']) && $default !== false) {
 						return $default;
 				}
-				if (isset($hook['data']) && empty($noencode)) {
-						$data = htmlspecialchars($hook['data'], ENT_QUOTES, 'UTF-8');
-						$str  = $data;
-				} elseif ($noencode == true) {
-						$str = $data;
-				}
-				if (strlen($str) > 0) {
-						$str = ossn_emojis_to_entites($str);
-						if ($strip) {
-								return trim(ossn_input_escape($str));
-						} else {
-								return ossn_input_escape($str);
+
+				if(isset($hook['data'])) {
+						if($noencode === '' || $noencode === false) {
+								$str = htmlspecialchars($hook['data'], ENT_QUOTES, 'UTF-8');
+						} elseif($noencode === true) {
+								$str = $hook['data'];
 						}
 				}
+
+				if(strlen($str) > 0) {
+						$str = ossn_emojis_to_entites($str);
+						$str = ossn_input_escape($str);
+						return $strip ? trim($str) : $str;
+				}
 		}
+
 		return false;
 }
 /**
@@ -146,16 +148,16 @@ function input($input, $noencode = '', $default = false, $strip = true) {
  * @return string
  */
 function ossn_restore_new_lines($string, $br = false) {
-		if (strlen($string) == 0) {
+		if(strlen($string) == 0) {
 				return false;
 		}
 		$replacements = array(
 				"\n" => '\n',
-				"\r" => '\r'
+				"\r" => '\r',
 		);
 		$replacements = array_flip($replacements);
 		$result       = strtr($string, $replacements);
-		if ($br === true) {
+		if($br === true) {
 				$result = nl2br($result);
 		}
 		return $result;
@@ -165,11 +167,11 @@ function ossn_restore_new_lines($string, $br = false) {
  *
  * @param string $name Name of input
  * @param string $value Value of input
- * 
+ *
  * @return void
  */
 function ossn_set_input($name, $value) {
-		if (isset($name) && isset($value)) {
+		if(isset($name) && isset($value)) {
 				$_REQUEST[$name] = $value;
 		}
 }
