3804d3968144db8e0181d895b0edb1514e692769
lianglee <manactric@gmail.com>
2025-09-19 16:11:06 +0500

    [E] Add styling to license page on installation #2473

--- installation/pages/license.php
+++ installation/pages/license.php
@@ -10,8 +10,16 @@
  */
 
 
-echo '<div><div class="layout-installation"><h2>' . ossn_installation_print('ossn:check') . '</h2><div style="margin:0 auto; width:900px;"><div style="background: #f9f9f9; padding: 20px; border-radius: 3px; border: 2px dashed #eee;">';
+echo '<div><div class="layout-installation"><h2>' . ossn_installation_print('ossn:check') . '</h2><div style="margin:0 auto; width:900px;"><div id="ossnlicense" style="background: #f9f9f9; padding: 20px; border-radius: 3px; border: 2px dashed #eee;">';
 echo nl2br(file_get_contents(dirname(dirname(dirname(__FILE__))) . '/LICENSE.md'));
 echo '</div><br />';
 echo '<a href="' . ossn_installation_paths()->url . '?page=settings" class="button-blue primary">'.ossn_installation_print('ossn:install:next').'</a>';
 echo '</div><br /><br /></div>';
+?>
+<script type="module">
+    import { marked } from 'https://cdn.jsdelivr.net/npm/marked@4.0.12/lib/marked.esm.min.js';
+
+    const markdownContent = document.getElementById('ossnlicense').textContent;
+    const htmlContent = marked(markdownContent);
+    document.getElementById('ossnlicense').innerHTML = htmlContent;
+</script>
