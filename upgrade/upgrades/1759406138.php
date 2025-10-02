c41b75faedf2c87b2b152ab5277d44acf12c53b8
lianglee <manactric@gmail.com>
2025-10-02 17:01:39 +0500

    1759406138

---
+++ upgrade/upgrades/1759406138.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   Open Source Social Network (OSSN)
+ * @author    OSSN Core Team <info@openteknik.com>
+ * @copyright (C) OpenTeknik LLC
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+ 
+//update version once done
+ossn_generate_server_config('apache');
+ossn_version_upgrade('8.7');
+
+$factory = new OssnFactory(array(
+		'callback' => 'installation',
+		'website'  => ossn_site_url(),
+		'email'    => ossn_site_settings('owner_email'),
+		'version'  => '8.7',
+));
+$factory->connect();
