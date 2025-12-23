774b24ce6ecc550e8a9d31198e7ae192d388beb1
lianglee <manactric@gmail.com>
2025-12-23 14:51:52 +0500

    Remove extra strings  #2509

--- components/OssnLocation/locale/ossn.zh.php
+++ components/OssnLocation/locale/ossn.zh.php
@@ -11,8 +11,5 @@
 $zh = array(
 		'ossnlocation' => 'OssnLocation',
 		'enter:location' => '輸入地點',
-		'ossn:location:settings:save:error' => '無法儲存設定! 請稍後再試一次.',
-		'ossn:location:settings:saved' => '設定已儲存!',
-		'ossn:location:admin:notice' => '個人首頁貼文串',
 );
 ossn_register_languages('zh', $zh);
