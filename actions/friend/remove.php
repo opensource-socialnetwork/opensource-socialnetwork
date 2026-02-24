239cba2d5b189140f17758354cd5ea7f4df2e0d2
lianglee <manactric@gmail.com>
2026-02-24 17:55:24 +0500

    [B] missing exit 2 times in actions/friend/remove #2529

--- actions/friend/remove.php
+++ actions/friend/remove.php
@@ -8,25 +8,29 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
-if (ossn_is_xhr()) {
-    header('Content-Type: application/json');
+if(ossn_is_xhr()) {
+		header('Content-Type: application/json');
 }
-if (ossn_remove_friend(ossn_loggedin_user()->guid, input('user'))) {
-    if (!ossn_is_xhr()) {
-        ossn_trigger_message(ossn_print('ossn:notification:delete:friend'), 'error');
-        redirect(REF);
-    }
-    if (ossn_is_xhr()) {
-        echo json_encode(array(
-                'type' => 1,
-                'text' => ossn_print('ossn:notification:delete:friend'),
-            ));
-    }
+if(ossn_remove_friend(ossn_loggedin_user()->guid, input('user'))) {
+		if(!ossn_is_xhr()) {
+				ossn_trigger_message(ossn_print('ossn:notification:delete:friend'), 'error');
+				redirect(REF);
+		}
+		if(ossn_is_xhr()) {
+				echo json_encode(array(
+						'type' => 1,
+						'text' => ossn_print('ossn:notification:delete:friend'),
+				));
+				exit();
+		}
 } else {
-    if (!ossn_is_xhr()) {
-        redirect(REF);
-    }
-    if (ossn_is_xhr()) {
-        echo json_encode(array('type' => 0,));
-    }
+		if(!ossn_is_xhr()) {
+				redirect(REF);
+		}
+		if(ossn_is_xhr()) {
+				echo json_encode(array(
+						'type' => 0,
+				));
+				exit();
+		}
 }
