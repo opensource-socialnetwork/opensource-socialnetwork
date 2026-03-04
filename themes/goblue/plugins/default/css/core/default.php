b1c35b1b80a37a8e18767d68723c2857877587f4
lianglee <manactric@gmail.com>
2026-03-04 21:34:41 +0500

    [B] background is loading from wrong URL #2539

--- themes/goblue/plugins/default/css/core/default.php
+++ themes/goblue/plugins/default/css/core/default.php
@@ -3740,7 +3740,7 @@ label {
 	inset: 0;
 	background-size: cover;
 	z-index: -2;
-	background: url(http://local.opensource-socialnetwork.org/themes/goblue/images/background.jpg) no-repeat;
+	background: url("<?php echo ossn_theme_url();?>themes/goblue/images/background.jpg") no-repeat;
 	background-size: cover;
 }
 
