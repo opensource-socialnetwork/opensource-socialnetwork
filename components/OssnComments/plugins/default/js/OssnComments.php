f08842a3093b0957323079c3ae8dc65bc074063d
lianglee <manactric@gmail.com>
2026-05-25 20:12:16 +0500

    [E] Unifed ossn comments js code #2577

--- components/OssnComments/plugins/default/js/OssnComments.php
+++ components/OssnComments/plugins/default/js/OssnComments.php
@@ -13,205 +13,129 @@ Ossn.register_callback('ossn', 'init', 'ossn_comment_delete_handler');
 Ossn.register_callback('ossn', 'init', 'ossn_comment_edit');
 Ossn.register_callback('ossn', 'init', 'ossn_comment_post_sm_button');
 
-function ossn_comments_replace_tags(input, allowed) {
-	allowed = (((allowed || '') + '').toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join('')
-	var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi
-	var commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>|&nbsp;/gi
-	return input.replace(commentsAndPhpTags, '').replace(tags, function ($0, $1) {
-		return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : ''
+/**
+ * UNIFIED COMMENT ENGINE PIPELINE (Plain-Text Optimized)
+ */
+Ossn.ExecuteCommentPipeline = function (targetUrl, $container, typeKey, callbackNamespace) {
+	const $box = $('#comment-box-' + typeKey + $container);
+	const $form = $('#comment-container-' + typeKey + $container);
+
+	// Event Interceptions: Keypress Enter Submission Guards, Space Hooks & Active Input Scanning
+	$box.off('keypress paste drop keyup focus').on('keypress', function (e) {
+		// 1. Handle Spacebar Input Callback Trigger
+		if (e.which == 32) {
+			const selection = window.getSelection();
+			if (selection.rangeCount > 0) {
+				const range = selection.getRangeAt(0);
+				const textBeforeCursor = range.startContainer.textContent ? range.startContainer.textContent.slice(0, range.startOffset) : '';
+				const words = textBeforeCursor.split(/\s|\u00A0/);
+				const currentWord = words[words.length - 1] || "";
+
+				Ossn.trigger_callback('comment', 'input:space', {
+					word: currentWord,
+					$editor: $box,
+					container: $container,
+					type: typeKey
+				});
+			}
+		}
+
+		// 2. Handle Enter Key Comment Submission (Using Clean Plain-Text)
+		if ($('.comment-post-btn').is(":visible") === false) {
+			if (e.which == 13) {
+				if (e.shiftKey === false) {
+					e.preventDefault(); // Prevents an extra line break from being added on submission
+
+					// Grab element text content directly (converts <br> elements to actual line breaks smoothly)
+					let plainText = $box[0].innerText || $box[0].textContent;
+
+					$form.append("<textarea name='comment' class='hidden'></textarea>");
+					$form.find('textarea[name="comment"]').text(plainText); // Safe text binding escape
+					$form.submit();
+				}
+			}
+		}
+	}).on('paste', function (e) {
+		e.preventDefault();
+		var text = (e.originalEvent || e).clipboardData.getData('text/plain');
+		window.document.execCommand('insertText', false, text);
+	}).on('drop', function (e) {
+		e.preventDefault();
+		var text = (e.originalEvent || e).dataTransfer.getData('text/plain');
+		if (!text) {
+			return;
+		}
+		$box.focus();
+		window.document.execCommand('insertText', false, text);
+	}).on('keyup focus', function (e) {
+		const selection = window.getSelection();
+		if (selection.rangeCount > 0) {
+			const range = selection.getRangeAt(0);
+			const textBeforeCursor = range.startContainer.textContent ? range.startContainer.textContent.slice(0, range.startOffset) : '';
+			const words = textBeforeCursor.split(/\s|\u00A0/);
+			const activeWord = words[words.length - 1] || "";
+
+			Ossn.trigger_callback('comment', 'input:scan', {
+				word: activeWord,
+				$editor: $box,
+				container: $container,
+				type: typeKey
+			});
+		}
 	});
-}
+
+	// Dynamic Core AJAX Request Submission Process
+	Ossn.ajaxRequest({
+		url: targetUrl,
+		form: '#comment-container-' + typeKey + $container,
+		beforeSend: function (request) {
+			$box.attr('readonly', 'readonly');
+			$box.attr('contenteditable', false);
+		},
+		callback: function (callback) {
+			if (callback['process'] == 1) {
+				$box.removeAttr('readonly');
+				$box.val('');
+				$('.ossn-comments-list-' + typeKey + $container).append(callback['comment']);
+				$('#comment-attachment-container-' + typeKey + $container).hide();
+				$('#ossn-comment-attachment-' + typeKey + $container).find('.image-data').html('');
+				$form.find('input[name="comment-attachment"]').val('');
+			}
+			if (callback['process'] == 0) {
+				$box.removeAttr('readonly');
+				Ossn.MessageBox('syserror/unknown');
+			}
+			$box.attr('contenteditable', true);
+			$box.html("");
+
+			Ossn.trigger_callback('comment', callbackNamespace + ':callback', {
+				guid: $container,
+				response: callback,
+			});
+		}
+	});
+};
+
+/**
+ * Structural Interface Layer Wrappers 
+ */
 Ossn.PostComment = function ($container) {
-	$('#comment-box-p' + $container).on('keypress', function (e) {
-		if ($('.comment-post-btn').is(":visible") === false) {
-			if (e.which == 13) {
-				if (e.shiftKey === false) {
-					//Postings and comments with same behaviour #924
-					$text = $('#comment-box-p' + $container).html();
-					$text = ossn_comments_replace_tags($text, '<br>').replace(/\<br\\?>/g, "\n");
-					$('#comment-container-p' + $container).append("<textarea name='comment' class='hidden'>" + $text + "</textarea>");
-					$('#comment-container-p' + $container).submit();
-				}
-			}
-		}
-	});
-	$('#comment-box-p' + $container).on('paste', function (e) {
-		e.preventDefault();
-		var text = (e.originalEvent || e).clipboardData.getData('text/plain');
-		window.document.execCommand('insertText', false, text);
-	});
-	$('#comment-box-p' + $container).on('drop', function (e) {
-		e.preventDefault();
-
-		// Get plain text from the drop dataTransfer object
-		var text = (e.originalEvent || e).dataTransfer.getData('text/plain');
-
-		// Fallback if the dropped data doesn't contain standard plain text
-		if (!text) {
-			return;
-		}
-
-		$(this).focus();
-		window.document.execCommand('insertText', false, text);
-	});
-	Ossn.ajaxRequest({
-		url: Ossn.site_url + 'action/post/comment',
-		form: '#comment-container-p' + $container,
-		beforeSend: function (request) {
-			$('#comment-box-p' + $container).attr('readonly', 'readonly');
-			$('#comment-box-p' + $container).attr('contenteditable', false);
-		},
-		callback: function (callback) {
-			if (callback['process'] == 1) {
-				$('#comment-box-p' + $container).removeAttr('readonly');
-				$('#comment-box-p' + $container).val('');
-				$('.ossn-comments-list-p' + $container).append(callback['comment']);
-				$('#comment-attachment-container-p' + $container).hide();
-				$('#ossn-comment-attachment-p' + $container).find('.image-data').html('');
-				//commenting pic followed by text gives warnings #664 $dev.githubertus
-				$('#comment-container-p' + $container).find('input[name="comment-attachment"]').val('');
-			}
-			if (callback['process'] == 0) {
-				$('#comment-box-p' + $container).removeAttr('readonly');
-				Ossn.MessageBox('syserror/unknown');
-			}
-			$('#comment-box-p' + $container).attr('contenteditable', true);
-			$('#comment-box-p' + $container).html("");
-			Ossn.trigger_callback('comment', 'post:callback', {
-				guid: $container,
-				response: callback,
-			});
-		}
-	});
+	const url = Ossn.site_url + 'action/post/comment';
+	Ossn.ExecuteCommentPipeline(url, $container, 'p', 'post');
 };
+
 Ossn.ObjectComment = function ($container) {
-	$('#comment-box-o' + $container).on('keypress', function (e) {
-		if ($('.comment-post-btn').is(":visible") === false) {
-			if (e.which == 13) {
-				if (e.shiftKey === false) {
-					//Postings and comments with same behaviour #924
-					$text = $('#comment-box-o' + $container).html();
-					$text = ossn_comments_replace_tags($text, '<br>').replace(/\<br\\?>/g, "\n");
-					$('#comment-container-o' + $container).append("<textarea name='comment' class='hidden'>" + $text + "</textarea>");
-					$('#comment-container-o' + $container).submit();
-				}
-			}
-		}
-	});
-	$('#comment-box-o' + $container).on('paste', function (e) {
-		e.preventDefault();
-		var text = (e.originalEvent || e).clipboardData.getData('text/plain');
-		window.document.execCommand('insertText', false, text);
-	});
-	$('#comment-box-o' + $container).on('drop', function (e) {
-		e.preventDefault();
-
-		// Get plain text from the drop dataTransfer object
-		var text = (e.originalEvent || e).dataTransfer.getData('text/plain');
-
-		// Fallback if the dropped data doesn't contain standard plain text
-		if (!text) {
-			return;
-		}
-
-		$(this).focus();
-		window.document.execCommand('insertText', false, text);
-	});
-	Ossn.ajaxRequest({
-		url: Ossn.site_url + 'action/post/object/comment',
-		form: '#comment-container-o' + $container,
-		beforeSend: function (request) {
-			$('#comment-box-o' + $container).attr('readonly', 'readonly');
-			$('#comment-box-o' + $container).attr('contenteditable', false);
-		},
-		callback: function (callback) {
-			if (callback['process'] == 1) {
-				$('#comment-box-o' + $container).removeAttr('readonly');
-				$('#comment-box-o' + $container).val('');
-				$('.ossn-comments-list-o' + $container).append(callback['comment']);
-				$('#comment-attachment-container-o' + $container).hide();
-				$('#ossn-comment-attachment-o' + $container).find('.image-data').html('');
-				//commenting pic followed by text gives warnings #664 $dev.githubertus
-				$('#comment-container-o' + $container).find('input[name="comment-attachment"]').val('');
-			}
-			if (callback['process'] == 0) {
-				$('#comment-box-o' + $container).removeAttr('readonly');
-				Ossn.MessageBox('syserror/unknown');
-			}
-			$('#comment-box-o' + $container).attr('contenteditable', true);
-			$('#comment-box-o' + $container).html("");
-			Ossn.trigger_callback('comment', 'object:callback', {
-				guid: $container,
-				response: callback,
-			});
-		}
-	});
+	const url = Ossn.site_url + 'action/post/object/comment';
+	Ossn.ExecuteCommentPipeline(url, $container, 'o', 'object');
 };
+
 Ossn.EntityComment = function ($container) {
-	$('#comment-box-e' + $container).on('keypress', function (e) {
-		if ($('.comment-post-btn').is(":visible") === false) {
-			if (e.which == 13) {
-				if (e.shiftKey === false) {
-					//Postings and comments with same behaviour #924
-					$text = $('#comment-box-e' + $container).html();
-					$text = ossn_comments_replace_tags($text, '<br>').replace(/\<br\\?>/g, "\n");
-					$('#comment-container-e' + $container).append("<textarea name='comment' class='hidden'>" + $text + "</textarea>");
-					$('#comment-container-e' + $container).submit();
-				}
-			}
-		}
-	});
-	$('#comment-box-e' + $container).on('paste', function (e) {
-		e.preventDefault();
-		var text = (e.originalEvent || e).clipboardData.getData('text/plain');
-		window.document.execCommand('insertText', false, text);
-	});
-	$('#comment-box-e' + $container).on('drop', function (e) {
-		e.preventDefault();
-
-		// Get plain text from the drop dataTransfer object
-		var text = (e.originalEvent || e).dataTransfer.getData('text/plain');
-
-		// Fallback if the dropped data doesn't contain standard plain text
-		if (!text) {
-			return;
-		}
-
-		$(this).focus();
-		window.document.execCommand('insertText', false, text);
-	});
-	Ossn.ajaxRequest({
-		url: Ossn.site_url + 'action/post/entity/comment',
-		form: '#comment-container-e' + $container,
-		beforeSend: function (request) {
-			$('#comment-box-e' + $container).attr('readonly', 'readonly');
-			$('#comment-box-e' + $container).attr('contenteditable', false);
-
-		},
-		callback: function (callback) {
-			if (callback['process'] == 1) {
-				$('#comment-box-e' + $container).removeAttr('readonly');
-				$('#comment-box-e' + $container).val('');
-				$('.ossn-comments-list-e' + $container).append(callback['comment']);
-				$('#comment-attachment-container-e' + $container).hide();
-				$('#ossn-comment-attachment-e' + $container).find('.image-data').html('');
-				$('#comment-container-e' + $container).find('input[name="comment-attachment"]').val('');
-			}
-			if (callback['process'] == 0) {
-				$('#comment-box-e' + $container).removeAttr('readonly');
-				Ossn.MessageBox('syserror/unknown');
-			}
-			$('#comment-box-e' + $container).attr('contenteditable', true);
-			$('#comment-box-e' + $container).html("");
-			Ossn.trigger_callback('comment', 'entity:callback', {
-				guid: $container,
-				response: callback,
-			});
-		}
-	});
+	const url = Ossn.site_url + 'action/post/entity/comment';
+	Ossn.ExecuteCommentPipeline(url, $container, 'e', 'entity');
 };
+
 Ossn.CommentMenu = function ($id) {
-	$element = $($id).find('.menu-links');
+	var $element = $($id).find('.menu-links');
 	if ($element.is(":not(:visible)")) {
 		$element.show();
 		$($id).find('.drop-down-arrow').attr('style', 'display:block;');
@@ -225,25 +149,25 @@ function ossn_comment_delete_handler() {
 	$(document).ready(function () {
 		$('body').on('click', '.ossn-delete-comment', function (e) {
 			e.preventDefault();
-			$comment = $(this);
-			$url = $comment.attr('href');
-			$comment_id = Ossn.UrlParams('comment', $url);
+			var $comment = $(this);
+			var url = $comment.attr('href');
+			var id = Ossn.UrlParams('comment', url);
 
 			Ossn.PostRequest({
-				url: $url,
+				url: url,
 				action: false,
 				beforeSend: function () {
-					$('#comments-item-' + $comment_id).attr('style', 'opacity:0.6;');
+					$('#comments-item-' + id).attr('style', 'opacity:0.6;');
 				},
 				callback: function (callback) {
 					if (callback == 1) {
-						$('#comments-item-' + $comment_id).fadeOut().remove();
+						$('#comments-item-' + id).fadeOut().remove();
 					}
 					if (callback == 0) {
-						$('#comments-item-' + $comment_id).attr('style', 'opacity:0.6;');
+						$('#comments-item-' + id).attr('style', 'opacity:1.0;');
 					}
 					Ossn.trigger_callback('comment', 'delete:callback', {
-						id: $comment_id,
+						id: id,
 						response: callback,
 					});
 				}
@@ -251,35 +175,35 @@ function ossn_comment_delete_handler() {
 		});
 	});
 }
+
 Ossn.CommentImage = function ($container, $ftype) {
-	$ftype = $ftype[0]; // '[p]ost' or '[e]ntity' or '[o]bject'
+	var typeKey = $ftype[0];
 	$(document).ready(function () {
-		$("#ossn-comment-image-file-" + $ftype + "" + $container).on('change', function (event) {
+		$("#ossn-comment-image-file-" + typeKey + $container).off('change').on('change', function (event) {
 			event.preventDefault();
-			var formData = new FormData($('#ossn-comment-attachment-' + $ftype + '' + $container)[0]);
+			var formData = new FormData($('#ossn-comment-attachment-' + typeKey + $container)[0]);
 			$.ajax({
 				url: Ossn.site_url + 'comment/attachment',
 				type: 'POST',
 				data: formData,
 				async: true,
 				beforeSend: function () {
-					$('#ossn-comment-attachment-' + $ftype + '' + $container).find('.image-data')
+					$('#ossn-comment-attachment-' + typeKey + $container).find('.image-data')
 						.html('<img src="' + Ossn.site_url + 'components/OssnComments/images/loading.gif" style="width:30px;border:none;height: initial;" />');
-					$('#comment-attachment-container-' + $ftype + '' + $container).show();
-
+					$('#comment-attachment-container-' + typeKey + $container).show();
 				},
 				cache: false,
 				contentType: false,
 				processData: false,
 				success: function (callback) {
 					if (callback['success']) {
-						$('#comment-container-' + $ftype + '' + $container).find('input[name="comment-attachment"]').val(callback['file']);
-						$('#ossn-comment-attachment-' + $ftype + '' + $container).find('.image-data')
+						$('#comment-container-' + typeKey + $container).find('input[name="comment-attachment"]').val(callback['file']);
+						$('#ossn-comment-attachment-' + typeKey + $container).find('.image-data')
 							.html('<img src="' + Ossn.site_url + 'comment/staticimage?image=' + callback['file'] + '" />');
 					} else {
 						if (callback['error']) {
-							$('#comment-container-' + $ftype + '' + $container).find('input[name="comment-attachment"]').val('');
-							$('#comment-attachment-container-' + $ftype + '' + $container).hide();
+							$('#comment-container-' + typeKey + $container).find('input[name="comment-attachment"]').val('');
+							$('#comment-attachment-container-' + typeKey + $container).hide();
 							$('.ossn-message-box').html(callback['error']).fadeIn();
 						} else {
 							Ossn.MessageBox('syserror/unknown');
@@ -287,7 +211,7 @@ Ossn.CommentImage = function ($container, $ftype) {
 					}
 					Ossn.trigger_callback('comment', 'attachment:image:callback', {
 						guid: $container,
-						type: $ftype,
+						type: typeKey,
 						response: callback,
 					});
 				},
@@ -305,29 +229,25 @@ function ossn_comment_edit() {
 	$(document).ready(function () {
 		$('body').on('click', '.ossn-edit-comment', function () {
 			var $dataguid = $(this).attr('data-guid');
+
 			Ossn.MessageBox('comment/edit/' + $dataguid);
 		});
-		//comment edit
 		Ossn.ajaxRequest({
 			url: Ossn.site_url + "action/comment/edit",
 			containMedia: true,
 			form: '#ossn-comment-edit-form',
 			beforeSend: function () {
-				$('#ossn-comment-edit-form').find('textarea').hide();
-				$('#ossn-comment-edit-form').append('<div class="ossn-loading ossn-box-loading"></div>');
+				$('#ossn-comment-edit-form').find('textarea').hide().parent().append('<div class="ossn-loading ossn-box-loading"></div>');
 			},
 			callback: function (callback) {
 				if (callback['success']) {
-					$text = $('#ossn-comment-edit-form').find('#comment-edit').val();
-					$guid = $('#ossn-comment-edit-form').find('input[name="guid"]').val();
-					$elem = $("#comments-item-" + $guid).find('.comment-contents').find('.comment-text:first');
-					if ($elem) {
+					var $text = $('#ossn-comment-edit-form').find('#comment-edit').val();
+					var $guid = $('#ossn-comment-edit-form').find('input[name="guid"]').val();
+					var $elem = $("#comments-item-" + $guid).find('.comment-contents').find('.comment-text:first');
+					if ($elem.length > 0) {
 						$elem.text('');
 						Ossn.PostRequest({
 							url: Ossn.site_url + "action/comment/embed",
-							// params: 'content=' + $text,
-							// make '+' characters work
-							// see https://stackoverflow.com/questions/1373414/ajax-post-and-plus-sign-how-to-encode
 							params: 'content=' + encodeURIComponent($text) + '&guid=' + $guid,
 							callback: function (return_data) {
 								$elem.append(return_data['data']);
@@ -346,22 +266,27 @@ function ossn_comment_edit() {
 				Ossn.MessageBoxClose();
 			}
 		});
-		//comment-edit />									
 	});
 }
 
+/**
+ * Update the SM Button handler to use the same clean plain-text method
+ */
 function ossn_comment_post_sm_button() {
 	$(document).ready(function () {
 		$('body').on('click', '.comment-post-btn', function () {
-			$type = $(this).attr('data-type');
-			$guid = $(this).attr('data-guid');
+			var $type = $(this).attr('data-type');
+			var $guid = $(this).attr('data-guid');
 			if ($type == 'p' || $type == 'o' || $type == 'e') {
-				$text = $('#comment-box-' + $type + '' + $guid).html();
-				$container = $('#comment-container-' + $type + '' + $guid);
+				let $box = $('#comment-box-' + $type + $guid);
+				let $container = $('#comment-container-' + $type + $guid);
+
+				let plainText = $box[0].innerText || $box[0].textContent;
+
+				$container.append("<textarea name='comment' class='hidden'></textarea>");
+				$container.find('textarea[name="comment"]').text(plainText);
+				$container.submit();
 			}
-			$text = ossn_comments_replace_tags($text, '<br>').replace(/\<br\\?>/g, "\n");
-			$container.append("<textarea name='comment' class='hidden'>" + $text + "</textarea>");
-			$container.submit();
 		});
 	});
 }
