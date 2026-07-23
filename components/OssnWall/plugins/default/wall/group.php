a035b636cf7a309779e22ae8e71bb6e9f98a4e40
lianglee <manactric@gmail.com>
2026-07-23 16:15:04 +0500

    [E] Add callback before wall activity output each post #2623

--- components/OssnWall/plugins/default/wall/group.php
+++ components/OssnWall/plugins/default/wall/group.php
@@ -8,34 +8,44 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
-//[B] Wall is not visible for group member (other than creator) #2340 
+//[B] Wall is not visible for group member (other than creator) #2340
 //should be == instead of ===  as value can be either 1 or true ,  0 or false
 if($params['ismember'] == true) {
 		echo '<div class="ossn-wall-container">';
-		echo ossn_view_form('group/container', array(
-				'action' => ossn_site_url() . 'action/wall/post/g',
-				'component' => 'OssnWall',
-				'id' => 'ossn-wall-form',
-				'params' => array(
-						'group' => $params['group']
-				)
-		), false);
+		echo ossn_view_form(
+				'group/container',
+				array(
+						'action'    => ossn_site_url() . 'action/wall/post/g',
+						'component' => 'OssnWall',
+						'id'        => 'ossn-wall-form',
+						'params'    => array(
+								'group' => $params['group'],
+						),
+				),
+				false
+		);
 		echo '</div>';
 }
 echo '<div class="user-activity">';
-$posts = new OssnWall;
+$posts = new OssnWall();
 $count = 0;
 if($params['ismember'] == true || $params['membership'] == OSSN_PUBLIC) {
 		$count = $posts->GetPostByOwner($params['group']['group']->guid, 'group', true);
 		$posts = $posts->GetPostByOwner($params['group']['group']->guid, 'group');
 }
 if($posts) {
-		foreach($posts as $post) {
+		foreach ($posts as $index => $post) {
 				$vars = ossn_wallpost_to_item($post);
 				//selecting a CLOSED group like MYSITE/group/123/ gives warning #663
-				if(!empty($vars) && is_array($vars)){ 
-					$vars['ismember'] = $params['ismember'];
-					echo ossn_wall_view_template($vars);
+				if(!empty($vars) && is_array($vars)) {
+						$vars['ismember'] = $params['ismember'];
+
+						ossn_trigger_callback('wall', 'loop:item:view:group', array(
+								'post'  => $post,
+								'index' => $index,
+						));
+
+						echo ossn_wall_view_template($vars);
 				}
 		}
 }
--- components/OssnWall/plugins/default/wall/user/wall.php
+++ components/OssnWall/plugins/default/wall/user/wall.php
@@ -8,27 +8,35 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
-$OssnWall = new OssnWall;
-$posts = $OssnWall->GetUserPosts($params['user']);
-//lastchage: site admins are unable to access member profile threads without friendship #176 
+$OssnWall = new OssnWall();
+$posts    = $OssnWall->GetUserPosts($params['user']);
+//lastchage: site admins are unable to access member profile threads without friendship #176
 if(ossn_loggedin_user() && (ossn_user_is_friend(ossn_loggedin_user()->guid, $params['user']->guid) || ossn_loggedin_user()->guid == $params['user']->guid || ossn_isAdminLoggedin())) {
 		echo '<div class="ossn-wall-container">';
-		echo ossn_view_form('user/container', array(
-				'action' => ossn_site_url() . 'action/wall/post/u',
-				'component' => 'OssnWall',
-				'id' => 'ossn-wall-form',
-				'params' => array(
-						'user' => $params['user']
-				)
-		), false);
+		echo ossn_view_form(
+				'user/container',
+				array(
+						'action'    => ossn_site_url() . 'action/wall/post/u',
+						'component' => 'OssnWall',
+						'id'        => 'ossn-wall-form',
+						'params'    => array(
+								'user' => $params['user'],
+						),
+				),
+				false
+		);
 		echo '</div>';
 }
 echo '<div class="user-activity">';
 if($posts) {
 		$count = $OssnWall->GetUserPosts($params['user'], array(
-			'count' => true,													 
-		));	
-		foreach($posts as $post) {
+				'count' => true,
+		));
+		foreach ($posts as $index => $post) {
+				ossn_trigger_callback('wall', 'loop:item:view:user', array(
+						'post'  => $post,
+						'index' => $index,
+				));
 				$item = ossn_wallpost_to_item($post);
 				echo ossn_wall_view_template($item);
 		}
