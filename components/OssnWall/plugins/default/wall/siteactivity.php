4845cd9a6271ef3beb8eb5dfbde1267a9dcdaca3
lianglee <manactric@gmail.com>
2026-07-23 16:12:35 +0500

    [E] Add callback before wall activity output each post #2623

--- components/OssnWall/plugins/default/wall/siteactivity.php
+++ components/OssnWall/plugins/default/wall/siteactivity.php
@@ -8,52 +8,55 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
-$wall       = new OssnWall;
+$wall = new OssnWall();
 
-$accesstype = ossn_get_homepage_wall_access();
+$accesstype   = ossn_get_homepage_wall_access();
 $loggedinuser = ossn_loggedin_user();
 
 // allow admin to watch ALL postings independant of Wall setting
 if($loggedinuser->canModerate()) {
-	$posts = $wall->getAllPosts(array(
-				'type' => 'user',
+		$posts = $wall->getAllPosts(array(
+				'type'     => 'user',
 				'distinct' => true,
-	));
-	$count = $wall->getAllPosts(array(
-			'type' => 'user',
-			'count' => true,
-			'distinct' => true,
-	));
-// wall mode: all site posts
+		));
+		$count = $wall->getAllPosts(array(
+				'type'     => 'user',
+				'count'    => true,
+				'distinct' => true,
+		));
+		// wall mode: all site posts
 } elseif($accesstype == 'public') {
-	$posts = $wall->getPublicPosts(array(
-				'type' => 'user',
+		$posts = $wall->getPublicPosts(array(
+				'type'     => 'user',
 				'distinct' => true,
-	));
-	$count = $wall->getPublicPosts(array(
-			'type' => 'user',
-			'count' => true,
-			'distinct' => true,
-	));
-// wall mode: friends-only posts	
+		));
+		$count = $wall->getPublicPosts(array(
+				'type'     => 'user',
+				'count'    => true,
+				'distinct' => true,
+		));
+		// wall mode: friends-only posts
 } elseif($accesstype == 'friends') {
-	$posts = $wall->getFriendsPosts(array(
-				'type' => 'user',
+		$posts = $wall->getFriendsPosts(array(
+				'type'     => 'user',
 				'distinct' => true,
-	));
-	$count = $wall->getFriendsPosts(array(
-			'type' => 'user',
-			'count' => true,
-			'distinct' => true,
-	));
+		));
+		$count = $wall->getFriendsPosts(array(
+				'type'     => 'user',
+				'count'    => true,
+				'distinct' => true,
+		));
 }
 
 if($posts) {
-		foreach($posts as $post) {
+		foreach ($posts as $index => $post) {
+				ossn_trigger_callback('wall', 'loop:item:view', array(
+						'post'  => $post,
+						'index' => $index,
+				));
 				$item = ossn_wallpost_to_item($post);
 				echo ossn_wall_view_template($item);
 		}
-		
 }
 
 echo ossn_view_pagination($count);
