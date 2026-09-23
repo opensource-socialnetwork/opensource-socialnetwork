ae2e8cef14f0c98b267ad968b62efda72cb9b2c9
lianglee <manactric@gmail.com>
2026-09-23 14:26:23 +0500

    delete action

--- components/OssnWall/actions/wall/post/delete.php
+++ components/OssnWall/actions/wall/post/delete.php
@@ -13,44 +13,24 @@ $id = input('post');
 $post = $ossnwall->GetPost($id);
 if ($post->type == 'user' && !ossn_isAdminLoggedin()) {
     if ($post->poster_guid != ossn_loggedin_user()->guid && $post->owner_guid !== ossn_loggedin_user()->guid) {
-        if (!ossn_is_xhr()) {
-            ossn_trigger_message(ossn_print('post:delete:fail'), 'error');
-            redirect(REF);
-        } else {
-            echo 0;
-            exit;
-        }
+        echo 0;
+        exit;
     }
 }
 if ($post->type == 'group' && !ossn_isAdminLoggedin() && !ossn_loggedin_user()->canModerate()) {
     $group = new OssnGroup;
     $group = $group->getGroup($post->owner_guid);
-	//lastchange group admins are unable to delete member posting on group wall #171
-	// change or operator to and
-    if ($post->poster_guid != ossn_loggedin_user()->guid && ossn_loggedin_user()->guid !== $group->owner_guid &&  !$group->isModerator(ossn_loggedin_user()->guid)) {
-        if (!ossn_is_xhr()) {
-            ossn_trigger_message(ossn_print('post:delete:fail'), 'error');
-            redirect(REF);
-        } else {
-            echo 0;
-            exit;
-        }
+    //lastchange group admins are unable to delete member posting on group wall #171
+    // change or operator to and
+    if ($post->poster_guid != ossn_loggedin_user()->guid && ossn_loggedin_user()->guid !== $group->owner_guid && !$group->isModerator(ossn_loggedin_user()->guid)) {
+        echo 0;
+        exit;
     }
 }
 if ($ossnwall->deletePost($id)) {
-    if (ossn_is_xhr()) {
-        echo 1;
-	exit;
-    } else {
-        ossn_trigger_message(ossn_print('post:delete:success'), 'success');
-        redirect(REF);
-    }
+    echo 1;
+    exit;
 } else {
-    if (ossn_is_xhr()) {
-        echo 0;
-	exit;
-    } else {
-        ossn_trigger_message(ossn_print('post:delete:fail'), 'error');
-        redirect(REF);
-    }
+    echo 0;
+    exit;
 }
